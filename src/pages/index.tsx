2c24731624360efc83e8dbc8f24d146384d0ea51
Artyom Gorin <mysubcult1@gmail.com>
2023-03-26 01:45:12 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Contact from "../components/Contact";
+import Pricing from "../components/Pricing";
 import { Hero } from "../components/Hero";
 import Layout from "../components/Layout";
-import Pricing from "../components/Pricing";
 import { Services } from "../components/Services";
 import Faq from "../components/Faq";
 import Customers from "../components/Customers";
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <Layout>
       <Hero />
-      <Services />
       <Pricing />
+      <Services />
       <Faq />
       <Contact />
       <Customers />
