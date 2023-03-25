6ef63a57f370e11a8b8851ba0184f6af97cf9669
Artyom Gorin <mysubcult1@gmail.com>
2023-03-26 02:03:28 +0400

    Update Prefooter.tsx

--- src/components/Prefooter.tsx
+++ src/components/Prefooter.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 
 export default function Prefooter() {
   return (
-    <div className="bg-gray-50 dark:bg-neutral-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pt-20 pb-10">
         </Link>
       </div>
     </div>
