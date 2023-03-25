2cf94082521f38ef5ca47fb4e7ca23cb815a3841
Artyom Gorin <mysubcult1@gmail.com>
2023-03-25 23:29:51 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -94,7 +94,7 @@ export default function Contact() {
             <form onSubmit={handleSubmit(onSubmit)}>
               <input
                 type="hidden"
-                value={process.env.NEXT_PUBLIC_FORM_CONTACT_API}
+                value="c39d2187-6537-4c0b-87e1-3cff0bf0c1c3"
                 {...register("access_key")}
               />
               <input type="hidden" {...register("subject")} />
