eddc476950bb0694bc3dfc8c75a8e3bd454eec6e
Artyom Gorin <mysubcult1@gmail.com>
2023-03-25 20:33:30 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,14 +7,11 @@ export function Hero() {
       <div className="max-w-7xl mx-auto pt-20 pb-16 px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2">
         <div className="pt-6 md:pt-32 justify-center text-center sm:justify-start sm:text-start">
           <h1 className="text-5xl font-bold">
-            Welcome to <br />{" "}
-            <span className="text-rose-500 font-extrabold">Halley</span>
+            Добро пожаловать в <br />{" "}
+            <span className="text-rose-500 font-extrabold">SmartDiag</span>
           </h1>
           <p className="pt-6 text-base w-auto sm:w-10/12 md:w-10/12 dark:text-neutral-400">
-            Plantilla de aplicación de página única (SPA) desarrollada con
-            Next.js, TypeScript y TailwindCSS. Personalizable y algunas
-            secciones para agregar información importante sobre un producto o un
-            servicio.
+            Здесь Вы можете найти все необходимое программное обеспечение для диагностики и обслуживания вашего автомобиля. Мы предоставляем высококачественное и надежное оборудование, которое поможет вам провести быстрый и эффективный анализ и ремонт вашего автомобиля.
           </p>
           <div className="flex flex-auto pt-10 gap-2 min-w-[350px] justify-center sm:justify-start">
             <div>
