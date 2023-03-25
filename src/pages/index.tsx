39a3711bd3b2ffe663cfd1d87e82488eade9fc15
Artyom Gorin <mysubcult1@gmail.com>
2023-03-26 02:53:03 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,7 @@ import Layout from "../components/Layout";
 import Faq from "../components/Faq";
 import { Services } from "../components/Services";
 import Customers from "../components/Customers";
-<script>var LHC_API = LHC_API||{};
-LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
-(function() {
-var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-</script>
+
 export default function Home() {
   return (
     <Layout>
