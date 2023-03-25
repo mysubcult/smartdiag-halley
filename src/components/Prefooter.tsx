7f67eafcea122df9cd736e07070c39007488958d
Artyom Gorin <mysubcult1@gmail.com>
2023-03-26 02:07:37 +0400

    Update Prefooter.tsx

--- src/components/Prefooter.tsx
+++ src/components/Prefooter.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Prefooter() {
   return (
-    <div className="bg-gray-50 dark:bg-neutral-800">
+    <div className="bg-white dark:bg-neutral-900">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pt-20 pb-10">
         <h1 className="text-4xl font-bold text-center">
           ¿Quieres una copia del código funete?
