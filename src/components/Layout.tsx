9c4da5340dd10786ba965052a5bdb1628ae227bc
Artyom Gorin <mysubcult1@gmail.com>
2023-03-28 03:06:28 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,6 +15,7 @@ const Layout = (props: any) => {
     type: "website",
     ...customMeta,
   };
+
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
@@ -31,33 +32,18 @@ const Layout = (props: any) => {
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-
-        {/* Добавьте этот скрипт */}
+        {/* Добавляем скрипт */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              var LHC_API = LHC_API || {};
-              LHC_API.args = {
-                mode: 'widget',
-                lhc_base_url: '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
-                wheight: 450,
-                wwidth: 350,
-                pheight: 520,
-                pwidth: 500,
-                leaveamessage: true,
-                check_messages: false
-              };
-              (function () {
-                var po = document.createElement('script');
-                po.type = 'text/javascript';
-                po.setAttribute('crossorigin', 'anonymous');
-                po.async = true;
-                var date = new Date();
-                po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ('' + date.getFullYear() + date.getMonth() + date.getDate());
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(po, s);
-              })();
-            `
+            var LHC_API = LHC_API||{};
+            LHC_API.args = {mode:'widget',lhc_base_url:'//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,leaveamessage:true,department:["1"],theme:"1",check_messages:false};
+            (function() {
+              var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+              var date = new Date();po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+            })();
+            `,
           }}
         />
       </Head>
