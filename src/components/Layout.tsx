5b606a9f438c73e86a246118d3aa4c8dc1e17b0b
Artyom Gorin <mysubcult1@gmail.com>
2023-03-28 03:02:21 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -26,11 +26,40 @@ const Layout = (props: any) => {
           property="og:url"
           content={`https://halley.vercel.app${router.asPath}`}
         />
-          <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
+
+        {/* Добавьте этот скрипт */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var LHC_API = LHC_API || {};
+              LHC_API.args = {
+                mode: 'widget',
+                lhc_base_url: '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
+                wheight: 450,
+                wwidth: 350,
+                pheight: 520,
+                pwidth: 500,
+                leaveamessage: true,
+                check_messages: false
+              };
+              (function () {
+                var po = document.createElement('script');
+                po.type = 'text/javascript';
+                po.setAttribute('crossorigin', 'anonymous');
+                po.async = true;
+                var date = new Date();
+                po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ('' + date.getFullYear() + date.getMonth() + date.getDate());
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(po, s);
+              })();
+            `
+          }}
+        />
       </Head>
 
       <main id="skip" className="bg-white dark:bg-neutral-900">
