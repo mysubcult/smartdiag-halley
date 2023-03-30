8097149e5b1fb513b3fda63464f67c7e64ae9970
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 00:54:54 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,10 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Prefooter from "./Prefooter";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-const Layout = (props: LayoutProps) => {
-  const { children, ...customMeta } = props;
+const Layout = ({ children, ...customMeta }) => {
   const router = useRouter();
   const meta = {
     title: "SmartDiag",
@@ -24,18 +18,39 @@ const Layout = (props: LayoutProps) => {
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
         <script
-          id="livehelperchat"
+          id="LHC_API"
           dangerouslySetInnerHTML={{
             __html: `
-var LHC_API = LHC_API||{};
-LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
-(function() {
-var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-          `}
-        />
+            var LHC_API = LHC_API || {};
+            LHC_API.args = {
+              mode: "widget",
+              lhc_base_url: "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/",
+              wheight: 450,
+              wwidth: 350,
+              pheight: 520,
+              pwidth: 500,
+              domain: "смартдиаг.рф",
+              leaveamessage: true,
+              department: ["1"],
+              theme: "1",
+              check_messages: false,
+              lang: "rus/",
+            };
+            (function () {
+              var po = document.createElement("script");
+              po.type = "text/javascript";
+              po.setAttribute("crossorigin", "anonymous");
+              po.async = true;
+              var date = new Date();
+              po.src =
+                "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?" +
+                ("" + date.getFullYear() + date.getMonth() + date.getDate());
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(po, s);
+            })();
+          `,
+          }}
+        ></script>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <link href="/favicon.ico" rel="shortcut icon" />
