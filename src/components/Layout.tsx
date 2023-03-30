ffdd2ba6dc0714dfbdb8b34a414d3907f519da6f
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 00:45:17 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,60 +1,70 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { ReactNode } from 'react';
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Prefooter from "./Prefooter";
+import Script from "next/script";
 
-interface LayoutProps {
-  children: ReactNode;
-  title?: string;
-  description?: string;
-  image?: string;
-  type?: string;
-}
-
-const Layout = ({ children, title = "SmartDiag", description = "Ваш проводник в мире автодиагностики", image = "/images/seo/halley-banner.png", type = "website" }: LayoutProps) => {
+const Layout = (props: any) => {
+  const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title,
-    description,
-    image,
-    type,
+    title: "SmartDiag",
+    description: `Ваш проводник в мире автодиагностики`,
+    image: "/images/seo/halley-banner.png",
+    type: "website",
+    ...customMeta,
   };
 
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `var LHC_API = LHC_API||{};
-            LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
-            (function() {
-            var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-            var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-            })();`
-          }}
-        />
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
-        <link href="/favicon.ico" rel="shortcut icon" />
-        <meta content={meta.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://halley.vercel.app${router.asPath}`}
-        />
-        <meta property="og:type" content={meta.type} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:url" content={`https://смартдиаг.рф${router.asPath}`} />
+        <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
       </Head>
 
+      <Script strategy="beforeInteractive">
+        {`
+          var LHC_API = LHC_API||{};
+          LHC_API.args = {
+            mode:'widget',
+            lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
+            wheight:450,
+            wwidth:350,
+            pheight:520,
+            pwidth:500,
+            domain:'смартдиаг.рф',
+            leaveamessage:true,
+            department:["1"],
+            theme:"1",
+            check_messages:false,
+            lang:'rus/'
+          };
+          (function() {
+            var po = document.createElement('script');
+            po.type = 'text/javascript';
+            po.setAttribute('crossorigin','anonymous');
+            po.async = true;
+            var date = new Date();
+            po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(po, s);
+          })();
+        `}
+      </Script>
+
       <main id="skip" className="bg-white dark:bg-neutral-900">
         <Navbar />
         {children}
-        <Prefooter />
+        <Prefooter />   
         <Footer />
       </main>
     </div>
