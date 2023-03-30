8d3c9f90a6e23c38ba437be695e129a95a92a36f
Artyom Gorin <mysubcult1@gmail.com>
2023-03-30 23:50:04 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,8 +18,7 @@ const Layout = (props: any) => {
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
-<script> var LHC_API = LHC_API || {}; LHC_API.args = { mode: 'widget', lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/', wheight: 450, wwidth: 350, pheight: 520, pwidth: 500, domain: 'смартдиаг.рф', leaveamessage: true, department: ["1"], theme: "1", check_messages: false, lang: 'rus/' }; (function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin', 'anonymous'); po.async = true; var date = new Date(); po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate() + Math.random()); var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); </script>
-        <title>{meta.title}</title>
+   <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <link href="/favicon.ico" rel="shortcut icon" />
         <meta content={meta.description} name="description" />
