5abe4bfddd4b6935b9a9232ebd1f0d7a502be09d
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 00:33:40 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Prefooter from "./Prefooter";
 
-const Layout = ({ children, ...customMeta }) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = (props: LayoutProps) => {
+  const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
     title: "SmartDiag",
@@ -17,23 +23,45 @@ const Layout = ({ children, ...customMeta }) => {
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `var LHC_API = LHC_API||{};
-            LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
-            (function() {
-            var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-            var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-            })();`,
-          }}
-        />
+        <script>
+          var LHC_API = LHC_API || {};
+          LHC_API.args = {
+            mode: "widget",
+            lhc_base_url:
+              "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/",
+            wheight: 450,
+            wwidth: 350,
+            pheight: 520,
+            pwidth: 500,
+            domain: "смартдиаг.рф",
+            leaveamessage: true,
+            department: ["1"],
+            theme: "1",
+            check_messages: false,
+            lang: "rus/",
+          };
+          (function () {
+            var po = document.createElement("script");
+            po.type = "text/javascript";
+            po.setAttribute("crossorigin", "anonymous");
+            po.async = true;
+            var date = new Date();
+            po.src =
+              "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?" +
+              ("" + date.getFullYear() + date.getMonth() + date.getDate());
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(po, s);
+          })();
+        </script>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:url" content={`https://halley.vercel.app${router.asPath}`} />
-        <meta property="og:type" content={meta.type} />
+        <link href="/favicon.ico" rel="shortcut icon" />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://halley.vercel.app${router.asPath}`}
+        />
+        <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
@@ -43,7 +71,7 @@ const Layout = ({ children, ...customMeta }) => {
       <main id="skip" className="bg-white dark:bg-neutral-900">
         <Navbar />
         {children}
-        <Prefooter />   
+        <Prefooter />
         <Footer />
       </main>
     </div>
