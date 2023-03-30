3f421245a50477517b3fafbcf8a2798465c9c539
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 02:08:14 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,12 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Prefooter from "./Prefooter";
 
+declare global {
+  interface Window {
+    LHC_API: any;
+  }
+}
+
 const Layout = (props: any) => {
   const { children, ...customMeta } = props;
   const router = useRouter();
@@ -18,35 +24,17 @@ const Layout = (props: any) => {
   };
 
   useEffect(() => {
-    window.onload = function () {
-      var LHC_API = LHC_API || {};
-      LHC_API.args = {
-        mode: "widget",
-        lhc_base_url: "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/",
-        wheight: 450,
-        wwidth: 350,
-        pheight: 520,
-        pwidth: 500,
-        domain: "смартдиаг.рф",
-        leaveamessage: true,
-        department: ["1"],
-        theme: "1",
-        check_messages: false,
-        lang: "rus/",
-      };
-      (function () {
-        var po = document.createElement("script");
-        po.type = "text/javascript";
-        po.setAttribute("crossorigin", "anonymous");
-        po.async = true;
-        var date = new Date();
-        po.src =
-          "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?" +
-          ("" + date.getFullYear() + date.getMonth() + date.getDate());
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(po, s);
-      })();
-    };
+    const script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.innerHTML = `var LHC_API = LHC_API||{};
+    LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+      var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();`;
+
+    document.body.appendChild(script);
   }, []);
 
   return (
