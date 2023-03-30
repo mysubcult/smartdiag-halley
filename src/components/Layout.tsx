ce4f5b057e154c764b211f2ce6b1d7456860dd09
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 00:38:11 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,58 +1,41 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Prefooter from "./Prefooter";
 
 interface LayoutProps {
   children: ReactNode;
+  title?: string;
+  description?: string;
+  image?: string;
+  type?: string;
 }
 
-const Layout = (props: LayoutProps) => {
-  const { children, ...customMeta } = props;
+const Layout = ({ children, title = "SmartDiag", description = "Ваш проводник в мире автодиагностики", image = "/images/seo/halley-banner.png", type = "website" }: LayoutProps) => {
   const router = useRouter();
   const meta = {
-    title: "SmartDiag",
-    description: `Ваш проводник в мире автодиагностики`,
-    image: "/images/seo/halley-banner.png",
-    type: "website",
-    ...customMeta,
+    title,
+    description,
+    image,
+    type,
   };
 
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
-        <script>
-          var LHC_API = LHC_API || {};
-          LHC_API.args = {
-            mode: "widget",
-            lhc_base_url:
-              "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/",
-            wheight: 450,
-            wwidth: 350,
-            pheight: 520,
-            pwidth: 500,
-            domain: "смартдиаг.рф",
-            leaveamessage: true,
-            department: ["1"],
-            theme: "1",
-            check_messages: false,
-            lang: "rus/",
-          };
-          (function () {
-            var po = document.createElement("script");
-            po.type = "text/javascript";
-            po.setAttribute("crossorigin", "anonymous");
-            po.async = true;
-            var date = new Date();
-            po.src =
-              "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?" +
-              ("" + date.getFullYear() + date.getMonth() + date.getDate());
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(po, s);
-          })();
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var LHC_API = LHC_API||{};
+            LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
+            (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+            var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+            })();`
+          }}
+        />
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <link href="/favicon.ico" rel="shortcut icon" />
@@ -61,7 +44,7 @@ const Layout = (props: LayoutProps) => {
           property="og:url"
           content={`https://halley.vercel.app${router.asPath}`}
         />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
