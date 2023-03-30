7419714ec86bf2c2cbd465f76ec505bd351d5ead
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 00:57:34 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,53 +4,34 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Prefooter from "./Prefooter";
 
-const Layout = ({ children, ...customMeta }) => {
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
   const router = useRouter();
+
   const meta = {
     title: "SmartDiag",
     description: `Ваш проводник в мире автодиагностики`,
     image: "/images/seo/halley-banner.png",
     type: "website",
-    ...customMeta,
   };
 
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
         <script
-          id="LHC_API"
+          id="livehelperchat"
           dangerouslySetInnerHTML={{
-            __html: `
-            var LHC_API = LHC_API || {};
-            LHC_API.args = {
-              mode: "widget",
-              lhc_base_url: "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/",
-              wheight: 450,
-              wwidth: 350,
-              pheight: 520,
-              pwidth: 500,
-              domain: "смартдиаг.рф",
-              leaveamessage: true,
-              department: ["1"],
-              theme: "1",
-              check_messages: false,
-              lang: "rus/",
-            };
-            (function () {
-              var po = document.createElement("script");
-              po.type = "text/javascript";
-              po.setAttribute("crossorigin", "anonymous");
-              po.async = true;
-              var date = new Date();
-              po.src =
-                "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?" +
-                ("" + date.getFullYear() + date.getMonth() + date.getDate());
-              var s = document.getElementsByTagName("script")[0];
-              s.parentNode.insertBefore(po, s);
-            })();
-          `,
+            __html: `var LHC_API = LHC_API||{};LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
+            (function() {
+              var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+              var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+            })();`,
           }}
-        ></script>
+        />
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <link href="/favicon.ico" rel="shortcut icon" />
