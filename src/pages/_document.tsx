987992d457a0b72141218c6dcc9f1d182785d8d3
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 21:00:37 +0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -6,14 +6,33 @@ export default function Document() {
       <Head>
         <script
           dangerouslySetInnerHTML={{
-            __html: `var LHC_API = LHC_API||{};
-              LHC_API.args = {mode:'widget',lhc_base_url:'//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,leaveamessage:true,check_messages:false};
+            __html: `
+              var LHC_API = LHC_API || {};
+              LHC_API.args = {
+                mode: 'widget',
+                lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
+                wheight: 450,
+                wwidth: 350,
+                pheight: 520,
+                pwidth: 500,
+                domain: 'смартдиаг.рф',
+                leaveamessage: true,
+                department: ['1'],
+                theme: '1',
+                check_messages: false,
+                lang: 'rus/'
+              };
               (function() {
-                var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-                var date = new Date();po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+                var po = document.createElement('script');
+                po.type = 'text/javascript';
+                po.setAttribute('crossorigin', 'anonymous');
+                po.async = true;
+                var date = new Date();
+                po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ('' + date.getFullYear() + date.getMonth() + date.getDate());
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(po, s);
               })();
-            `,
+            `
           }}
         />
       </Head>
@@ -22,5 +41,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
