aa6e2ae9a06264a3c194729f14f72e4715c7e68e
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 20:48:39 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,40 +13,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} font-sans`}>
       <ThemeProvider attribute="class">
-        <Head>
-          <script
-            data-iframe="false"
-            dangerouslySetInnerHTML={{
-              __html: `
-                var LHC_API = LHC_API||{};
-                LHC_API.args = {
-                  mode:'widget',
-                  lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
-                  wheight:450,
-                  wwidth:350,
-                  pheight:520,
-                  pwidth:500,
-                  domain:'смартдиаг.рф',
-                  leaveamessage:true,
-                  department:["1"],
-                  theme:"1",
-                  check_messages:false,
-                  lang:'rus/'
-                };
-                (function() {
-                  var po = document.createElement('script');
-                  po.type = 'text/javascript';
-                  po.setAttribute('crossorigin','anonymous');
-                  po.async = true;
-                  var date = new Date();
-                  po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-                  var s = document.getElementsByTagName('script')[0];
-                  s.parentNode.insertBefore(po, s);
-                })();
-              `,
-            }}
-          />
-        </Head>
         <Component {...pageProps} />
         <Analytics />
       </ThemeProvider>
