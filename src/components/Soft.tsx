63dd678ff00ec81682665c8307e90635d5b20f2f
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 15:28:52 +0400

    Update Soft.tsx

--- src/components/Soft.tsx
+++ src/components/Soft.tsx
@@ -185,8 +185,8 @@ const products = [
 type BillingInterval = "year" | "month" | "elm";
 
 export default function Soft() {
-  const [billingInterval, setBillingInterval] = useState<BillingInterval>("month");
-
+  const [billingInterval, setBillingInterval] =
+    useState<BillingInterval>("month");
   return (
     <div className="bg-gray-50 dark:bg-neutral-900" id="soft">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16">
@@ -197,44 +197,43 @@ export default function Soft() {
         </p>
       </div>
 
-      <div className="mx-auto px-4 sm:px-6 lg:px-8 flex justify-center mt-6 sm:mt-8">
-        <button
-          onClick={() => setBillingInterval("month")}
-          type="button"
-          className={`${
-            billingInterval === "month"
-              ? "bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
-              : "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-400"
-          } rounded-md m-1 py-2 px-6 whitespace-nowrap`}
-        >
-          Мультимарочные
-        </button>
-        <button
-          onClick={() => setBillingInterval("year")}
-          type="button"
-          className={`${
-            billingInterval === "year"
-              ? "bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
-              : "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-400"
-          } rounded-md m-1 py-2 px-6 whitespace-nowrap`}
-        >
-          Марочные
-        </button>
-        <button
-          onClick={() => setBillingInterval("elm")}
-          type="button"
-          className={`${
-            billingInterval === "elm"
-              ? "bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
-              : "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-400"
-          } rounded-md m-1 py-2 px-6 whitespace-nowrap`}
-        >
-          Приложения ELM
-        </button>
+<div className="mx-auto px-4 sm:px-6 lg:px-8 sm:flex sm:flex-col sm:align-center">
+        <div className="relative self-center text-base font-semibold mt-6 bg-neutral-200  dark:bg-neutral-800 rounded-lg flex sm:mt-8">
+          <button
+            onClick={() => setBillingInterval("month")}
+            type="button"
+            className={`${
+              billingInterval === "month"
+                ? "relative w-1/2 bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
+                : "ml-0.5 relative w-1/2 text-neutral-900 dark:text-neutral-400"
+            } rounded-md m-1 py-2 whitespace-nowrap sm:w-auto sm:px-8`}
+          >
+            Мультимарочные
+          </button>
+          <button
+            onClick={() => setBillingInterval("year")}
+            type="button"
+            className={`${
+              billingInterval === "year"
+                ? "relative w-1/2 bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
+                : "ml-0.5 relative w-1/2 text-neutral-900 dark:text-neutral-400"
+            } rounded-md m-1 py-2 whitespace-nowrap sm:w-auto sm:px-8`}
+          >
+            Марочные
+          </button>
+          <button
+            onClick={() => setBillingInterval("elm")}
+            type="button"
+            className={`${
+              billingInterval === "elm"
+                ? "relative w-1/2 bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
+                : "ml-0.5 relative w-1/2 text-neutral-900 dark:text-neutral-400"
+            } rounded-md m-1 py-2 whitespace-nowrap sm:w-auto sm:px-8`}
+          >
+            Приложения ELM
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
 
 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-16">
         {products.map(
