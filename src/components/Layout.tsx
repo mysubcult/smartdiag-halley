329b98a86ad6a17e9966498d35f80140e74b3771
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 08:13:00 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -10,27 +9,12 @@ const Layout = (props: any) => {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title: "SmartDiag",
-    description: "Ваш проводник в мире автодиагностики",
+    title: "Halley",
+    description: `Halley is a landing page template built with Nextjs and Tailwindcss`,
     image: "/images/seo/halley-banner.png",
     type: "website",
     ...customMeta,
   };
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js";
-    script.async = true;
-    script.setAttribute("crossorigin", "anonymous");
-    script.onload = () => console.log("Script loaded!");
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
@@ -42,7 +26,7 @@ const Layout = (props: any) => {
           property="og:url"
           content={`https://halley.vercel.app${router.asPath}`}
         />
-        <meta property="og:type" content="website" />
+          <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
