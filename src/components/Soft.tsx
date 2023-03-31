7f8004904f6080637e0128dfeb9621a5d4bf30d2
Artyom Gorin <mysubcult1@gmail.com>
2023-03-31 18:16:52 +0400

    Update Soft.tsx

--- src/components/Soft.tsx
+++ src/components/Soft.tsx
@@ -235,81 +235,77 @@ export default function Soft() {
         </div>
       </div>
 
-import { useState } from "react";
+<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-16">
+        {products.map(
+          ({
+            title,
+            mostPopular,
+            description,
+            currency,
+            price,
+            frecuency,
+            cta,
+            features,
+            href,
+          }) => {
+            return (
+              <>
+                {billingInterval === frecuency ? (
+                  <div
+                    key={title}
+                    className={`rounded-lg py-8 relative flex flex-col ${
+                      mostPopular
+                        ? "border-red-300 border-2 border-solid dark:border-red-600"
+                        : "border-neutral-300 border dark:border-neutral-600"
+                    }`}
+                  >
+                    <h3 className="px-6 text-lg font-semibold leading-5">
+                      {title}
+                    </h3>
+                    {mostPopular && (
+                      <p className="mx-6 absolute top-0 px-4 py-1 -translate-y-1/2 bg-red-100 text-red-600  rounded-full text-sm font-semibold tracking-wide shadow-md">
+                        Популярное
+                      </p>
+                    )}
 
-function ProductList({ products, billingInterval }) {
-  const [showMore, setShowMore] = useState(false);
+                    <p className="px-6 mt-4 leading-6 dark:text-neutral-400">
+                      {description}
+                    </p>
 
-  const visibleProducts = showMore ? products : products.slice(0, 4);
+                    {/* Call to action */}
+                    <Link
+                      href={href}
+                      target="_blank"
+                      className={`mt-4 mx-6 block px-6 py-3 font-medium leading-4 text-center rounded-lg ${
+                        mostPopular
+                          ? "bg-red-600 text-white shadow-md"
+                          : "bg-black text-white dark:bg-white dark:text-black"
+                      }`}
+                    >
+                      {cta}
+                    </Link>
 
-  const handleShowMoreClick = () => {
-    setShowMore(true);
-  };
-
-  return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-16">
-      {visibleProducts.map(
-        ({
-          title,
-          mostPopular,
-          description,
-          currency,
-          price,
-          frecuency,
-          cta,
-          features,
-          href,
-        }) => {
-          return (
-            <div
-              key={title}
-              className={`rounded-lg py-8 relative flex flex-col ${
-                mostPopular
-                  ? "border-red-300 border-2 border-solid dark:border-red-600"
-                  : "border-neutral-300 border dark:border-neutral-600"
-              }`}
-            >
-              <h3 className="px-6 text-lg font-semibold leading-5">{title}</h3>
-              {mostPopular && (
-                <p className="mx-6 absolute top-0 px-4 py-1 -translate-y-1/2 bg-red-100 text-red-600  rounded-full text-sm font-semibold tracking-wide shadow-md">
-                  Популярное
-                </p>
-              )}
-
-              <p className="px-6 mt-4 leading-6 dark:text-neutral-400">{description}</p>
-
-              {/* Call to action */}
-              <Link
-                href={href}
-                target="_blank"
-                className={`mt-4 mx-6 block px-6 py-3 font-medium leading-4 text-center rounded-lg ${
-                  mostPopular
-                    ? "bg-red-600 text-white shadow-md"
-                    : "bg-black text-white dark:bg-white dark:text-black"
-                }`}
-              >
-                {cta}
-              </Link>
-
-              {/* features */}
-              <ul className="mt-6 px-6 space-y-4 flex-1 border-t border-neutral-300 dark:border-neutral-500">
-                <p className="mt-6 font-semibold dark:text-neutral-300">Программы:</p>
-                {features.map((features) => (
-                  <li key={features} className="leading-6 flex">
-                    <CheckIcon className="mt-2 w-3 h-3 text-red-600 shrink-0" />
-                    <span className="ml-3 dark:text-neutral-400">{features}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        }
-      )}
-      {products.length > 4 && !showMore && (
-        <button className="mx-auto mt-8 bg-white text-black py-2 px-4 rounded-lg shadow-md" onClick={handleShowMoreClick}>
-          Загрузить еще
-        </button>
-      )}
-    </div>
+                    {/* features */}
+                    <ul className="mt-6 px-6 space-y-4 flex-1 border-t border-neutral-300 dark:border-neutral-500">
+                      <p className="mt-6 font-semibold dark:text-neutral-300">
+                        Программы:
+                      </p>
+                      {features.map((features) => (
+                        <li key={features} className="leading-6 flex">
+                          <CheckIcon className="mt-2 w-3 h-3 text-red-600 shrink-0" />
+                          <span className="ml-3 dark:text-neutral-400">
+                            {features}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </>
+            );
+          }
+        )}
+      </div>
+    </div> 
   );
 }
