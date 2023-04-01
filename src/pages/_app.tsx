77317fa7fb6f90f451efa19d0f5662046760b595
Artyom Gorin <mysubcult1@gmail.com>
2023-04-01 15:46:33 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,27 +10,12 @@ const inter = Inter({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const script = body.createElement("script");
-    script.src =
-      "//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/rus/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1/(theme)/1";
-    script.async = true;
-    const referrer = encodeURIComponent(
-      document.referrer.substr(document.referrer.indexOf("://") + 1)
-    );
-    const location = encodeURIComponent(
-      window.location.href.substring(window.location.protocol.length)
-    );
-    script.src += `?r=${referrer}&l=${location}`;
-    document.body.appendChild(script);
-  }, []);
-
   return (
-    <ThemeProvider attribute="class">
-      <div className={`${inter.variable} font-sans`}>
+    <main className={`${inter.variable} font-sans`}>
+      <ThemeProvider attribute="class">
         <Component {...pageProps} />
         <Analytics />
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </main>
   );
 }
