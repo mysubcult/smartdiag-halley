60132e78c1f1b942292aa4e4a2fc4b2f22f3f3e8
Artyom Gorin <mysubcult1@gmail.com>
2023-04-01 16:18:56 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from "../components/Layout";
 import Faq from "../components/Faq";
 import { Services } from "../components/Services";
 import Customers from "../components/Customers";
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Faq />
       <Services />
       <Contact />
+      <Script src="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/rus/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1/(theme)/1" />
     </Layout>
   );
 }
