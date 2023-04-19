59c409b362c601c5d414e3882a9f0626649bc8bd
Artyom Gorin <mysubcult1@gmail.com>
2023-04-19 13:58:31 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export function Hero() {
           <div className="flex flex-auto pt-10 gap-2 min-w-[350px] justify-center sm:justify-start">
             <div>
               <Link href="#soft">
-                <button className="bg-black dark:bg-white text-white dark:text-black text-base rounded-full px-4 p-2 font-medium hover:bg-gray-800 hover:text-white transition-all">
+                <button className="bg-black dark:bg-white text-white dark:text-black text-base rounded-full px-4 p-2 font-medium hover:bg-gray-300 hover:text-white transition-all">
                   Программы для приборов
                 </button>
               </Link>
