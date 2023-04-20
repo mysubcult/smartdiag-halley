0e3a7d7b9aacce26790dd33853378141a1c63386
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 01:22:37 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,22 +11,11 @@ const inter = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <script>
-        {`var LHC_API = LHC_API||{};
-        LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',leaveamessage:true,department:["1"],theme:"1",check_messages:false,lang:'rus/'};
-        (function() {
-        var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-        var date = new Date();po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-        })();`}
-      </script>
-      <main className={`${inter.variable} font-sans`}>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-          <Analytics />
-        </ThemeProvider>
-      </main>
-    </>
+    <main className={`${inter.variable} font-sans`}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+        <Analytics />
+      </ThemeProvider>
+    </main>
   );
 }
