aca2d41508c2fd1827ab7adfece56e1a74c674ad
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 01:40:59 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -16,18 +15,6 @@ const Layout = (props: any) => {
     type: "website",
     ...customMeta,
   };
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.setAttribute('crossorigin','anonymous');
-    script.async = true;
-    const date = new Date();
-    script.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-    const s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(script, s);
-  }, []);
-
   return (
     <div className="min-w-[350px] overflow-x-hidden">
       <Head>
@@ -39,7 +26,7 @@ const Layout = (props: any) => {
           property="og:url"
           content={`https://xn--80aajcuv3afm.xn--p1ai${router.asPath}`}
         />
-        <meta property="og:type" content="website" />
+          <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
