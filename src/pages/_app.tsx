0e3827d665427ca2b4f15db054839a88b58af775
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 02:02:12 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,8 +14,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
         <Analytics />
+      <Script src="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/rus/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1/(theme)/1" />
       </ThemeProvider>
-    <Script src="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/rus/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1/(theme)/1" />
     </main>
   );
 }
