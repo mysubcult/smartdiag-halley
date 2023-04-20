6fce68611b09a561ffb82a9a3e72e4e98ee45f13
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 01:53:01 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         src="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js"
         onLoad={() => {
           var LHC_API: any = LHC_API||{};
-          LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',department:["1"],theme:"1",check_messages:false,lang:'rus/'};
+          LHC_API.args = {mode:'widget',lhc_base_url:'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'смартдиаг.рф',department:["1"],theme:"5",check_messages:false,lang:'rus/'};
           LHC_API.init();
         }}
       />
