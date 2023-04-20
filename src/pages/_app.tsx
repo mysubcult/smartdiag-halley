9146e35ff3f894c60e33c2fad1c78b3718861b7e
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 01:57:16 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import Script from 'next/script'
-import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
@@ -8,7 +6,12 @@ import Script from 'next/script'
 
 const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
+});
 export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <ThemeProvider attribute="class">
         <Component {...pageProps} />
         <Analytics />
       </ThemeProvider>
