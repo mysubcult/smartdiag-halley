cbf561c0a316003dc5f8c86d178b10ec1fda823c
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 01:23:52 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 import Faq from "../components/Faq";
 import { Services } from "../components/Services";
 import Customers from "../components/Customers";
-import Script from 'next/script'
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -15,6 +15,22 @@ export default function Home() {
       <Faq />
       <Services />
       <Contact />
- </Layout>
+      <Script
+        src="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js"
+        strategy="lazyOnload"
+        data-mode="widget"
+        data-lhc_base_url="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/"
+        data-wheight="450"
+        data-wwidth="350"
+        data-pheight="520"
+        data-pwidth="500"
+        data-domain="смартдиаг.рф"
+        data-leaveamessage="true"
+        data-department="1"
+        data-theme="1"
+        data-check_messages="false"
+        data-lang="rus/"
+      />
+    </Layout>
   );
 }
