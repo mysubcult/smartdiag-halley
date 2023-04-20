f1106d46fe63c5b6e5332bf0306174bcd3c8f67b
Artyom Gorin <mysubcult1@gmail.com>
2023-04-21 02:04:28 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
+import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-import Script from 'next/script'
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} font-sans`}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
         <Analytics />
-      <Script src="https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/rus/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1/(theme)/1" />
       </ThemeProvider>
     </main>
   );
