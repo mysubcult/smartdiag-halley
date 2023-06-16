301283c1e08b280671cca626a8c28b5109803271
Artyom Gorin <mysubcult1@gmail.com>
2023-06-17 01:27:34 +0400

    Update Soft.tsx

--- src/components/Soft.tsx
+++ src/components/Soft.tsx
@@ -211,7 +211,7 @@ export default function Soft() {
         </p>
       </div>
 
-<div className="max-w-max mx-auto px-16 sm:px-12">
+<div className="max-w-max mx-auto px-8 sm:px-8">
   <div className="relative text-base font-semibold mt-6 bg-neutral-200 dark:bg-neutral-800 rounded-lg inline-flex flex-wrap justify-center sm:mt-8">
     <button
       onClick={() => setBillingInterval("month")}
