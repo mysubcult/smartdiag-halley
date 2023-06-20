80350c91cfc343a4f27b9d46b2f44a243e4beae9
Artyom Gorin <mysubcult1@gmail.com>
2023-06-20 21:10:48 +0400

    Update Faq.tsx

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,31 +1,22 @@
 import React, { useState } from "react";
 import { Disclosure } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
 
 const newsItems = [
   {
     title: "Заголовок новости 1",
-    date: "15 июня 2023",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a metus sed ligula cursus eleifend. Nullam tristique ex ante, vitae rhoncus risus commodo non.",
+    content: "Содержание новости 1",
   },
   {
     title: "Заголовок новости 2",
-    date: "10 июня 2023",
-    content:
-      "Nullam cursus enim at elit euismod, a dapibus arcu hendrerit. Donec laoreet mi et interdum placerat. Aliquam posuere quam ut ligula vehicula fringilla.",
+    content: "Содержание новости 2",
   },
   {
     title: "Заголовок новости 3",
-    date: "5 июня 2023",
-    content:
-      "Etiam in tortor sit amet metus bibendum cursus. Nullam lacinia tellus sed maximus pellentesque. Suspendisse potenti. Maecenas venenatis urna sit amet tristique congue.",
+    content: "Содержание новости 3",
   },
   {
     title: "Заголовок новости 4",
-    date: "1 июня 2023",
-    content:
-      "Praesent sed mauris eu lorem auctor feugiat. Sed cursus ligula eget mauris mollis hendrerit. Ut interdum risus et aliquam consectetur.",
+    content: "Содержание новости 4",
   },
 ];
 
@@ -37,91 +28,56 @@ export default function News() {
     setShowAllNews((prevState) => !prevState);
   };
 
+  const displayedNews = showAllNews ? newsItems : newsItems.slice(0, initialNewsCount);
+
   return (
-    <div className="bg-white dark:bg-neutral-900 px-4 py-16">
-      <div className="max-w-3xl mx-auto">
-        <h2 className="text-4xl font-bold text-center mb-8">
-          Новости и события 📰
-        </h2>
-        {newsItems.slice(0, initialNewsCount).map((item, index) => (
-          <Disclosure key={index}>
-            {({ open }) => (
-              <div className="mb-4">
-                <Disclosure.Button
-                  className={`${
-                    open
-                      ? "bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-100"
-                      : ""
-                  } flex justify-between items-center w-full py-4 px-6 rounded-lg bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-800 focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-50`}
-                >
-                  <div>
-                    <h3 className="text-lg font-semibold">{item.title}</h3>
-                    <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                      {item.date}
-                    </p>
-                  </div>
-                  <ChevronDownIcon
-                    className={`${
-                      open ? "transform rotate-180" : ""
-                    } h-5 w-5 flex-shrink-0 text-neutral-500 dark:text-neutral-400`}
-                  />
-                </Disclosure.Button>
-                <Disclosure.Panel
-                  className={`${
-                    open ? "px-6 py-4 text-base" : "hidden"
-                  } bg-neutral-100 dark:bg-neutral-800 rounded-lg mt-2`}
-                >
-                  <p>{item.content}</p>
-                </Disclosure.Panel>
-              </div>
-            )}
-          </Disclosure>
-        ))}
-        {!showAllNews && newsItems.length > initialNewsCount && (
-          <div className="text-center mt-4">
-            <button
-              onClick={toggleShowAllNews}
-              className="bg-primary-500 hover:bg-primary-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-50"
-            >
-              Читать далее
-            </button>
-          </div>
-        )}
-        {showAllNews &&
-          newsItems.slice(initialNewsCount).map((item, index) => (
-            <Disclosure key={index} as="div" className="mb-4">
+    <div className="bg-white dark:bg-neutral-900 w-full px-4 pt-16 pb-16" id="news">
+      <h2 className="text-4xl font-bold text-center">Новости 📰</h2>
+
+      <div className="mx-auto w-full max-w-2xl rounded-2xl bg-transparent dark:bg-transparent">
+        {displayedNews.map((news, index) => (
+          <div key={index}>
+            <Disclosure>
               {({ open }) => (
-                <div>
+                <div className="mt-4">
                   <Disclosure.Button
                     className={`${
                       open
-                        ? "bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-100"
+                        ? "bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-100 rounded-b-none"
                         : ""
-                    } flex justify-between items-center w-full py-4 px-6 rounded-lg bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-800 focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-50`}
+                    } flex w-full justify-between rounded-lg bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-800 px-4 py-4 text-left text-base font-medium `}
                   >
-                    <div>
-                      <h3 className="text-lg font-semibold">{item.title}</h3>
-                      <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                        {item.date}
-                      </p>
-                    </div>
-                    <ChevronDownIcon
+                    <h2 className="font-semibold">{news.title}</h2>
+                    <span
                       className={`${
                         open ? "transform rotate-180" : ""
                       } h-5 w-5 flex-shrink-0 text-neutral-500 dark:text-neutral-400`}
-                    />
+                    >
+                      {open ? "▲" : "▼"}
+                    </span>
                   </Disclosure.Button>
                   <Disclosure.Panel
                     className={`${
-                      open ? "px-6 py-4 text-base" : "hidden"
-                    } bg-neutral-100 dark:bg-neutral-800 rounded-lg mt-2`}
+                      open
+                        ? "px-4 pt-4 pb-2 text-base dark:text-neutral-400"
+                        : ""
+                    } bg-neutral-100 dark:bg-neutral-800 rounded-b-lg`}
                   >
-                    <p>{item.content}</p>
+                    <p>{news.content}</p>
                   </Disclosure.Panel>
                 </div>
               )}
             </Disclosure>
-          ))}
+          </div>
+        ))}
+        {!showAllNews && (
+          <button
+            className="block mx-auto mt-4 bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-lg px-4 py-2 text-base font-medium text-neutral-500 dark:text-neutral-400"
+            onClick={toggleShowAllNews}
+          >
+            Читать далее
+          </button>
+        )}
       </div>
     </div>
   );
