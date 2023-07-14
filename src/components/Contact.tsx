89fd860e52d856121ef0471379c9b5ee314d3890
Artyom Gorin <mysubcult1@gmail.com>
2023-07-14 14:26:54 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -79,8 +79,8 @@ export default function Contact() {
 <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
   <EnvelopeIcon className="w-5 h-5" />
   <a href="https://t.me/smartdiag_robot" target="_blank" className="hover:text-blue-500">Telegram</a>
-  <a href="https://t.me/smartdiag_robot" target="_blank">
-    <div className="flex flex-col items-center mt-2 space-y-2 text-dark-600 dark:text-neutral-400">
+  </div>
+      <div className="flex flex-col items-center mt-2 space-y-2 text-dark-600 dark:text-neutral-400"><a href="https://t.me/smartdiag_robot" target="_blank">
       <img src="https://api.qrserver.com/v1/create-qr-code/?data=https://t.me/smartdiag_robot&amp;size=70x70&amp;color=007BFF" alt="QR Code" />
     </div>
   </a>
