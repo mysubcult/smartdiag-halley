b9e55ef63c8cdf737a51c08098a6abebf1ac8245
Artyom Gorin <mysubcult1@gmail.com>
2023-07-14 11:01:05 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -81,15 +81,18 @@ export default function Contact() {
 <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
   <EnvelopeIcon className="w-5 h-5" />
   <a href="https://t.me/smartdiag_robot" target="_blank" className="hover:text-blue-500">Telegram</a>
-  <img src="https://api.qrserver.com/v1/create-qr-code/?data=https://t.me/smartdiag_robot&amp;size=70x70&amp;color=007BFF" alt="QR Code" />
+  <a href="https://t.me/smartdiag_robot" target="_blank">
+    <img src="https://api.qrserver.com/v1/create-qr-code/?data=https://t.me/smartdiag_robot&amp;size=70x70&amp;color=007BFF" alt="QR Code" />
+  </a>
 </div>
 
 <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
   <PhoneIcon className="w-5 h-5" />
   <a href="https://wa.me/message/XVMV4LKBTXB4E1" target="_blank" className="hover:text-green-500">WhatsApp</a>
-  <img src="https://api.qrserver.com/v1/create-qr-code/?data=https://wa.me/message/XVMV4LKBTXB4E1&amp;size=70x70&amp;color=28A745" alt="QR Code" />
+  <a href="https://wa.me/message/XVMV4LKBTXB4E1" target="_blank">
+    <img src="https://api.qrserver.com/v1/create-qr-code/?data=https://wa.me/message/XVMV4LKBTXB4E1&amp;size=70x70&amp;color=28A745" alt="QR Code" />
+  </a>
 </div>
-
         </div>
 
         <div>
