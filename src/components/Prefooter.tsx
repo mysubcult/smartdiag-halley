e381e6bf7d8b00d8958ae6c5af4cb08a1a1296ea
Artyom Gorin <mysubcult1@gmail.com>
2023-07-20 13:45:49 +0400

    Update Prefooter.tsx

--- src/components/Prefooter.tsx
+++ src/components/Prefooter.tsx
@@ -9,8 +9,8 @@ export default function Prefooter() {
         </h1>
       </div>
 
-      <div className="text-center pb-20 flex flex-wrap justify-center">
-        <div className="flex flex-col mb-4">
+      <div className="text-center pb-20 flex flex-col items-center">
+        <div className="mb-4">
           <Link href="https://i.getspace.us/cloud/s/dzkdXFpscgCFsjH" target="_blank">
             <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
               GetSpace
@@ -18,7 +18,7 @@ export default function Prefooter() {
             </button>
           </Link>
         </div>
-        <div className="flex flex-col mb-4">
+        <div className="mb-4">
           <Link href="https://drive.filen.io/f/2a56c53b-d111-4237-927e-e888d41ca33f#SyzwqKMV3IGSLMDVPoQvnQ7vzhw7BtbV" target="_blank">
             <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
               Filen.io
@@ -26,7 +26,7 @@ export default function Prefooter() {
             </button>
           </Link>
         </div>
-        <div className="flex flex-col mb-4">
+        <div className="mb-4">
           <Link href="https://mega.nz/folder/N8BGBBRC#F4YYgKdUlFZmL2UGqfgK-Q" target="_blank">
             <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
               Mega.nz
