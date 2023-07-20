66501c9706388ce129c242b26329e4e55b48c81a
Artyom Gorin <mysubcult1@gmail.com>
2023-07-20 13:42:32 +0400

    Update Prefooter.tsx

--- src/components/Prefooter.tsx
+++ src/components/Prefooter.tsx
@@ -9,27 +9,31 @@ export default function Prefooter() {
         </h1>
       </div>
 
-      <div className="text-center pb-20">
-        <Link href="https://i.getspace.us/cloud/s/dzkdXFpscgCFsjH" target="_blank">
-          <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
-            GetSpace
-            <img src="https://getspace.us/wp-content/uploads/2020/02/FAVICON-GS-01.png" className="inline-block w-5 h-5 ml-2" />
-          </button>
-        </Link>
-        &nbsp;&nbsp;&nbsp;
-        <Link href="https://drive.filen.io/f/2a56c53b-d111-4237-927e-e888d41ca33f#SyzwqKMV3IGSLMDVPoQvnQ7vzhw7BtbV" target="_blank">
-          <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
-            Filen.io
-            <img src="https://filen.io/favicon.ico" className="inline-block w-5 h-5 ml-2" />
-          </button>
-        </Link>
-        &nbsp;&nbsp;&nbsp;
-        <Link href="https://mega.nz/folder/N8BGBBRC#F4YYgKdUlFZmL2UGqfgK-Q" target="_blank">
-          <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
-            Mega.nz
-            <img src="https://mega.nz/favicon.ico" className="inline-block w-5 h-5 ml-2" />
-          </button>
-        </Link>
+      <div className="text-center pb-20 flex flex-wrap justify-center">
+        <div className="flex flex-col mb-4">
+          <Link href="https://i.getspace.us/cloud/s/dzkdXFpscgCFsjH" target="_blank">
+            <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
+              GetSpace
+              <img src="https://getspace.us/wp-content/uploads/2020/02/FAVICON-GS-01.png" className="inline-block w-5 h-5 ml-2" />
+            </button>
+          </Link>
+        </div>
+        <div className="flex flex-col mb-4">
+          <Link href="https://drive.filen.io/f/2a56c53b-d111-4237-927e-e888d41ca33f#SyzwqKMV3IGSLMDVPoQvnQ7vzhw7BtbV" target="_blank">
+            <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
+              Filen.io
+              <img src="https://filen.io/favicon.ico" className="inline-block w-5 h-5 ml-2" />
+            </button>
+          </Link>
+        </div>
+        <div className="flex flex-col mb-4">
+          <Link href="https://mega.nz/folder/N8BGBBRC#F4YYgKdUlFZmL2UGqfgK-Q" target="_blank">
+            <button className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 p-3 font-medium transition duration-300 ease-in-out">
+              Mega.nz
+              <img src="https://mega.nz/favicon.ico" className="inline-block w-5 h-5 ml-2" />
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
