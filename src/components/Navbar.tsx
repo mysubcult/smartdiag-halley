9908ae24734fa88d7dcbdd905cd3d52a99827b4e
Artyom Gorin <mysubcult1@gmail.com>
2023-07-31 13:19:52 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,7 +71,7 @@ export default function Navbar() {
     target="_blank"
     rel="noopener noreferrer"
   >
-    <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 p-2 text-base font-medium rounded-full flex items-center gap-2" style={{ backgroundColor: '#005BFF' }}>
+    <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 p-2 text-base font-medium rounded-full flex items-center gap-2">
       OZON
     </button>
   </a>
@@ -80,7 +80,7 @@ export default function Navbar() {
     target="_blank"
     rel="noopener noreferrer"
   >
-    <button className="bg-orange-500 hover:bg-orange-600 text-white px-4 p-2 text-base font-medium rounded-full flex items-center gap-2" style={{ backgroundColor: '#fc0', hover: '#fc0' }}>
+    <button className="bg-orange-500 hover:bg-orange-600 text-white px-4 p-2 text-base font-medium rounded-full flex items-center gap-2">
       Яндекс Маркет
     </button>
   </a>
