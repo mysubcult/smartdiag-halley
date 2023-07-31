54f3f9b814b955c24d3fbce3ae7a981ca152de6d
Artyom Gorin <mysubcult1@gmail.com>
2023-07-31 13:33:41 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -84,7 +84,7 @@ export default function Navbar() {
     target="_blank"
     rel="noopener noreferrer"
   >
-    <button className="bg-yellow-400 hover:bg-yellow-500 text-black px-3 p-2 text-base font-medium rounded-full flex items-center gap-2">
+    <button className="bg-yellow-400 hover:bg-yellow-500 text-black px-3 p-1 text-base font-medium rounded-full flex items-center gap-2">
       <img src="https://yastatic.net/market-export/_/i/favicon/ymnew/favicon.ico" alt="Яндекс Маркет" className="w-4 h-4" />
       Яндекс Маркет
     </button>
