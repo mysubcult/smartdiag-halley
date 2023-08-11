9034431c8e5c8bdd71069ebc75bbfad43325b4df
Artyom Gorin <mysubcult1@gmail.com>
2023-08-11 11:20:43 +0400

    Add files via upload

---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,36 @@
+export const menus = [
+  {
+    title: "Nosotros",
+    url: "/#nosotros",
+  },
+  {
+    title: "Servicios",
+    url: "/#servicios",
+  },
+  {
+    title: "Precios",
+    url: "/#precios",
+  },
+  {
+    title: "App",
+    url: "/app",
+  },
+  {
+    title: "Tienda",
+    url: "/tienda",
+  },
+  // {
+  //   title: "Quina",
+  //   url: "",
+  //   submenus: [
+  //     {
+  //       title: "Nosotros",
+  //       url: "#nosotros",
+  //     },
+  //     {
+  //       title: "Blog",
+  //       url: "/",
+  //     },
+  //   ],
+  // },
+];
