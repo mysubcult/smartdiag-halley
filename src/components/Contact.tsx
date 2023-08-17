7812848e649247ecd4ebee7a8fa56af4d9b3131e
Artyom Gorin <mysubcult1@gmail.com>
2023-08-17 16:30:35 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -84,11 +84,13 @@ export default function Contact() {
   <div>
     <a href="https://t.me/smartdiag_robot" target="_blank">
      <img src="/images/hero/telegram-qr.svg"  className="transition-transform duration-300 ease-in-out transform hover:scale-110" width="120" height="120" alt="QR Code" />
-  </div>
+    </a>
+    </div>
      <div>
     <a href="https://t.me/smartdiag_robot" target="_blank">
      <img src="/images/hero/telegram-qr.svg"  className="transition-transform duration-300 ease-in-out transform hover:scale-110" width="120" height="120" alt="QR Code" />
-  </div> 
+    </a>
+    </div> 
 </div>
 
 <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
