990b71bb483c7c2643703362d08b72cafa4a4eac
Artyom Gorin <mysubcult1@gmail.com>
2023-11-03 21:05:12 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,32 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} font-sans`}>
       <Head>
-      <Script id="lhc-widget">
-        {`
-          var LHC_API = LHC_API || {};
-          LHC_API.args = {
-            mode: 'widget',
-            lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
-            wheight: 450,
-            wwidth: 350,
-            pheight: 520,
-            pwidth: 500,
-            department: ["1"],
-            check_messages: false,
-            lang: 'rus/'
-          };
-          (function() {
-            var po = document.createElement('script');
-            po.type = 'text/javascript';
-            po.setAttribute('crossorigin', 'anonymous');
-            po.async = true;
-            var date = new Date();
-            po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(po, s);
-          })();
-        `}
-      </Script>
       </Head>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
