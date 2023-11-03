7afae30be9a2700f07864bbdcac2d0c7b0fd887c
Artyom Gorin <mysubcult1@gmail.com>
2023-11-03 20:48:27 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,33 +17,25 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <Analytics />
       </ThemeProvider>
-      <Script id="lhc-widget">
-        {`
-          var LHC_API = LHC_API || {};
-          LHC_API.args = {
-            mode: 'widget',
-            lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
-            wheight: 450,
-            wwidth: 350,
-            pheight: 520,
-            pwidth: 500,
-            department: ["1"],
-            check_messages: false,
-            lang: 'rus/'
-          };
-
-          (function() {
-            var po = document.createElement('script');
-            po.type = 'text/javascript';
-            po.setAttribute('crossorigin', 'anonymous');
-            po.async = true;
-            var date = new Date();
-            po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(po, s);
-          })();
-        `}
-      </Script>
+<script>
+var LHC_API = LHC_API||{};
+LHC_API = {
+  "args": {
+    "mode": "widget",
+    "lhc_base_url": "//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/",
+    "wheight": 450,
+    "wwidth": 350,
+    "pheight": 520,
+    "pwidth": 500,
+    "department": [
+      1
+    ],
+    "leaveamessage": true,
+    "check_messages": false,
+    "theme": 9
+  }
+}
+</script>
     </main>
   );
 }
