0b5145201278a3f450e7c03239b3000a6ad019d9
Artyom Gorin <mysubcult1@gmail.com>
2024-02-01 15:33:42 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
               department: ["1"],
               theme: "9",
               check_messages: false,
-              lang: 'fre/'
+              lang: 'rus/'
             };
             (function() {
               var po = document.createElement('script');
