7ec6b5f5127e229390776c8b8f177dbe005dc431
Kostya <kostyakoz@users.noreply.github.com>
2024-02-15 19:33:56 +0500

    Update Soft.tsx

--- src/components/Soft.tsx
+++ src/components/Soft.tsx
@@ -394,12 +394,12 @@ export default function Soft() {
             docs && (<Link
   href={docsLink}
   target="_blank"
-  className={`mt-4 mx-6 block px-6 py-3 font-medium leading-4 text-center rounded-lg ${
+  className={`mt-4 mx-6 block px-6 py-3 font-small leading-4 text-center rounded-lg ${
     mostPopular
       ? "bg-red-600 text-white shadow-md"
       : "bg-black text-white dark:bg-white dark:text-black"
   }`}
-  style={{ transition: "all 0.3s ease", marginLeft: "0 !important" }}
+  style={{ transition: "all 0.3s ease", marginLeft: "0 !important", opacity: "0.7 }}
   onMouseEnter={(e) => {
     if (
       e.currentTarget.classList.contains("bg-black") ||
@@ -428,9 +428,7 @@ export default function Soft() {
       e.currentTarget.classList.add("bg-red-600");
     }
   }}
-><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-  <path fillRule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0ZM8.94 6.94a.75.75 0 1 1-1.061-1.061 3 3 0 1 1 2.871 5.026v.345a.75.75 0 0 1-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 1 0 8.94 6.94ZM10 15a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
-</svg>
+>{docsLabel}
 
 </Link>
         )} 
