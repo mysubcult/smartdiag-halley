0562190d9b5f09ad643a2a2e37f21b2cb0ee6116
Kostya <kostyakoz@users.noreply.github.com>
2024-02-15 19:26:07 +0500

    Update Soft.tsx

--- src/components/Soft.tsx
+++ src/components/Soft.tsx
@@ -428,7 +428,9 @@ export default function Soft() {
       e.currentTarget.classList.add("bg-red-600");
     }
   }}
->{docsLabel}
+><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+</svg>
 </Link>
         )} 
 
