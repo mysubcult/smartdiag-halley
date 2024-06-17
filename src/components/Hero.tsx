3e7ceb7d862f8680b5ab0c25ecf61f0228f10854
Artyom Gorin <mysubcult1@gmail.com>
2024-06-17 10:13:39 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ export function Hero() {
 
             <div>
               <Link href="#contact">
-                <button className="bg-gradient-to-r from-red-600 to-red-700 text-white text-base rounded-full px-6 py-3 font-medium shadow-md hover:from-red-700 hover:to-red-800 transition-all duration-300">
+                <button className="bg-red-600 text-white text-base rounded-full px-6 py-3 font-medium shadow-md hover:bg-red-700 transition-all duration-300">
                   Обратная связь
                 </button>
               </Link>
