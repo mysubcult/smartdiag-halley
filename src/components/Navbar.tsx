2b503e3a120d5e538b81431a50620d0a5be81ec0
Artyom Gorin <mysubcult1@gmail.com>
2024-06-21 07:47:24 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -147,7 +147,7 @@ export default function Navbar() {
     target="_blank"
   >
     <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 text-base font-medium rounded-full flex items-center gap-2">
-      <img src="https://www.ozon.ru/favicon.ico" alt="OZON" className="w-4 h-4" />
+      <img src="/images/logos/favicon.ico" alt="OZON" className="w-4 h-4" />
       OZON
     </button>
   </a>
