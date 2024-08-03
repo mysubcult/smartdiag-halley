c94cdbf6d1e7880cd9fe0700c50296175a356a02
Artyom Gorin <mysubcult1@gmail.com>
2024-08-03 13:17:49 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
 
 import { MapIcon } from "@heroicons/react/24/outline";
@@ -29,7 +29,7 @@ export default function Contact() {
   });
 
   const [isSuccess, setIsSuccess] = useState(false);
-  const [Message, setMessage] = useState("");
+  const [Message, setMessage] = useState('');
 
   const topic = watch("topic");
 
@@ -46,8 +46,8 @@ export default function Contact() {
     }
   };
 
-  const htmlLinkToTerms = `Я прочитал и согласен с <a href="#!" onclick="showPopup()" class="terms-link" style="color: inherit; text-decoration: inherit;">правилами на обработку персональных данных</a>.`;
-
+  const htmlLinkToTerms = (`Я прочитал и согласен с <a href="#!" onclick="showPopup()" class="terms-link" style="color: inherit; text-decoration: inherit;">правилами на обработку персональных данных</a>.`);
+  
   const onSubmit = async (data: any, e: any) => {
     console.log(data);
     await fetch("https://api.web3forms.com/submit", {
@@ -83,135 +83,45 @@ export default function Contact() {
         <h2 className="text-4xl font-bold">Обратная связь 📩</h2>
 
         <p className="pt-6 pb-6 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
-          Мы очень ценим ваше мнение и постоянно работаем над улучшением нашего
-          сервиса. Если у вас есть предложения, вопросы или комментарии,
-          пожалуйста, свяжитесь с нами! Вы можете написать нам через форму
-          обратной связи, которая находится ниже, или связаться с нами через
-          мессенджеры. Наша команда старается оперативно отвечать на все
-          запросы, и мы гарантируем, что вы получите ответ в течение 12 рабочих
-          часов.
+          Мы очень ценим ваше мнение и постоянно работаем над улучшением нашего сервиса. Если у вас есть предложения, вопросы или комментарии, пожалуйста, свяжитесь с нами! Вы можете написать нам через форму обратной связи, которая находится ниже, или связаться с нами через мессенджеры. Наша команда старается оперативно отвечать на все запросы, и мы гарантируем, что вы получите ответ в течение 12 рабочих часов.
         </p>
       </div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
         <div>
-          <h2 className="text-lg font-bold text-center">📍 Наши контакты</h2>
+          <h2 className="text-lg font-bold text-center">💬 Мы в мессенджерах!</h2>
           <p className="max-w mt-4 mb-4 dark:text-neutral-400 text-center">
-            🏢 <strong>Адрес:</strong>
-            <br />
-            <span className="block mt-2">г. Тольятти, Ленинградская 68</span>
-            📞 <strong>Телефон:</strong>
-            <br />
-            <span className="block mt-2">+7 (123) 456-78-90</span>
-            📧 <strong>Email:</strong>
-            <br />
-            <span className="block mt-2">info@example.com</span>
-          </p>
-
-          <div className="flex justify-center mt-2 text-dark-600 dark:text-neutral-400">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2389.2672578350473!2d49.3463675!3d53.5098036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4165a203737829e3%3A0x7d0b01c59eb88588!2z0YPQuy4g0JLQuNC70L7QstGB0LrQuNC5INCc0L7RgdC60LDRjywgNjgsINCi0LDRgNCw0L3QuNGPINC60YDQuNGB0LrQsNGPINGD0LsuLCA0NDQzNjA!5e0!3m2!1sru!2sru!4v1626953754035!5m2!1sru!2sru"
-              width="300"
-              height="225"
-              style={{ border: "0" }}
-              allowFullScreen
-              loading="lazy"
-              title="Наш адрес"
-            ></iframe>
-          </div>
-
-          <h2 className="text-lg font-bold text-center mt-10">
-            💬 Мы в мессенджерах!
-          </h2>
-          <p className="max-w mt-4 mb-4 dark:text-neutral-400 text-center">
-            🕑 <strong>График работы технической поддержки:</strong>
-            <br />
-            <span className="block mt-2">
-              Понедельник - Пятница: 10:00 - 19:00 (МСК)
-            </span>
-            <span className="block mt-2">
-              Суббота - Воскресенье: 10:00 - 18:00 (МСК)
-            </span>
-            <br />
+            🕑 <strong>График работы технической поддержки:</strong><br />
+            <span className="block mt-2">Понедельник - Пятница: 10:00 - 19:00 (МСК)</span>
+            <span className="block mt-2">Суббота - Воскресенье: 10:00 - 18:00 (МСК)</span><br />
             💬 <strong>Мы доступны в Telegram и WhatsApp:</strong>
           </p>
 
           <div className="flex justify-center mt-2 text-dark-600 dark:text-neutral-400">
             <div className="flex flex-col items-center space-y-2 mx-4">
               <div className="flex items-center space-x-2">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  style={{
-                    fillRule: "evenodd",
-                    clipRule: "evenodd",
-                    strokeLinejoin: "round",
-                    strokeMiterlimit: "1.41421",
-                    fill: "currentColor",
-                  }}
-                >
-                  <path
-                    id="telegram-1"
-                    d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
-                  />
+                <svg width="24" height="24" viewBox="0 0 24 24" style={{ fillRule: "evenodd", clipRule: "evenodd", strokeLinejoin: "round", strokeMiterlimit: "1.41421", fill: "currentColor" }}>
+                  <path id="telegram-1" d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z" />
                 </svg>
-                <a
-                  href="https://смартдиаг-поддержка.рф/telegram"
-                  target="_blank"
-                  className="hover:text-blue-500"
-                  style={{ width: "90px" }}
-                >
-                  Telegram
-                </a>
+                <a href="https://смартдиаг-поддержка.рф/telegram" target="_blank" className="hover:text-blue-500" style={{ width: "90px" }}>Telegram</a>
               </div>
               <div>
-                <a
-                  href="https://смартдиаг-поддержка.рф/telegram"
-                  target="_blank"
-                >
-                  <img
-                    src="/images/hero/telegram-qr.svg"
-                    className="transition-transform duration-300 ease-in-out transform hover:scale-110"
-                    width="120"
-                    height="120"
-                    alt="QR Code"
-                  />
+                <a href="https://смартдиаг-поддержка.рф/telegram" target="_blank">
+                  <img src="/images/hero/telegram-qr.svg" className="transition-transform duration-300 ease-in-out transform hover:scale-110" width="120" height="120" alt="QR Code" />
                 </a>
               </div>
             </div>
 
             <div className="flex flex-col items-center space-y-2 mx-4">
               <div className="flex items-center space-x-2">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  style={{ fill: "currentColor" }}
-                >
+                <svg width="24" height="24" viewBox="0 0 24 24" style={{ fill: "currentColor" }}>
                   <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
                 </svg>
-                <a
-                  href="https://смартдиаг-поддержка.рф/whatsapp"
-                  target="_blank"
-                  className="hover:text-green-500"
-                  style={{ width: "90px" }}
-                >
-                  WhatsApp
-                </a>
+                <a href="https://смартдиаг-поддержка.рф/whatsapp" target="_blank" className="hover:text-green-500" style={{ width: "90px" }}>WhatsApp</a>
               </div>
               <div>
-                <a
-                  href="https://смартдиаг-поддержка.рф/whatsapp"
-                  target="_blank"
-                >
-                  <img
-                    src="/images/hero/whatsapp-qr.svg"
-                    className="transition-transform duration-300 ease-in-out transform hover:scale-110"
-                    width="120"
-                    height="120"
-                    alt="QR Code"
-                  />
+                <a href="https://смартдиаг-поддержка.рф/whatsapp" target="_blank">
+                  <img src="/images/hero/whatsapp-qr.svg" className="transition-transform duration-300 ease-in-out transform hover:scale-110" width="120" height="120" alt="QR Code" />
                 </a>
               </div>
             </div>
@@ -227,11 +137,7 @@ export default function Contact() {
                 value="c39d2187-6537-4c0b-87e1-3cff0bf0c1c3"
                 {...register("access_key")}
               />
-              <input
-                type="hidden"
-                value="Обращение через форму обратной связи на сайте SmartDiag"
-                {...register("subject")}
-              />
+              <input type="hidden" value="Обращение через форму обратной связи на сайте SmartDiag" {...register("subject")} />
               <input type="hidden" value="SmartDiag" {...register("from_name")} />
               <input
                 type="checkbox"
@@ -299,30 +205,20 @@ export default function Contact() {
                   <select
                     id="topic"
                     autoComplete="off"
-                    className={`select-dropdown w-full px-4 py-3 appearance-none ${
+                    className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900 focus:ring-4 bg-white appearance-none ${
                       errors.topic
                         ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:border-rose-500 dark:focus:border-white dark:ring-0"
                         : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
                     }`}
-                    style={{
-                      paddingLeft: "6px",
-                      textIndent: "10px",
-                      paddingRight: "2rem",
-                    }}
-                    {...register("topic", {
-                      required: "Необходимо выбрать тему сообщения",
-                    })}
+                    style={{ paddingLeft: '6px', textIndent: '10px', paddingRight: '2rem' }}
+                    {...register("topic", { required: "Необходимо выбрать тему сообщения" })}
                   >
                     <option value="">Выберите тему сообщения</option>
                     <option value="Вопрос о товаре">Вопрос о товаре</option>
                     <option value="Активация прибора">Активация прибора</option>
-                    <option value="Помощь с установкой ПО">
-                      Помощь с установкой ПО
-                    </option>
+                    <option value="Помощь с установкой ПО">Помощь с установкой ПО</option>
                     <option value="Заказ оборудования">Заказ оборудования</option>
-                    <option value="Сообщить о проблеме">
-                      Сообщить о проблеме
-                    </option>
+                    <option value="Сообщить о проблеме">Сообщить о проблеме</option>
                     <option value="Другое">Другое</option>
                   </select>
                   <div className="absolute inset-y-0 right-2 flex items-center justify-center pointer-events-none">
@@ -346,8 +242,7 @@ export default function Contact() {
                 )}
               </div>
 
-              {(topic === "Активация прибора" ||
-                topic === "Помощь с установкой ПО") && (
+              {(topic === "Активация прибора" || topic === "Помощь с установкой ПО") && (
                 <div className="mb-3 relative">
                   <label htmlFor="orderNumber" className="sr-only">
                     Номер заказа
@@ -363,10 +258,7 @@ export default function Contact() {
                         : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
                     }`}
                     {...register("orderNumber", {
-                      required:
-                        (topic === "Активация прибора" ||
-                          topic === "Помощь с установкой ПО") &&
-                        "Номер заказа обязателен",
+                      required: (topic === "Активация прибора" || topic === "Помощь с установкой ПО") && "Номер заказа обязателен",
                     })}
                   />
                   {errors.orderNumber && (
@@ -385,9 +277,7 @@ export default function Contact() {
                       ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
                       : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
                   }`}
-                  {...register("message", {
-                    required: "Необходимо ввести текст сообщения",
-                  })}
+                  {...register("message", { required: "Необходимо ввести текст сообщения" })}
                 />
                 {errors.message && (
                   <div className="mt-1 text-rose-500">
