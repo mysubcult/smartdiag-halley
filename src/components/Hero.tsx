9492f94ae33e9ff2900fd8918c5a99a719dfb141
Artyom Gorin <mysubcult1@gmail.com>
2024-08-04 15:50:32 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,8 +23,13 @@ export function Hero() {
           <div className="flex flex-auto pt-10 gap-4 min-w-[350px] justify-center sm:justify-start">
             <div>
               <Link href="#soft">
-                <button className="btn-grad-red text-base font-medium">
+                <button className="btn-grad-red text-base font-medium flex items-center">
                   Программы для приборов
+                  <span className="icon-container ml-2">
+                    <svg className="icon w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M20 4.5C20 3.67157 19.3284 3 18.5 3C17.6716 3 17 3.67157 17 4.5V9H7V4.5C7 3.67157 6.32843 3 5.5 3C4.67157 3 4 3.67157 4 4.5V9H3.5C2.67157 9 2 9.67157 2 10.5V20.5C2 21.3284 2.67157 22 3.5 22H18.5C19.3284 22 20 21.3284 20 20.5V10.5C20 9.67157 19.3284 9 18.5 9H18V4.5ZM18.5 4H18V8H6V4H5.5C5.22386 4 5 4.22386 5 4.5V9H19V4.5C19 4.22386 18.7761 4 18.5 4ZM6 10H18V20H6V10Z" fill="currentColor"/>
+                    </svg>
+                  </span>
                 </button>
               </Link>
             </div>
@@ -51,6 +56,26 @@ export function Hero() {
           />
         </div>
       </div>
+
+      <style jsx>{`
+        .btn-grad-red {
+          position: relative;
+          overflow: hidden;
+        }
+
+        .icon-container {
+          display: flex;
+          align-items: center;
+        }
+
+        .icon {
+          transition: transform 0.3s ease;
+        }
+
+        .btn-grad-red:hover .icon {
+          transform: rotate(360deg);
+        }
+      `}</style>
     </div>
   );
 }
