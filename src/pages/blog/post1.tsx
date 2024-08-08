a6aa24bb29a444e81751a1350011cc8e51e9390c
Artyom Gorin <mysubcult1@gmail.com>
2024-08-08 12:57:47 +0400

    Update post1.tsx

--- src/pages/blog/post1.tsx
+++ src/pages/blog/post1.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Layout from "../../components/Layout";
 
-export default function BlogPost() {
+export default function Home() {
   const [isSticky, setIsSticky] = useState(false);
   const router = useRouter();
 
