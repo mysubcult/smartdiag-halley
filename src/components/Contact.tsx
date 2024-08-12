d06ec8f6e919f45f21b34da6b7d93c8c7fc9242a
Artyom Gorin <mysubcult1@gmail.com>
2024-08-13 03:22:37 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -30,7 +30,6 @@ export default function Contact() {
 
   const [isSuccess, setIsSuccess] = useState(false);
   const [Message, setMessage] = useState('');
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const topic = watch('topic');
 
@@ -47,15 +46,14 @@ export default function Contact() {
     }
   };
 
+  // Исправляем проблему с селектором '#!'
   const showPopup = () => {
-    setIsPopupOpen(true);
+    // Ваш код для отображения всплывающего окна
+    console.log('Showing popup with privacy policy.');
+    // Здесь должен быть код, который открывает всплывающее окно с правилами обработки персональных данных
   };
 
-  const hidePopup = () => {
-    setIsPopupOpen(false);
-  };
-
-  const htmlLinkToTerms = `Я прочитал и согласен с <a href="#" onClick={(e) => { e.preventDefault(); showPopup(); }} class="terms-link" style="color: inherit; text-decoration: inherit;">правилами на обработку персональных данных</a>.`;
+  const htmlLinkToTerms = `Я прочитал и согласен с <a href="javascript:void(0);" onclick="showPopup()" class="terms-link" style="color: inherit; text-decoration: inherit;">правилами на обработку персональных данных</a>.`;
 
   const onSubmit = async (data: any, e: any) => {
     console.log(data);
@@ -119,7 +117,84 @@ export default function Contact() {
           </p>
 
           <div className="flex justify-center mt-2 text-dark-600 dark:text-neutral-400">
-            {/* Здесь размещается код QR-кодов и ссылок на мессенджеры */}
+            <div className="flex flex-col items-center space-y-2 mx-4">
+              <div className="flex items-center space-x-2">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  style={{
+                    fillRule: 'evenodd',
+                    clipRule: 'evenodd',
+                    strokeLinejoin: 'round',
+                    strokeMiterlimit: '1.41421',
+                    fill: 'currentColor',
+                  }}
+                >
+                  <path
+                    id="telegram-1"
+                    d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
+                  />
+                </svg>
+                <a
+                  href="https://смартдиаг-поддержка.рф/telegram"
+                  target="_blank"
+                  className="hover:text-blue-500"
+                  style={{ width: '90px' }}
+                >
+                  Telegram
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://смартдиаг-поддержка.рф/telegram"
+                  target="_blank"
+                >
+                  <img
+                    src="/images/hero/telegram-qr.svg"
+                    className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                    width="120"
+                    height="120"
+                    alt="QR Code"
+                  />
+                </a>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center space-y-2 mx-4">
+              <div className="flex items-center space-x-2">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  style={{ fill: 'currentColor' }}
+                >
+                  <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+                </svg>
+                <a
+                  href="https://смартдиаг-поддержка.рф/whatsapp"
+                  target="_blank"
+                  className="hover:text-green-500"
+                  style={{ width: '90px' }}
+                >
+                  WhatsApp
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://смартдиаг-поддержка.рф/whatsapp"
+                  target="_blank"
+                >
+                  <img
+                    src="/images/hero/whatsapp-qr.svg"
+                    className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                    width="120"
+                    height="120"
+                    alt="QR Code"
+                  />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -409,23 +484,6 @@ export default function Contact() {
           )}
         </div>
       </div>
-
-      {/* Popup */}
-      {isPopupOpen && (
-        <div className="smartdiag-popup open">
-          <div className="blocker" onClick={hidePopup}></div>
-          <div className="sd-contents">
-            <a href="#!" onClick={hidePopup} className="close">
-              <svg width="15" height="15" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11.7612 9.99893L19.6305 2.14129C19.8657 1.90606 19.9979 1.58701 19.9979 1.25434C19.9979 0.921668 19.8657 0.602622 19.6305 0.367388C19.3953 0.132153 19.0763 0 18.7437 0C18.411 0 18.092 0.132153 17.8568 0.367388L10 8.23752L2.14319 0.367388C1.90799 0.132153 1.58897 2.95361e-07 1.25634 2.97839e-07C0.923701 3.00318e-07 0.604689 0.132153 0.36948 0.367388C0.134271 0.602622 0.00213201 0.921668 0.002132 1.25434C0.002132 1.58701 0.134271 1.90606 0.36948 2.14129L8.23878 9.99893L0.36948 17.8566C0.252404 17.9727 0.159479 18.1109 0.0960643 18.2631C0.0326494 18.4153 0 18.5786 0 18.7435C0 18.9084 0.0326494 19.0717 0.0960643 19.224C0.159479 19.3762 0.252404 19.5143 0.36948 19.6305C0.4856 19.7476 0.623751 19.8405 0.775965 19.9039C0.928178 19.9673 1.09144 20 1.25634 20C1.42123 20 1.5845 19.9673 1.73671 19.9039C1.88892 19.8405 2.02708 19.7476 2.14319 19.6305L10 11.7603L17.8568 19.6305C17.9729 19.7476 18.1111 19.8405 18.2633 19.9039C18.4155 19.9673 18.5788 20 18.7437 20C18.9086 20 19.0718 19.9673 19.224 19.9039C19.3762 19.8405 19.5144 19.7476 19.6305 19.6305C19.7476 19.5143 19.8405 19.3762 19.9039 19.224C19.9674 19.0717 20 18.9084 20 18.7435C20 18.5786 19.9674 18.4153 19.9039 18.2631C19.8405 18.1109 19.7476 17.9727 19.6305 17.8566L11.7612 9.99893Z" fill="currentColor"></path>
-              </svg>
-            </a>
-            <p><strong>Условия ОПД</strong></p>
-            <p>Политика в отношении обработки персональных данных...</p>
-            {/* Полное содержание политики обработки данных */}
-          </div>
-        </div>
-      )}
     </div>
   );
 }
