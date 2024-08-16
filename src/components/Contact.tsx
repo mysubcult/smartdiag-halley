2b67ca708d7aad22609729dc6df1b213bc9611b7
Artyom Gorin <mysubcult1@gmail.com>
2024-08-16 08:05:47 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,6 +19,7 @@ export default function Contact() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [Message, setMessage] = useState('');
+  const [isChecked, setIsChecked] = useState(true); // Добавлено для сохранения состояния флажка
 
   const {
     register,
@@ -143,7 +144,7 @@ export default function Contact() {
                   <Image
                     src="/images/hero/telegram-qr.svg"
                     alt="QR Code Telegram"
-                    className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                    className="rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
                     width={120}
                     height={120}
                   />
@@ -178,7 +179,7 @@ export default function Contact() {
                   <Image
                     src="/images/hero/whatsapp-qr.svg"
                     alt="QR Code WhatsApp"
-                    className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                    className="rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
                     width={120}
                     height={120}
                   />
@@ -369,7 +370,8 @@ export default function Contact() {
                   type="checkbox"
                   id="agree"
                   className="mr-2"
-                  defaultChecked
+                  checked={isChecked}
+                  onChange={() => setIsChecked(!isChecked)}
                   required
                 />
                 <label htmlFor="agree" className="text-sm">
@@ -384,7 +386,7 @@ export default function Contact() {
               </div>
               <button
                 type="submit"
-                className="w-full py-4 font-semibold text-white transition-colors bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-blue-200 px-7 dark:bg-white dark:text-black dark:hover:bg-gray-300"
+                className="w-full py-4 font-semibold text-white transition-colors bg-gradient-to-r from-black to-red-600 rounded-md hover:from-red-700 hover:to-black focus:outline-none focus:ring-offset-2 focus:ring focus:ring-red-200 px-7"
               >
                 {isSubmitting ? (
                   <svg
@@ -458,30 +460,5 @@ export default function Contact() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M27.9995 69C43.6205 53.379 52.3786 44.621 67.9995 29M26.8077 29L67.9995 69M48.2189 95C42.0906 95 36.0222 93.7929 30.3604 91.4477C24.6985 89.1025 19.554 85.6651 15.2206 81.3316C10.8872 76.9982 7.44975 71.8538 5.10454 66.1919C2.75932 60.53 1.55225 54.4617 1.55225 48.3333C1.55225 42.205 2.75932 36.1366 5.10454 30.4748C7.44975 24.8129 10.8872 19.6684 15.2206 15.335C19.554 11.0016 24.6985 7.56418 30.3604 5.21896C36.0222 2.87374 42.0906 1.66667 48.2189 1.66667C60.5957 1.66667 72.4655 6.58333 81.2172 15.335C89.9689 24.0867 94.8856 35.9566 94.8856 48.3333C94.8856 60.7101 89.9689 72.58 81.2172 81.3316C72.4655 90.0833 60.5957 95 48.2189 95Z"
-                  stroke="CurrentColor"
-                  strokeWidth="3"
-                />
-              </svg>
-
-              <h3 className=" py-5 text-2xl font-medium text-rose-500">
-                Упс, что-то пошло не так!
-              </h3>
-              <p className="text-neutral-900 dark:text-neutral-300 md:px-4">
-                {Message}
-              </p>
-              <button
-                className="mt-6 py-2 px-4 bg-rose-500 rounded-full focus:outline-none text-neutral-100"
-                onClick={() => reset()}
-              >
-                Попробовать еще
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-
-      <PrivacyPolicyModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-    </div>
-  );
-}
+                  d="M27.9995 69C43.6205 53.379 52.3786 44.621 67.9995 29M26.8077 29L67.9995 69M48.2189 95C42.0906 95 36.0222 93.7929 30.3604 91.4477C24.6985 89.1025 19.554 85.6651 15.2206 81.3316C10.8872 76.9982 7.44975 71.8538 5.10454 66.1919C2.75932 60.53 1.55225 54.4617 1.55225 48.3333C1.55225 42.205 2.75932 36.1366 5.10454 30.4748C7.44975 24.8129 10.8872 19.6684 15.2206 15.335C19.554 11.0016 24.6985 7.56418 30.3604 5.21896C36.0222 2.87374 42.0906 1.66667 48.2189 1.66667C60.5957 1.66667 72.4655 6.58333 81.2172 15.335C89.9689 24.0867 94.8856 35.9566 94.8856 
+The file is too long and its contents have been truncated.
