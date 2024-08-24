e8b3c7eaa6c382a5da6fc35a1e68f9d6dad196b3
Artyom Gorin <mysubcult1@gmail.com>
2024-08-24 15:19:31 +0400

    Update CookieConsent.tsx

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -67,8 +67,8 @@ const CookieConsent = () => {
         <p className="text-gray-800 dark:text-gray-200 text-sm w-full text-center">
           Мы используем файлы cookie для улучшения вашего опыта на нашем сайте, анализа трафика и персонализации контента. Пожалуйста, выберите, какие куки вы хотите разрешить.
         </p>
-        <div className="flex justify-between items-center w-full">
-          <div className="flex space-x-4 justify-center items-center flex-grow">
+        <div className="flex justify-center items-center w-full">
+          <div className="flex space-x-4 justify-center items-center">
             <button
               className="bg-red-600 text-white px-3 py-1.5 rounded-md hover:bg-red-700 transition duration-300 text-sm"
               onClick={acceptAllCookies}
@@ -82,12 +82,14 @@ const CookieConsent = () => {
               Настроить
             </button>
           </div>
-          <button
-            className="text-gray-500 underline text-xs"
-            onClick={openModal}
-          >
-            Подробнее
-          </button>
+          <div className="flex-grow flex justify-end">
+            <button
+              className="text-gray-500 underline text-xs"
+              onClick={openModal}
+            >
+              Подробнее
+            </button>
+          </div>
         </div>
       </div>
 
@@ -107,7 +109,7 @@ const CookieConsent = () => {
                   <label className="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" checked={true} disabled className="sr-only" />
                     <div className="w-10 h-4 bg-gray-200 rounded-full shadow-inner"></div>
-                    <div className="dot absolute left-0 top-0.5 w-4 h-4 bg-gray-400 rounded-full shadow transition"></div>
+                    <div className="dot absolute left-0 top-0.25 w-4 h-4 bg-gray-400 rounded-full shadow transition"></div>
                   </label>
                 </div>
                 <div className="flex justify-between items-center w-full">
@@ -120,7 +122,7 @@ const CookieConsent = () => {
                       className="sr-only"
                     />
                     <div className={`w-10 h-4 rounded-full shadow-inner ${cookies.analytics ? 'bg-green-500' : 'bg-gray-200'}`}></div>
-                    <div className={`dot absolute left-0 top-0.5 w-4 h-4 bg-white rounded-full shadow transition ${cookies.analytics ? 'transform translate-x-6 bg-green-500' : ''}`}></div>
+                    <div className={`dot absolute left-0 top-0.25 w-4 h-4 bg-white rounded-full shadow transition ${cookies.analytics ? 'transform translate-x-6 bg-green-500' : ''}`}></div>
                   </label>
                 </div>
                 <div className="flex justify-between items-center w-full">
@@ -133,7 +135,7 @@ const CookieConsent = () => {
                       className="sr-only"
                     />
                     <div className={`w-10 h-4 rounded-full shadow-inner ${cookies.marketing ? 'bg-green-500' : 'bg-gray-200'}`}></div>
-                    <div className={`dot absolute left-0 top-0.5 w-4 h-4 bg-white rounded-full shadow transition ${cookies.marketing ? 'transform translate-x-6 bg-green-500' : ''}`}></div>
+                    <div className={`dot absolute left-0 top-0.25 w-4 h-4 bg-white rounded-full shadow transition ${cookies.marketing ? 'transform translate-x-6 bg-green-500' : ''}`}></div>
                   </label>
                 </div>
               </div>
