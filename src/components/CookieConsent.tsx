a037b9c8ba17775385dd9cc8500f0312ce326438
Artyom Gorin <mysubcult1@gmail.com>
2024-08-24 14:27:25 +0400

    Update CookieConsent.tsx

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -74,120 +74,103 @@ const CookieConsent = () => {
           >
             Принять все
           </button>
-          {isSettingsOpen || isModalOpen ? (
-            <button
-              className="text-red-600 underline hover:text-red-800 text-sm"
-              onClick={openModal}
-            >
-              Что это?
-            </button>
-          ) : (
-            <>
-              {/* Для мобильной версии показываем только две кнопки */}
-              <button
-                className="text-red-600 underline hover:text-red-800 text-sm block md:hidden"
-                onClick={openSettingsModal}
-              >
-                Настроить
-              </button>
-              {/* Для ПК версии показываем три кнопки */}
-              <button
-                className="text-red-600 underline hover:text-red-800 text-sm hidden md:inline-block"
-                onClick={openModal}
-              >
-                Подробнее
-              </button>
-              <button
-                className="text-red-600 underline hover:text-red-800 text-sm hidden md:inline-block"
-                onClick={openSettingsModal}
-              >
-                Настроить
-              </button>
-            </>
-          )}
+          {/* Для мобильной версии показываем две кнопки */}
+          <button
+            className="text-red-600 underline hover:text-red-800 text-sm block md:hidden"
+            onClick={openSettingsModal}
+          >
+            Настроить
+          </button>
+          {/* Для ПК версии показываем три кнопки */}
+          <button
+            className={`text-red-600 underline hover:text-red-800 text-sm hidden md:inline-block ${isModalOpen ? 'hidden' : 'inline-block'}`}
+            onClick={openModal}
+          >
+            Подробнее
+          </button>
+          <button
+            className="text-red-600 underline hover:text-red-800 text-sm hidden md:inline-block"
+            onClick={openSettingsModal}
+          >
+            Настроить
+          </button>
         </div>
       </div>
 
       {/* Модальное окно с настройками куки */}
-      {isSettingsOpen && (
+      {(isSettingsOpen || isModalOpen) && (
         <div
           className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center"
-          onClick={closeSettingsModal}
+          onClick={isSettingsOpen ? closeSettingsModal : closeModal}
         >
-          <div
-            className="bg-white dark:bg-gray-800 rounded-lg p-6 w-80 shadow-lg relative"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <h2 className="text-xl font-bold mb-4 text-gray-800 dark:text-gray-200">Настройки файлов cookie</h2>
-            <div className="flex flex-col items-start space-y-2 w-full">
-              <label className="text-gray-800 dark:text-gray-200 text-sm flex items-center">
-                <input type="checkbox" checked={cookies.necessary} disabled className="mr-2" />
-                Необходимые куки (всегда включены)
-              </label>
-              <label className="text-gray-800 dark:text-gray-200 text-sm flex items-center">
-                <input
-                  type="checkbox"
-                  checked={cookies.analytics}
-                  onChange={() => handleCheckboxChange('analytics')}
-                  className="mr-2"
-                />
-                Аналитические куки
-              </label>
-              <label className="text-gray-800 dark:text-gray-200 text-sm flex items-center">
-                <input
-                  type="checkbox"
-                  checked={cookies.marketing}
-                  onChange={() => handleCheckboxChange('marketing')}
-                  className="mr-2"
-                />
-                Маркетинговые куки
-              </label>
-            </div>
-            <div className="flex space-x-2 justify-between mt-4">
-              <button
-                className="text-gray-600 underline hover:text-gray-800 text-sm"
-                onClick={openModal}
-              >
-                Что это?
-              </button>
-              <button
-                className="bg-red-600 text-white px-3 py-1.5 rounded-md hover:bg-red-700 transition duration-300 text-sm"
-                onClick={acceptSelectedCookies}
-              >
-                Принять выбранные
-              </button>
-            </div>
-            <button
-              className="absolute top-2 right-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 text-2xl"
-              onClick={closeSettingsModal}
+          {isSettingsOpen ? (
+            <div
+              className="bg-white dark:bg-gray-800 rounded-lg p-6 w-80 shadow-lg relative"
+              onClick={(e) => e.stopPropagation()}
             >
-              &times;
-            </button>
-          </div>
-        </div>
-      )}
-
-      {/* Модальное окно с информацией о куки */}
-      {isModalOpen && (
-        <div
-          className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center"
-          onClick={closeModal}
-        >
-          <div
-            className="bg-white dark:bg-gray-800 rounded-lg p-6 w-80 shadow-lg relative"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <h2 className="text-xl font-bold mb-4 text-gray-800 dark:text-gray-200">Что такое куки?</h2>
-            <p className="text-gray-700 dark:text-gray-300 mb-4">
-              Файлы cookie — это небольшие текстовые файлы, которые сохраняются на вашем устройстве при посещении сайта. Они помогают нам улучшать наш сайт и предоставлять вам более персонализированный сервис.
-            </p>
-            <button
-              className="absolute top-2 right-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 text-2xl"
-              onClick={closeModal}
+              <h2 className="text-xl font-bold mb-4 text-gray-800 dark:text-gray-200">Настройки файлов cookie</h2>
+              <div className="flex flex-col items-start space-y-2 w-full">
+                <label className="text-gray-800 dark:text-gray-200 text-sm flex items-center">
+                  <input type="checkbox" checked={cookies.necessary} disabled className="mr-2" />
+                  Необходимые куки (всегда включены)
+                </label>
+                <label className="text-gray-800 dark:text-gray-200 text-sm flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={cookies.analytics}
+                    onChange={() => handleCheckboxChange('analytics')}
+                    className="mr-2"
+                  />
+                  Аналитические куки
+                </label>
+                <label className="text-gray-800 dark:text-gray-200 text-sm flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={cookies.marketing}
+                    onChange={() => handleCheckboxChange('marketing')}
+                    className="mr-2"
+                  />
+                  Маркетинговые куки
+                </label>
+              </div>
+              <div className="flex space-x-2 justify-between mt-4">
+                <button
+                  className="text-gray-600 underline hover:text-gray-800 text-sm"
+                  onClick={openModal}
+                >
+                  Что это?
+                </button>
+                <button
+                  className="bg-red-600 text-white px-3 py-1.5 rounded-md hover:bg-red-700 transition duration-300 text-sm"
+                  onClick={acceptSelectedCookies}
+                >
+                  Принять выбранные
+                </button>
+              </div>
+              <button
+                className="absolute top-2 right-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 text-2xl"
+                onClick={closeSettingsModal}
+              >
+                &times;
+              </button>
+            </div>
+          ) : (
+            <div
+              className="bg-white dark:bg-gray-800 rounded-lg p-6 w-80 shadow-lg relative"
+              onClick={(e) => e.stopPropagation()}
             >
-              &times;
-            </button>
-          </div>
+              <h2 className="text-xl font-bold mb-4 text-gray-800 dark:text-gray-200">Что такое куки?</h2>
+              <p className="text-gray-700 dark:text-gray-300 mb-4">
+                Файлы cookie — это небольшие текстовые файлы, которые сохраняются на вашем устройстве при посещении сайта. Они помогают нам улучшать наш сайт и предоставлять вам более персонализированный сервис.
+              </p>
+              <button
+                className="absolute top-2 right-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 text-2xl"
+                onClick={closeModal}
+              >
+                &times;
+              </button>
+            </div>
+          )}
         </div>
       )}
     </>
