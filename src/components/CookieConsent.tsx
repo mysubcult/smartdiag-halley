14e468fde6b3949934317a737f5e6ad891a9de85
Artyom Gorin <mysubcult1@gmail.com>
2024-08-24 13:22:21 +0400

    Update CookieConsent.tsx

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -30,8 +30,8 @@ const CookieConsent = () => {
     <>
       {/* Основное окно согласия с куки, расположенное слева снизу */}
       <div className="fixed bottom-4 left-4 bg-white dark:bg-gray-800 shadow-lg p-4 rounded-lg w-96 border-2 border-gray-300 dark:border-gray-700 flex flex-col items-center space-y-3">
-        <p className="text-gray-800 dark:text-gray-200 text-sm w-full text-justify">
-          Мы используем файлы cookie для улучшения вашего опыта. Оставаясь на сайте, вы соглашаетесь с их использованием.
+        <p className="text-gray-800 dark:text-gray-200 text-sm w-full text-center">
+          Мы используем файлы cookie, чтобы улучшить ваш опыт на сайте. Продолжая использовать наш сайт, вы соглашаетесь с их применением.
         </p>
         <div className="flex space-x-2 justify-center">
           <button
