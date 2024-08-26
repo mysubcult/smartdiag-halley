c7e627b05b7dc45290806bea742280f3d0ffab3b
Artyom Gorin <mysubcult1@gmail.com>
2024-08-26 20:22:11 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = (props: any) => {
   const meta = {
     title: "SmartDiag - Ваш проводник в мире автодиагностики",
     description: `SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке.`,
-    keywords: "автодиагностика, Autocom CDP+, Delphi DS150E, VCDS, диагностика автомобилей, программы для диагностики, оборудование для диагностики",
+    keywords: "автодиагностика, Autocom CDP+, Delphi DS150E, VCDS, диагностика автомобилей, программы для диагностики, оборудование для диагностики, car diagnostics, diagnostic tools, software for diagnostics, diagnostic equipment, vehicle diagnostics, diagnostic software, installation instructions",
     image: "/images/seo/halley-banner.png",
     type: "website",
     ...customMeta,
