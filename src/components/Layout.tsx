270c090f5fcc1c7d9c8b7a0112d7d582d7505a8f
Artyom Gorin <mysubcult1@gmail.com>
2024-08-30 01:16:08 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,22 +3,13 @@ import { useRouter } from "next/router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-// Определяем интерфейс для пропсов
-interface LayoutProps {
-  children: React.ReactNode; // Дети - это элементы React
-  title?: string;
-  description?: string;
-  keywords?: string;
-  image?: string;
-  type?: string;
-}
-
-const Layout = ({ children, ...customMeta }: LayoutProps) => {
+const Layout = (props: any) => {
+  const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
     title: "SmartDiag - Ваш проводник в мире автодиагностики",
-    description: "SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей.",
-    keywords: "автодиагностика, диагностика автомобилей",
+    description: `SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке.`,
+    keywords: "автодиагностика, Autocom CDP+, Delphi DS150E, VCDS, Вася, mucar, thinkdiag, Thinkcar, диагностика автомобилей, программы для диагностики, оборудование для диагностики, car diagnostics, diagnostic tools, software for diagnostics, diagnostic equipment, vehicle diagnostics, diagnostic software, installation instructions, BMW, Audi, Mercedes, Toyota, Volkswagen, Ford, Nissan, Honda, Chevrolet, Kia",
     image: "/images/seo/halley-banner.png",
     type: "website",
     ...customMeta,
@@ -30,9 +21,12 @@ const Layout = ({ children, ...customMeta }: LayoutProps) => {
         <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
         <meta name="keywords" content={meta.keywords} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:image" content={meta.image} />
+        <link rel="canonical" href={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
       </Head>
       <Navbar />
       <main>{children}</main>
