e27bf8ef1e139e5bc6daa81a034d10537495f8ef
Artyom Gorin <mysubcult1@gmail.com>
2024-08-30 00:11:33 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,39 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Script
+          id="lhc-widget-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var LHC_API = LHC_API||{};
+              LHC_API.args = {
+                mode: 'widget',
+                lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
+                wheight: 450,
+                wwidth: 350,
+                pheight: 520,
+                pwidth: 500,
+                domain: 'смартдиаг.рф',
+                leaveamessage: true,
+                department: ["1"],
+                theme: "9",
+                check_messages: false,
+                lang: 'rus/'
+              };
+              (function() {
+                var po = document.createElement('script');
+                po.type = 'text/javascript';
+                po.setAttribute('crossorigin', 'anonymous');
+                po.async = true;
+                var date = new Date();
+                po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(po, s);
+              })();
+            `,
+          }}
+        />
       </ThemeProvider>
     </main>
   );
