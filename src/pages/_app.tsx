2b64b35ed97d546507e725b8dcdef5bf9f4e61e2
Artyom Gorin <mysubcult1@gmail.com>
2024-08-30 00:13:21 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,11 @@ import React from 'react';
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-import Script from 'next/script'; // Добавляем импорт
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.variable} font-sans relative`}>
+    <main className={`font-sans relative`}>
       <Head>
       </Head>
       <ThemeProvider attribute="class">
