691f2953d741db8b586a7f55c3112c5c0c1b2621
Artyom Gorin <mysubcult1@gmail.com>
2024-09-01 08:50:45 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,11 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 interface LayoutProps {
   children: ReactNode;
-  title?: string;  // Сделали title опциональным
+  title?: string; // Заголовок опциональный
   description?: string;
   keywords?: string;
   image?: string;
@@ -16,8 +16,16 @@ interface LayoutProps {
 const Layout = ({ children, title, description, keywords, image, type }: LayoutProps) => {
   const router = useRouter();
   const defaultTitle = "SmartDiag - Ваш проводник в мире автодиагностики"; // Дефолтный заголовок
+
+  useEffect(() => {
+    if (title) {
+      document.title = title; // Устанавливаем заголовок, если он передан как пропс
+    } else {
+      document.title = defaultTitle; // Используем дефолтный заголовок
+    }
+  }, [title]); // Обновляем заголовок только если изменился пропс title
+
   const meta = {
-    title: title || defaultTitle,  // Используем переданный title или дефолтный
     description: description || "SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке.",
     keywords: keywords || "автодиагностика, Autocom CDP+, Delphi DS150E, VCDS, Вася, mucar, thinkdiag, Thinkcar, диагностика автомобилей, программы для диагностики, оборудование для диагностики, car diagnostics, diagnostic tools, software for diagnostics, diagnostic equipment, vehicle diagnostics, diagnostic software, installation instructions, BMW, Audi, Mercedes, Toyota, Volkswagen, Ford, Nissan, Honda, Chevrolet, Kia",
     image: image || "/images/seo/halley-banner.png",
@@ -27,12 +35,11 @@ const Layout = ({ children, title, description, keywords, image, type }: LayoutP
   return (
     <>
       <Head>
-        <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
         <meta name="keywords" content={meta.keywords} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
-        <meta property="og:title" content={meta.title} />
+        <meta property="og:title" content={title || defaultTitle} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:image" content={meta.image} />
         <link rel="canonical" href={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
