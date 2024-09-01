82fd25fb444715f866ba1f11f229b82c63cff9ca
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 02:01:13 +0400

    Update post1.tsx

--- src/pages/blog/post1.tsx
+++ src/pages/blog/post1.tsx
@@ -17,6 +17,8 @@ export default function BlogPost() {
   const pageKeywords = "ошибки, архивы, решения, проблемы с антивирусом, устаревшее ПО";
 
   useEffect(() => {
+    // Обновление заголовка вручную для уверенности
+    document.title = baseTitle;
     setIsClient(true);
   }, []);
 
