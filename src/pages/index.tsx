e4c04914442ee2e058587f8f34426f650755a52b
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 02:58:27 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-// index.tsx
-import { Hero } from '../components/Hero'; // Предполагается, что Hero — это именованный экспорт
-import Soft from '../components/Soft'; // Импорт по умолчанию для Soft
-import Faq from '../components/Faq'; // Импорт по умолчанию для Faq
+// Пример для страницы index.tsx
+import { Hero } from '../components/Hero';
+import Soft from '../components/Soft';
+import Faq from '../components/Faq';
 import Layout from '../components/Layout';
-import Head from 'next/head'; // Используем Head для задания заголовка и мета-данных
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -15,7 +15,6 @@ export default function Home() {
       <Hero />
       <Soft />
       <Faq />
-      {/* Другие компоненты главной страницы */}
     </Layout>
   );
 }
