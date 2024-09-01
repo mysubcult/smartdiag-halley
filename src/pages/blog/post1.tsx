b3da981321c47fc3c24fb8fef2b22fcb43203c63
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 03:00:55 +0400

    Update post1.tsx

--- src/pages/blog/post1.tsx
+++ src/pages/blog/post1.tsx
@@ -1,20 +1,18 @@
-// post1.txt
+// post1.tsx
 import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../../components/Layout';
-import Head from 'next/head'; // Используем Head для заголовков
+import Head from 'next/head';
 
 export default function BlogPost() {
   const baseTitle = "Как справиться с ошибкой при открытии архива";
   const pageDescription = "Руководство по устранению ошибок при открытии архивов, связанных с антивирусами, устаревшим ПО и другими проблемами.";
-  const pageKeywords = "ошибки, архивы, решения, проблемы с антивирусом, устаревшее ПО";
 
   return (
     <Layout>
       <Head>
         <title>{baseTitle}</title>
         <meta name="description" content={pageDescription} />
-        <meta name="keywords" content={pageKeywords} />
       </Head>
       <main className="bg-white dark:bg-neutral-900 w-full px-4 pt-24 pb-16">
         <div className="container mx-auto flex flex-col lg:flex-row lg:justify-between lg:space-x-6">
