a6e07c68b4137b6aacf7b7753db83257a427504c
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 03:41:00 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,28 @@
 // /src/components/Layout.tsx
 
 import Head from "next/head";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 interface LayoutProps {
   children: ReactNode;
   title?: string;
+  description?: string;
+  keywords?: string;
+  image?: string;
+  type?: string;
 }
 
-const Layout = ({ children, title = "SmartDiag - Ваш проводник в мире автодиагностики" }: LayoutProps) => {
+const Layout = ({ children, title, description, keywords, image, type }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>{title}</title>
-        {/* Здесь можно добавить другие мета-теги */}
+        <title>{title || "SmartDiag - Ваш проводник в мире автодиагностики"}</title>
+        {description && <meta name="description" content={description} />}
+        {keywords && <meta name="keywords" content={keywords} />}
+        {image && <meta property="og:image" content={image} />}
+        {type && <meta property="og:type" content={type} />}
       </Head>
       <Navbar />
       <main>{children}</main>
