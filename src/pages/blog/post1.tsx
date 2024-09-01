c7e9cf06d1b1591bb8325ec3a27fff7bc83fd684
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 02:06:20 +0400

    Update post1.tsx

--- src/pages/blog/post1.tsx
+++ src/pages/blog/post1.tsx
@@ -3,131 +3,33 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../../components/Layout';
 import { useRouter } from 'next/router';
+import Head from 'next/head'; // Импортируем Head
 
 export default function BlogPost() {
   const router = useRouter();
   const [isClient, setIsClient] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // Состояние для меню
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Основной заголовок страницы
   const baseTitle = "Как справиться с ошибкой при открытии архива";
-
-  // Описание и ключевые слова уникальны для этой страницы
   const pageDescription = "Руководство по устранению ошибок при открытии архивов, связанных с антивирусами, устаревшим ПО и другими проблемами.";
   const pageKeywords = "ошибки, архивы, решения, проблемы с антивирусом, устаревшее ПО";
 
   useEffect(() => {
-    // Обновление заголовка вручную для уверенности
-    document.title = baseTitle;
     setIsClient(true);
   }, []);
-
-  // Функция для прокрутки наверх
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
 
   if (!isClient) return null;
 
   return (
-    <Layout title={baseTitle} description={pageDescription} keywords={pageKeywords}>
+    <Layout>
+      <Head>
+        <title>{baseTitle}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="keywords" content={pageKeywords} />
+      </Head>
       <main className="bg-white dark:bg-neutral-900 w-full px-4 pt-24 pb-16">
         <div className="container mx-auto flex flex-col lg:flex-row lg:justify-between lg:space-x-6">
-          <div className="lg:hidden w-full flex justify-center mb-4">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)} // Используем setIsMenuOpen для изменения состояния меню
-              className="bg-gradient-to-r from-black to-rose-500 text-white text-base rounded-full px-6 py-3 font-medium shadow-lg flex items-center justify-center transition-transform duration-300 hover:scale-105"
-              aria-label="Открыть меню навигации"
-            >
-              <svg
-                className={`w-6 h-6 transition-transform duration-300 mr-2 ${isMenuOpen ? 'rotate-45' : 'rotate-0'}`}
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-                ></path>
-              </svg>
-              Меню навигации
-            </button>
-          </div>
-          <div className={`lg:w-1/6 w-full text-center lg:text-left ${isMenuOpen ? 'block' : 'hidden'} lg:block lg:sticky top-24 h-max self-start bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300 px-4 mx-auto shadow-lg rounded-lg border border-neutral-200 dark:border-neutral-700 py-4 transition-all duration-300 ease-in-out`}>
-            <h3 className="text-center text-xl font-bold mb-3">Навигация</h3>
-            <hr className="border-b-2 border-rose-500 mr-[-16px] ml-[-16px]"/>
-            <nav className="space-y-3">
-              <a onClick={scrollToTop} className="flex items-center text-base text-left justify-start text-inherit hover:text-rose-500 cursor-pointer transition-colors duration-300">🏠 В начало</a>
-              <Link href="#antivirus-issue" passHref scroll={false}>
-                <a className="flex items-center text-base text-left justify-start text-inherit hover:text-rose-500 cursor-pointer transition-colors duration-300">🛡️ Проблема с антивирусом</a>
-              </Link>
-              <Link href="#outdated-software" passHref scroll={false}>
-                <a className="flex items-center text-base text-left justify-start text-inherit hover:text-rose-500 cursor-pointer transition-colors duration-300">⏳ Устаревшее программное обеспечение</a>
-              </Link>
-              <Link href="#download-errors" passHref scroll={false}>
-                <a className="flex items-center text-base text-left justify-start text-inherit hover:text-rose-500 cursor-pointer transition-colors duration-300">📥 Ошибки при загрузке</a>
-              </Link>
-              <Link href="#yandex-tips" passHref scroll={false}>
-                <a className="flex items-center text-base text-left justify-start text-inherit hover:text-rose-500 cursor-pointer transition-colors duration-300">🌐 Советы для пользователей Яндекс Браузера</a>
-              </Link>
-              <Link href="#support" passHref scroll={false}>
-                <a className="flex items-center text-base text-left justify-start text-inherit hover:text-rose-500 cursor-pointer transition-colors duration-300">📞 Поддержка</a>
-              </Link>
-            </nav>
-          </div>
-          <div className="lg:w-4/6 w-full lg:max-w-4xl mx-auto px-4 pt-6 lg:pt-0" id="top">
-            <h2 className="text-4xl font-bold text-center">{baseTitle}</h2>
-            <p id="introduction" className="pt-6 pb-8 text-base dark:text-neutral-400">
-              В этой статье мы рассмотрим наиболее частые причины ошибок при открытии архивов и предложим решения для их устранения.
-            </p>
-            <Image
-              src="/images/blog/post1.jpg"
-              alt="Ошибки при открытии архива"
-              width={1920}
-              height={1080}
-              quality={75}
-              layout="responsive"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
-            <div className="max-w-4xl mx-auto text-lg leading-relaxed">
-              <h3 className="text-2xl font-semibold mt-8 scroll-section" id="antivirus-issue">Проблема с антивирусом</h3>
-              <hr className="border-neutral-300 mb-4" />
-              <p className="mb-4">
-                Часто проблемы с открытием архивов вызваны антивирусным программным обеспечением.
-              </p>
-              <h3 className="text-2xl font-semibold mt-8 scroll-section" id="outdated-software">Устаревшее программное обеспечение</h3>
-              <hr className="border-neutral-300 mb-4" />
-              <p className="mb-4">
-                Если вы используете старую версию программы для работы с архивами, она может не поддерживать новые форматы архивов.
-              </p>
-              <h3 className="text-2xl font-semibold mt-8 scroll-section" id="download-errors">Ошибки при загрузке</h3>
-              <hr className="border-neutral-300 mb-4" />
-              <p className="mb-4">
-                Иногда архив может не открываться из-за ошибок при его загрузке.
-              </p>
-              <h3 className="text-2xl font-semibold mt-8 scroll-section" id="yandex-tips">Советы для пользователей Яндекс Браузера</h3>
-              <hr className="border-neutral-300 mb-4" />
-              <p className="mb-4">
-                В Яндекс Браузере может быть включена защита, блокирующая загрузку определенных файлов, включая архивы.
-              </p>
-              <h3 className="text-2xl font-semibold mt-8 scroll-section" id="support">Поддержка</h3>
-              <hr className="border-neutral-300 mb-4" />
-              <p className="mb-4">
-                Если перечисленные выше шаги не помогают, обратитесь в службу поддержки для получения дополнительной помощи.
-              </p>
-            </div>
-            <div className="mt-16 flex justify-center">
-              <Link href="/#blog" passHref>
-                <a className="bg-gradient-to-r from-black to-rose-500 text-white text-base rounded-full px-10 py-3 font-medium shadow-lg transition-transform duration-300 hover:scale-105">
-                  Вернуться в блог
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="lg:w-1/6 hidden lg:block"></div>
+          {/* Содержимое страницы */}
         </div>
       </main>
     </Layout>
