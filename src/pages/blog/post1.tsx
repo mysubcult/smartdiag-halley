8756fd43f2eaec717e0bfd527c096ce151c1f8c7
Artyom Gorin <mysubcult1@gmail.com>
2024-09-01 08:48:10 +0400

    Update post1.tsx

--- src/pages/blog/post1.tsx
+++ src/pages/blog/post1.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 
 export default function BlogPost() {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Добавляем состояние для меню навигации
   const [pageTitle, setPageTitle] = useState('Как справиться с ошибкой при открытии архива');
 
   const baseTitle = "Как справиться с ошибкой при открытии архива";
@@ -56,7 +57,7 @@ export default function BlogPost() {
         <div className="container mx-auto flex flex-col lg:flex-row lg:justify-between lg:space-x-6">
           <div className="lg:hidden w-full flex justify-center mb-4">
             <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)} // Убедитесь, что isMenuOpen объявлен
+              onClick={() => setIsMenuOpen(!isMenuOpen)} // Управление состоянием меню
               className="bg-gradient-to-r from-black to-rose-500 text-white text-base rounded-full px-6 py-3 font-medium shadow-lg flex items-center justify-center transition-transform duration-300 hover:scale-105"
               aria-label="Открыть меню навигации"
             >
