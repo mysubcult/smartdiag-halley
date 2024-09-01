05133c6176b025ddf97c44ac3ea22ab99d4ce21a
Artyom Gorin <mysubcult1@gmail.com>
2024-09-01 08:14:55 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,14 +13,31 @@ interface LayoutProps {
   type?: string;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, title, description, keywords, image, type }: LayoutProps) => {
   const router = useRouter();
+
+  const meta = {
+    title: title || "SmartDiag - Ваш проводник в мире автодиагностики",
+    description: description || "SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке.",
+    keywords: keywords || "автодиагностика, Autocom CDP+, Delphi DS150E, VCDS, Вася, mucar, thinkdiag, Thinkcar, диагностика автомобилей, программы для диагностики, оборудование для диагностики, car diagnostics, diagnostic tools, software for diagnostics, diagnostic equipment, vehicle diagnostics, diagnostic software, installation instructions, BMW, Audi, Mercedes, Toyota, Volkswagen, Ford, Nissan, Honda, Chevrolet, Kia",
+    image: image || "/images/seo/halley-banner.png",
+    type: type || "website",
+  };
 
   return (
     <>
-      {/* Убираем глобальный компонент Head, чтобы избежать конфликта с заголовками страниц */}
-      {/* Заменяем Head на использование в конкретных страницах, например, в post1.txt */}
-      
+      {/* Устанавливаем мета-теги только если они не указаны на уровне страницы */}
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <meta name="keywords" content={meta.keywords} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.image} />
+        <link rel="canonical" href={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
+      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
