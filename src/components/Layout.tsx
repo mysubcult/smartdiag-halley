2dff54af18602ab454991491855003da8bbe39f2
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 02:51:21 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,4 @@
 // Layout.txt
-import Head from "next/head";
-import { useRouter } from "next/router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import { ReactNode } from "react";
@@ -10,18 +8,8 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const router = useRouter();
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || '';
-
   return (
     <>
-      <Head>
-        {/* Общие мета-теги */}
-        <meta property="og:url" content={`${siteUrl}${router.asPath}`} />
-        <link rel="canonical" href={`${siteUrl}${router.asPath}`} />
-        <meta name="robots" content="index, follow" />
-        <meta name="author" content="SmartDiag Team" />
-      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
