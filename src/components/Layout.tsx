e81f94a8554f1d23aae9c6a21a2aaae058e3ea28
Artyom Gorin <mysubcult1@gmail.com>
2024-09-01 09:11:50 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
@@ -15,14 +15,8 @@ interface LayoutProps {
 
 const Layout = ({ children, title, description, keywords, image, type }: LayoutProps) => {
   const router = useRouter();
-  const defaultTitle = "SmartDiag - Ваш проводник в мире автодиагностики";
-
-  const metaTitle = title || defaultTitle;
-
-  // Не устанавливаем document.title в Layout
-  // Заголовок будет устанавливаться только компонентом BlogPost или другим компонентом
-
   const meta = {
+    title: title || "SmartDiag - Ваш проводник в мире автодиагностики",
     description: description || "SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке.",
     keywords: keywords || "автодиагностика, Autocom CDP+, Delphi DS150E, VCDS, Вася, mucar, thinkdiag, Thinkcar, диагностика автомобилей, программы для диагностики, оборудование для диагностики, car diagnostics, diagnostic tools, software for diagnostics, diagnostic equipment, vehicle diagnostics, diagnostic software, installation instructions, BMW, Audi, Mercedes, Toyota, Volkswagen, Ford, Nissan, Honda, Chevrolet, Kia",
     image: image || "/images/seo/halley-banner.png",
@@ -32,12 +26,12 @@ const Layout = ({ children, title, description, keywords, image, type }: LayoutP
   return (
     <>
       <Head>
-        <title>{metaTitle}</title> {/* Оставляем установку title через Head */}
+        <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
         <meta name="keywords" content={meta.keywords} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
-        <meta property="og:title" content={metaTitle} />
+        <meta property="og:title" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:image" content={meta.image} />
         <link rel="canonical" href={`${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`} />
