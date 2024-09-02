589644705972703582bfd1d68e81394aaa98001f
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 15:11:20 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,72 @@
-// src/components/Navbar.tsx
-
-import React, { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitchButton from "./ThemeSwitchButton";
 import { ChevronDownIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/router";
 
-interface NavbarProps {
-  onNavigateTo: (section: string) => void;
-}
-
+// Навигационные ссылки
 const navigation = [
-  { name: "Главная", anchor: "hero" },
-  { name: "Программы", anchor: "soft" },
-  { name: "Блог", anchor: "faq" },
-  { name: "О нас", anchor: "services" },
-  { name: "Обратная связь", anchor: "contact" },
+  { name: "Главная", href: "/", anchor: "#hero" },
+  { name: "Программы", href: "/#soft", anchor: "#soft" },
+  { name: "Блог", href: "/#blog", anchor: "#blog" },
+  { name: "О нас", href: "/#services", anchor: "#services" },
+  { name: "Обратная связь", href: "/#contact", anchor: "#contact" },
 ];
 
-const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
+// Комбинирование классов
+function classNames(...classes: string[]): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
   const [fontSize, setFontSize] = useState("18px");
   const [isMobileView, setIsMobileView] = useState(false);
 
-  const handleNavigationClick = (anchor: string) => (event: React.MouseEvent) => {
-    event.preventDefault();
-    onNavigateTo(anchor);
-    setIsMenuOpen(false);
-  };
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 1200);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleNavigationClick = useCallback(
+    (anchor: string) => (event: React.MouseEvent) => {
+      event.preventDefault();
+      if (router.pathname !== '/') {
+        router.push('/').then(() => router.push(anchor, undefined, { scroll: false }));
+      } else {
+        router.push(anchor, undefined, { scroll: false });
+      }
+      setIsMenuOpen(false);
+    },
+    [router]
+  );
+
+  const updateFontSize = useCallback(() => {
+    const baseFontSize = 18;
+    const minFontSize = 14;
+    const screenHeight = window.innerHeight;
+    const itemsCount = isSubMenuOpen ? navigation.length + 3 : navigation.length;
+    const maxMenuHeight = screenHeight - 64;
+    const requiredHeight = itemsCount * 48;
+
+    const scaleFactor = maxMenuHeight / requiredHeight;
+    setFontSize(`${Math.max(minFontSize, baseFontSize * Math.min(scaleFactor, 1))}px`);
+  }, [isSubMenuOpen]);
+
+  useEffect(() => {
+    updateFontSize();
+    window.addEventListener("resize", updateFontSize);
+    return () => window.removeEventListener("resize", updateFontSize);
+  }, [updateFontSize]);
 
   return (
     <nav className="navbar fixed top-0 left-0 right-0 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-400 border-b border-neutral-200 dark:border-neutral-700 backdrop-blur-sm bg-white/90 dark:bg-neutral-900/80 z-20">
@@ -36,7 +74,7 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <Link href="/" scroll={false}>
+              <Link href="/" scroll={false} onClick={handleNavigationClick("#hero")}>
                 <Image
                   className="block h-12 w-auto logo-animation"
                   src="/images/logos/logo.png"
@@ -50,27 +88,30 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
               </Link>
             </div>
 
+            {/* Горизонтальное меню навигации */}
             <div className={`${isMobileView ? "hidden" : "flex"} navbar-nav`}>
               <div className="flex space-x-5 items-center">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    onClick={handleNavigationClick(item.anchor)}
-                    className="text-neutral-900 dark:text-neutral-400 hover:text-red-500"
+                    href={item.href}
+                    className={classNames("text-neutral-900 dark:text-neutral-400", "nav-link")}
                     style={{ textDecoration: "none" }}
+                    scroll={false}
+                    onClick={handleNavigationClick(item.anchor)}
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
           </div>
 
+          {/* Кнопки магазинов, смены темы и меню */}
           <div className="flex items-center gap-2">
             {!isMobileView && (
               <>
-                {/* Кнопки магазинов */}
-                <Link href="https://www.ozon.ru/seller/smartdiag-862410/" target="_blank" rel="noopener noreferrer">
+                <Link href="https://www.ozon.ru/seller/smartdiag-862410/" target="_blank" rel="noopener noreferrer" className="block">
                   <button className="btn-ozon">
                     <Image
                       src="/images/logos/favicon.ico"
@@ -83,7 +124,8 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
                     OZON
                   </button>
                 </Link>
-                <Link href="https://market.yandex.ru/business--smartdiag/50025236" target="_blank" rel="noopener noreferrer">
+
+                <Link href="https://market.yandex.ru/business--smartdiag/50025236" target="_blank" rel="noopener noreferrer" className="block">
                   <button className="btn-yandex">
                     <Image
                       src="https://yastatic.net/market-export/_/i/favicon/ymnew/favicon.ico"
@@ -96,7 +138,8 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
                     Яндекс Маркет
                   </button>
                 </Link>
-                <Link href="https://www.wildberries.ru/seller/1343369" target="_blank" rel="noopener noreferrer">
+
+                <Link href="https://www.wildberries.ru/seller/1343369" target="_blank" rel="noopener noreferrer" className="block">
                   <button className="btn-wildberries">
                     <Image
                       src="/images/logos/favicon.ico"
@@ -112,8 +155,10 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
               </>
             )}
 
+            {/* Кнопка смены темы */}
             <ThemeSwitchButton />
 
+            {/* Кнопка меню появляется, когда isMobileView == true */}
             {isMobileView && (
               <div className="flex items-center">
                 <button
@@ -142,6 +187,7 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
         </div>
       </div>
 
+      {/* Popup Menu */}
       {isMenuOpen && (
         <div
           className="mobile-menu bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 rounded-xl shadow-lg p-4 absolute right-4 top-20 w-64 z-30"
@@ -155,13 +201,15 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
         >
           <div className="flex flex-col items-center justify-center space-y-4">
             {navigation.map((item) => (
-              <a
+              <Link
                 key={item.name}
+                href={item.href}
+                className={classNames("text-neutral-900 dark:text-neutral-400", "block py-2 text-lg font-medium hover:text-red-500")}
+                scroll={false}
                 onClick={handleNavigationClick(item.anchor)}
-                className="text-neutral-900 dark:text-neutral-400 block py-2 text-lg font-medium hover:text-red-500"
               >
                 {item.name}
-              </a>
+              </Link>
             ))}
             <div className="flex flex-col items-center w-full mt-4">
               <button
@@ -170,21 +218,62 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
               >
                 Магазины
                 <ChevronDownIcon
-                  className={`h-5 w-5 transition-transform transform ${
+                  className={`h-5 w-5 ml-2 transition-transform ${
                     isSubMenuOpen ? "rotate-180" : "rotate-0"
                   }`}
                 />
               </button>
               {isSubMenuOpen && (
-                <div className="flex flex-col items-center w-full mt-2">
-                  <Link href="https://www.ozon.ru/seller/smartdiag-862410/" target="_blank" rel="noopener noreferrer">
-                    <button className="btn-mobile-submenu py-2 px-4 text-lg font-medium">OZON</button>
+                <div className="submenu mt-2 space-y-3 w-full">
+                  <Link
+                    href="https://www.ozon.ru/seller/smartdiag-862410/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-ozon flex items-center justify-center w-full mx-auto px-4 py-3 rounded-lg hover:bg-blue-500 transition-colors"
+                  >
+                    <Image
+                      src="/images/logos/favicon.ico"
+                      alt="OZON"
+                      className="w-4 h-4 mr-2"
+                      width={16}
+                      height={16}
+                      loading="lazy"
+                    />
+                    OZON
                   </Link>
-                  <Link href="https://market.yandex.ru/business--smartdiag/50025236" target="_blank" rel="noopener noreferrer">
-                    <button className="btn-mobile-submenu py-2 px-4 text-lg font-medium">Яндекс Маркет</button>
+
+                  <Link
+                    href="https://market.yandex.ru/business--smartdiag/50025236"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-yandex flex items-center justify-center w-full mx-auto px-4 py-3 rounded-lg hover:bg-orange-500 transition-colors"
+                  >
+                    <Image
+                      src="https://yastatic.net/market-export/_/i/favicon/ymnew/favicon.ico"
+                      alt="Яндекс Маркет"
+                      className="w-4 h-4 mr-2"
+                      width={16}
+                      height={16}
+                      loading="lazy"
+                    />
+                    Яндекс Маркет
                   </Link>
-                  <Link href="https://www.wildberries.ru/seller/1343369" target="_blank" rel="noopener noreferrer">
-                    <button className="btn-mobile-submenu py-2 px-4 text-lg font-medium">Wildberries</button>
+
+                  <Link
+                    href="https://www.wildberries.ru/seller/1343369"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-wildberries flex items-center justify-center w-full mx-auto px-4 py-3 rounded-lg hover:bg-purple-500 transition-colors"
+                  >
+                    <Image
+                      src="/images/logos/favicon.ico"
+                      alt="Wildberries"
+                      className="w-4 h-4 mr-2"
+                      width={16}
+                      height={16}
+                      loading="lazy"
+                    />
+                    Wildberries
                   </Link>
                 </div>
               )}
@@ -194,6 +283,4 @@ const Navbar: React.FC<NavbarProps> = ({ onNavigateTo }) => {
       )}
     </nav>
   );
-};
-
-export default Navbar;
+}
