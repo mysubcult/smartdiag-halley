a287d0c405d34f330ccdf7a451bf9847b13cbaa1
Artyom Gorin <mysubcult1@gmail.com>
2024-09-02 04:09:37 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,5 @@
-// /src/components/Contact.tsx
-
-import React, { useState, useCallback } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import PrivacyPolicyModal from './PrivacyPolicyModal';
 import Image from 'next/image';
 
@@ -26,20 +24,25 @@ export default function Contact() {
     handleSubmit,
     reset,
     formState: { errors, isSubmitSuccessful, isSubmitting },
-  } = useForm<Info>({ mode: 'onSubmit' });
+  } = useForm<Info>({
+    mode: 'onSubmit',
+  });
 
   const topic = watch('topic');
 
-  const getMessagePlaceholder = useCallback((topic: string) => {
+  const getMessagePlaceholder = (topic: string) => {
     const placeholders: { [key: string]: string } = {
-      'Активация прибора': 'Введите ваше сообщение. В зависимости от типа прибора, необходимо предоставить соответствующую информацию, такую как номер заказа, серийный номер, ACTIVATION ID и т.п.',
-      'Помощь с установкой ПО': 'Введите ваше сообщение. Уточните модель прибора и удобное для вас время, когда мы можем связаться с вами для дистанционной установки ПО.',
-      'Заказ оборудования': 'Мы рады предложить вам разнообразный ассортимент приборов, доступных как с наших складов в Москве, так и напрямую от лучших поставщиков из Китая. У нас есть всё, что вам может понадобиться, и это по лучшим ценам. Напишите нам, чтобы уточнить подробности.',
+      'Активация прибора':
+        'Введите ваше сообщение. В зависимости от типа прибора, необходимо предоставить соответствующую информацию, такую как номер заказа, серийный номер, ACTIVATION ID и т.п.',
+      'Помощь с установкой ПО':
+        'Введите ваше сообщение. Уточните модель прибора и удобное для вас время, когда мы можем связаться с вами для дистанционной установки ПО.',
+      'Заказ оборудования':
+        'Мы рады предложить вам разнообразный ассортимент приборов, доступных как с наших складов в Москве, так и напрямую от лучших поставщиков из Китая. У нас есть всё, что вам может понадобиться, и это по лучшим ценам. Напишите нам, чтобы уточнить подробности.',
     };
     return placeholders[topic] || 'Введите ваше сообщение';
-  }, []);
+  };
 
-  const onSubmit: SubmitHandler<Info> = async (data, e) => {
+  const onSubmit = async (data: Info, e: any) => {
     try {
       const response = await fetch('https://api.web3forms.com/submit', {
         method: 'POST',
@@ -47,21 +50,21 @@ export default function Contact() {
           'Content-Type': 'application/json',
           Accept: 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data, null, 2),
       });
       const json = await response.json();
-      setModalState((prevState) => ({ ...prevState, isSuccess: json.success }));
       if (json.success) {
-        e?.target.reset();
+        setModalState({ ...modalState, isSuccess: true });
+        e.target.reset();
         reset();
+      } else {
+        setModalState({ ...modalState, isSuccess: false });
       }
     } catch (error) {
-      setModalState((prevState) => ({ ...prevState, isSuccess: false }));
+      setModalState({ ...modalState, isSuccess: false });
       console.error(error);
     }
   };
-
-  const toggleModal = () => setModalState((prevState) => ({ ...prevState, isModalOpen: !prevState.isModalOpen }));
 
   return (
     <div className="bg-white-50 dark:bg-neutral-900" id="contact">
@@ -78,15 +81,60 @@ export default function Contact() {
           <p className="max-w mt-4 mb-4 dark:text-neutral-400 text-center">
             🕑 <strong>График работы технической поддержки:</strong>
             <br />
-            <span className="block mt-2">Понедельник - Пятница: 10:00 - 19:00 (МСК)</span>
-            <span className="block mt-2">Суббота - Воскресенье: 10:00 - 18:00 (МСК)</span>
+            <span className="block mt-2">
+              Понедельник - Пятница: 10:00 - 19:00 (МСК)
+            </span>
+            <span className="block mt-2">
+              Суббота - Воскресенье: 10:00 - 18:00 (МСК)
+            </span>
             <br />
             💬 <strong>Мы доступны в:</strong>
           </p>
+          {/* Контактная информация */}
           <div className="flex justify-center mt-2 text-dark-600 dark:text-neutral-400">
-            <div className="m-2"><a href="https://wa.me/79991234567" target="_blank" className="text-green-500 hover:text-green-600">WhatsApp</a></div>
-            <div className="m-2"><a href="viber://chat?number=+79991234567" target="_blank" className="text-purple-500 hover:text-purple-600">Viber</a></div>
-            <div className="m-2"><a href="tg://resolve?domain=smartdiag" target="_blank" className="text-blue-500 hover:text-blue-600">Telegram</a></div>
+            <ContactInfo
+              href="https://смартдиаг-поддержка.рф/telegram"
+              imageSrc="/images/hero/telegram-qr.svg"
+              alt="QR Code Telegram"
+              platform="Telegram"
+              icon={
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  style={{
+                    fillRule: 'evenodd',
+                    clipRule: 'evenodd',
+                    strokeLinejoin: 'round',
+                    strokeMiterlimit: '1.41421',
+                    fill: 'currentColor',
+                  }}
+                >
+                  <path
+                    id="telegram-1"
+                    d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
+                  />
+                </svg>
+              }
+              hoverColor="hover:text-blue-500"
+            />
+            <ContactInfo
+              href="https://смартдиаг-поддержка.рф/whatsapp"
+              imageSrc="/images/hero/whatsapp-qr.svg"
+              alt="QR Code WhatsApp"
+              platform="WhatsApp"
+              icon={
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  style={{ fill: 'currentColor' }}
+                >
+                  <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+                </svg>
+              }
+              hoverColor="hover:text-green-500"
+            />
           </div>
           <div className="mt-4 text-center dark:text-neutral-400">
             📧 <strong>Email отдела продаж:</strong>
@@ -113,8 +161,8 @@ export default function Contact() {
               <input type="hidden" value="c39d2187-6537-4c0b-87e1-3cff0bf0c1c3" {...register('access_key')} />
               <input type="hidden" value="Обращение через форму обратной связи на сайте SmartDiag" {...register('subject')} />
               <input type="hidden" value="SmartDiag" {...register('from_name')} />
-              <input type="checkbox" className="hidden" style={{ display: 'none' }} {...register('botcheck')} />
-
+              <input type="checkbox" id="" className="hidden" style={{ display: 'none' }} {...register('botcheck')}></input>
+              
               <InputField
                 type="text"
                 placeholder="Введите Ваше имя"
@@ -130,7 +178,7 @@ export default function Contact() {
                 error={errors.email}
                 register={register}
                 name="email"
-                validation={{
+                validation={{ 
                   required: 'Необходимо ввести Ваш Email',
                   pattern: { value: /^\S+@\S+$/i, message: 'Введите, пожалуйста, правильный адрес электронной почты' },
                 }}
@@ -185,7 +233,10 @@ export default function Contact() {
                   <span className="text-sm">
                     Я прочитал и согласен с{' '}
                     <span
-                      onClick={toggleModal}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setModalState({ ...modalState, isModalOpen: true });
+                      }}
                       className="text-red-600 hover:underline cursor-pointer"
                     >
                       правилами на обработку персональных данных
@@ -214,12 +265,15 @@ export default function Contact() {
             </form>
           )}
 
-          {isSubmitSuccessful && modalState.isSuccess && <SuccessMessage onReset={reset} />}
+          {isSubmitSuccessful && modalState.isSuccess && (
+            <SuccessMessage onReset={reset} />
+          )}
 
-          {isSubmitSuccessful && !modalState.isSuccess && <ErrorMessage onReset={reset} />}
+          {isSubmitSuccessful && !modalState.isSuccess && (
+            <ErrorMessage onReset={reset} />
+          )}
         </div>
       </div>
-
       <PrivacyPolicyModal
         isOpen={modalState.isModalOpen}
         onClose={() => setModalState({ ...modalState, isModalOpen: false })}
@@ -227,6 +281,33 @@ export default function Contact() {
     </div>
   );
 }
+
+const ContactInfo = ({ href, imageSrc, alt, platform, icon, hoverColor }: { href: string; imageSrc: string; alt: string; platform: string; icon: JSX.Element; hoverColor: string }) => (
+  <div className="flex flex-col items-center space-y-2 mx-4">
+    <div className="flex items-center space-x-2">
+      {icon}
+      <a
+        href={href}
+        target="_blank"
+        className={`transition-colors duration-300 ${hoverColor}`}
+        style={{ width: '90px' }}
+      >
+        {platform}
+      </a>
+    </div>
+    <div>
+      <a href={href} target="_blank">
+        <Image
+          src={imageSrc}
+          alt={alt}
+          className="rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
+          width={120}
+          height={120}
+        />
+      </a>
+    </div>
+  </div>
+);
 
 const InputField = ({ type, placeholder, error, register, name, validation }: any) => (
   <div className="mb-4 mt-4">
