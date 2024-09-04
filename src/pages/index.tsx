4948b229f757dcd13f63eafb415b97dac263af94
Artyom Gorin <mysubcult1@gmail.com>
2024-09-04 15:44:03 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,10 @@ import { Hero } from "../components/Hero";
 import Layout from "../components/Layout";
 import Faq from "../components/Faq";
 import { Services } from "../components/Services";
-import Head from "next/head"; // Importing Head for meta tags
 
 export default function Home() {
-  const pageTitle = "SmartDiag - Ваш проводник в мире автодиагностики";
-
   return (
-    <Layout>
-      <Head>
-        <title>{pageTitle}</title>
-        <meta name="description" content="SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке." />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content="SmartDiag предлагает широкий ассортимент оборудования для диагностики автомобилей, включая Autocom CDP+, Delphi DS150E, VCDS. Программы и инструкции по установке." />
-      </Head>
+    <Layout title="SmartDiag - Ваш проводник в мире автодиагностики">
       <Hero />
       <Soft />
       <Faq />
