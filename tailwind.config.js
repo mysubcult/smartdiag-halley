cdd309040d52a0261e4187e6e40f4abe3a451508
Artyom Gorin <mysubcult1@gmail.com>
2024-09-07 22:45:28 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "class", // Вы можете использовать 'media' или 'class'
   theme: {
     extend: {
       fontFamily: {
@@ -18,10 +15,8 @@ module.exports = {
       },
     },
   },
-  variants: {
-    typography: ["dark"],
-  },
   plugins: [
-    require("@tailwindcss/typography")
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"), // Плагин для обрезки текста
   ],
 };
