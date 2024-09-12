8f743714ea42b624bab5449246e1b0cfe3923027
Artyom Gorin <mysubcult1@gmail.com>
2024-09-12 15:11:04 +0400

    Update autocom2021.tsx

--- src/pages/articles/software/autocom2021.tsx
+++ src/pages/articles/software/autocom2021.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import Layout from '../../components/Layout';
+import Layout from '../../../components/Layout';
 import { useRouter } from 'next/router';
 
 export default function BlogPost() {
