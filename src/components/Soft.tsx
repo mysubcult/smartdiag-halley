fe3175ee95043826021e38f0ff9faef04218cd6c
Artyom Gorin <mysubcult1@gmail.com>
2024-09-13 09:41:55 +0400

    Update Soft.tsx

--- src/components/Soft.tsx
+++ src/components/Soft.tsx
@@ -16,183 +16,14 @@ interface Product {
 }
 
 const products: Product[] = [
-{
-    title: "Delphi DS150e",
-    description: "Многофункциональный диагностический инструмент для легковых и грузовых автомобилей.",
-    features: ["Delphi 2021.10b, Delphi + Delphi 2020.23", "Инструкции по установке ПО", "Руководство пользователя"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/BiaqYzKpxZRTc58", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/8MZQfLRjSy9z4Bk", label: "Скачать с сервера 2" }
-    ],
-    mostPopular: false,
-    docs: true,
-    docsLinks: [
-      { link: "https://i.getspace.us/cloud/s/7BwyBJf2YHxEkaC", label: "Инструкция по установке Delphi 2020.23" },
-      { link: "https://i.getspace.us/cloud/s/qJRfJdgjsqkPxme", label: "Инструкция по установке Delphi 2021.10b" }
-    ],
-    type: "мультимарочные"
-  },
-  {
-    title: "Autocom CDP+",
-    description: "Универсальный диагностический сканер для чтения и удаления кодов неисправностей.",
-    features: ["Autocom 2021.11, Delphi + Autocom 2020.23", "Инструкции по установке ПО", "Руководство пользователя"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/S9CKwWMNDbeB2XH", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/XbJnfSYNiw3dzFm", label: "Скачать с сервера 2" }
-    ],
-    mostPopular: true,
-    docs: true,
-    docsLinks: [
-      { link: "https://i.getspace.us/cloud/s/xdr4QZqwsR6k8rr", label: "Инструкция по установке Autocom 2020.23" },
-      { link: "https://i.getspace.us/cloud/s/bbRzaksyH6LkSg4", label: "Инструкция по установке Autocom 2021.11" },
-    ],
-    type: "мультимарочные"
-  },
-  {
-    title: "Wurth WoW Snooper+",
-    description: "Инструмент диагностики автомобилей для чтения и удаления кодов неисправностей.",
-    features: ["Wurth WoW 5.00.8", "Инструкция по установке ПО", "Руководство пользователя"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/eTR2gqbEbZi66Md", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/KCCEPPDbpb7j7oJ", label: "Скачать с сервера 2" },
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "мультимарочные"
-  },
-  {
-    title: "MUCAR BT200/Thinkcar Mini/Thinkdiag",
-    description: "Универсальные мобильные приборы для диагностики автомобилей.",
-    features: ["Diagzone", "ProDiag", "X-DIAG", "X-PRO5"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/fAMr3QsBMekwR2n", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/T6c4C7Gj5Me3mGF", label: "Скачать с сервера 2" },
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "мультимарочные"
-  },
-  {
-    title: "Galletto 1260",
-    description: "Универсальный программатор для чип-тюнинга, чтения и удаления кодов неисправностей.",
-    features: ["Galletto 1260", "Драйвер"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/dfYejQP9rZGK9Td", label: "Скачать с сервера 1" }
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "мультимарочные"
-  },
-  {
-    title: "VCDS + Вася",
-    description: "Диагностический инструмент для автомобилей Volkswagen Group.",
-    features: ["Вася, VCDS", "Инструкции по установке ПО", "Сборники кодировок", "Видеокурсы"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/R7ycKecn9P6b55a", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/mnBd982CC52NEko", label: "Скачать с сервера 2" },
-    ],
-    mostPopular: true,
-    docs: true,
-    docsLinks: [
-      { link: "https://i.getspace.us/cloud/s/bmi7a7zdHbXHMnB", label: "Инструкция 1" },
-    ],
-    type: "марочные"
-  },
-  {
-    title: "BMW E-NET (E-Sys)",
-    description: "Диагностическая система для автомобилей BMW.",
-    features: ["E-SYS", "Rheingold", "ISTA+", "ISTA-P", "Инструкции по установке ПО"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/jiiandKXdi6BEJS", label: "Скачать с сервера 1" }
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "марочные"
-  },
-  {
-    title: "K-Dcan INPA",
-    description: "Диагностическая система для автомобилей BMW.",
-    features: ["Rheingold", "INPA", "ISTA-D", "DIS", "NCS", "Инструкции по установке ПО"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/jiiandKXdi6BEJS", label: "Скачать с сервера 1" }
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "марочные"
-  },
-  {
-    title: "ELS 27",
-    description: "Диагностический интерфейс для автомобилей Ford и Mazda.",
-    features: ["FORScan", "FoCCCus", "ELMConfig", "Инструкции по установке ПО"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/oBNcC2w85wnj2Lx", label: "Скачать с сервера 1" }
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "марочные"
-  },
-  {
-    title: "Mini-VCI",
-    description: "Диагностический инструмент для автомобилей Toyota и Lexus.",
-    features: ["Techstream ", "Инструкция по установке ПО"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/Q3kWQ8ajB8WdF5g", label: "Скачать с сервера 1" }
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "марочные"
-  },
-  {
-    title: "Lexia 3/PP2000",
-    description: "Диагностический инструмент для автомобилей Peugeot и Citroen.",
-    features: ["Diagbox", "Инструкция по установке ПО"],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/eBmZpZWza2kt2Dc", label: "Скачать с сервера 1" }
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "марочные"
-  },
-  {
-    title: "ELM 327 Mini",
-    description: "Универсальный диагностический сканер для автомобилей, который подключается к порту OBD-II и работает через приложение на смартфоне или компьютере.",
-    features: ["EOBD Facile", "Car Scanner", "Torque", "ELMScan", "Carista", "BimmerCode", "LeafSpy", "и т.д."],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/Xg9rLCQgfZbedxe", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/7jirqk7RWaqYwCM", label: "Скачать с сервера 1" },
-    ],
-    mostPopular: true,
-    docs: false,
-    docsLinks: [],
-    type: "адаптеры elm"
-  },
-  {
-    title: "Kingbolen ELM",
-    description: "Диагностический инструмент для автомобилей, оснащенный функцией Bluetooth/Wi-Fi и поддерживающий различные протоколы OBD-II, что позволяет работать с разными автомобильными брендами.",
-    features: ["EOBD Facile", "Car Scanner", "Torque", "ELMScan", "Carista", "BimmerCode", "LeafSpy", "и т.д."],
-    downloadLinks: [
-      { link: "https://i.getspace.us/cloud/s/Xg9rLCQgfZbedxe", label: "Скачать с сервера 1" },
-      { link: "https://nch.pl/s/7jirqk7RWaqYwCM", label: "Скачать с сервера 1" },
-    ],
-    mostPopular: false,
-    docs: false,
-    docsLinks: [],
-    type: "адаптеры elm"
-  }
+  // Продукты здесь...
 ];
 
 const DeviceTypes: ProductType[] = ["мультимарочные", "марочные", "адаптеры elm"];
 
 export default function Soft() {
   const [selectedType, setSelectedType] = useState<ProductType>("мультимарочные");
+  const [searchTerm, setSearchTerm] = useState<string>(""); // Для строки поиска
   const [modalLinks, setModalLinks] = useState<{ link: string; label: string }[] | null>(null);
 
   const handleDownloadClick = (links: { link: string; label: string }[]) => {
@@ -218,73 +49,120 @@ export default function Soft() {
     </button>
   );
 
+  const filteredProducts = products.filter(
+    ({ title, type }) =>
+      type === selectedType &&
+      title.toLowerCase().includes(searchTerm.toLowerCase()) // Фильтр по строке поиска
+  );
+
   return (
     <div className="bg-gray-50 dark:bg-neutral-900" id="soft">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16">
         <h2 className="text-4xl font-bold text-center">Программы для оборудования 💻</h2>
-        <p className="pt-6 text-base max-w-2xl text-center m-auto dark:text-neutral-400"> 
+        <p className="pt-6 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
           В этом разделе вы можете скачать программное обеспечение для своего устройства.
-          Для начала определите тип вашего устройства — &quot;Марочный&quot; или &quot;Мультимарочный&quot;.
+          Для начала определите тип вашего устройства — "Марочный" или "Мультимарочный".
           Информацию о типе устройства вы найдёте в упаковке. После этого найдите карточку с вашим устройством
-          и нажмите кнопку &quot;Скачать&quot;. Инструкция по установке программного обеспечения находится на кнопке
-          &quot;Инструкция&quot;.
+          и нажмите кнопку "Скачать". Инструкция по установке программного обеспечения находится на кнопке
+          "Инструкция".
         </p>
       </div>
 
-      <div className="max-w-max mx-auto px-6">
-        {/* Применяем стили, как в предыдущем примере */}
-        <div className="relative text-base font-semibold mt-6 bg-neutral-200 dark:bg-neutral-800 rounded-lg inline-flex flex-col sm:flex-row sm:flex-wrap justify-center sm:mt-8 p-1 gap-1">
-          {DeviceTypes.map((type) => renderButton(type.charAt(0).toUpperCase() + type.slice(1), type))}
+      <div className="max-w-max mx-auto px-6 mt-6 sm:mt-8">
+        {/* Категории и строка поиска */}
+        <div className="relative text-base font-semibold bg-neutral-200 dark:bg-neutral-800 rounded-lg p-1 sm:mt-0 flex items-center justify-between w-full sm:w-auto">
+          {/* Категории */}
+          <div className="flex items-center w-full sm:w-auto flex-grow gap-2">
+            <div className="relative">
+              <div className="hidden sm:flex gap-2">
+                {DeviceTypes.map((type) => renderButton(type.charAt(0).toUpperCase() + type.slice(1), type))}
+              </div>
+            </div>
+
+            {/* Строка поиска для ПК */}
+            <div className="hidden sm:flex">
+              <input
+                type="text"
+                placeholder="Поиск..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-40 p-2 border rounded-md text-neutral-900 dark:text-neutral-100 bg-white dark:bg-neutral-700"
+              />
+            </div>
+          </div>
+
+          {/* Иконка лупы для мобильной версии */}
+          <button
+            className="ml-auto sm:hidden bg-transparent text-neutral-900 dark:text-neutral-100 px-4 py-2 rounded-md"
+            onClick={() => setShowSearch(!showSearch)}
+          >
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M16 10.5a5.5 5.5 0 1 0-11 0 5.5 5.5 0 0 0 11 0z" />
+            </svg>
+          </button>
+        </div>
+
+        {/* Строка поиска для мобильной версии */}
+        <div
+          className={`relative w-full sm:hidden transition-all duration-300 ${
+            showSearch ? "max-h-40" : "max-h-0"
+          } overflow-hidden`}
+        >
+          <input
+            type="text"
+            placeholder="Поиск..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full p-2 border rounded-md text-neutral-900 dark:text-neutral-100 bg-white dark:bg-neutral-700 mt-2"
+          />
         </div>
       </div>
 
-      {/* Сетка статей */}
+      {/* Сетка продуктов */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-16">
-        {products
-          .filter(({ type }) => type === selectedType)
-          .map(({ title, mostPopular, description, features, downloadLinks, docs, docsLinks }) => (
-            <div
-              key={title}
-              className={`rounded-lg py-8 relative flex flex-col ${
-                mostPopular
-                  ? "border-red-300 border-2 border-solid dark:border-red-600"
-                  : "border-neutral-300 border dark:border-neutral-600"
-              } hover:bg-neutral-100 dark:hover:bg-neutral-700 hover:shadow-lg transition-all duration-300`}
-            >
-              <h3 className="px-6 text-lg font-semibold leading-5">{title}</h3>
-              {mostPopular && (
-                <p className="mx-6 absolute top-0 px-4 py-1 -translate-y-1/2 bg-red-100 text-red-600 rounded-full text-sm font-semibold tracking-wide shadow-md">
-                  Топ продаж
-                </p>
-              )}
-              <p className="px-6 mt-4 leading-6 dark:text-neutral-400">{description}</p>
-              <div className="flex mt-4 mx-6">
+        {filteredProducts.map(({ title, mostPopular, description, features, downloadLinks, docs, docsLinks }) => (
+          <div
+            key={title}
+            className={`rounded-lg py-8 relative flex flex-col ${
+              mostPopular
+                ? "border-red-300 border-2 border-solid dark:border-red-600"
+                : "border-neutral-300 border dark:border-neutral-600"
+            } hover:bg-neutral-100 dark:hover:bg-neutral-700 hover:shadow-lg transition-all duration-300`}
+          >
+            <h3 className="px-6 text-lg font-semibold leading-5">{title}</h3>
+            {mostPopular && (
+              <p className="mx-6 absolute top-0 px-4 py-1 -translate-y-1/2 bg-red-100 text-red-600 rounded-full text-sm font-semibold tracking-wide shadow-md">
+                Топ продаж
+              </p>
+            )}
+            <p className="px-6 mt-4 leading-6 dark:text-neutral-400">{description}</p>
+            <div className="flex mt-4 mx-6">
+              <button
+                onClick={() => handleDownloadClick(downloadLinks)}
+                className="block px-6 py-3 font-medium leading-4 text-center rounded-lg bg-red-600 text-white shadow-md hover:bg-green-500 dark:hover:bg-green-500 transition-colors duration-200 ease-in-out transform active:scale-95 w-full"
+              >
+                Скачать
+              </button>
+              {docs && docsLinks.length > 0 && (
                 <button
-                  onClick={() => handleDownloadClick(downloadLinks)}
-                  className="block px-6 py-3 font-medium leading-4 text-center rounded-lg bg-red-600 text-white shadow-md hover:bg-green-500 dark:hover:bg-green-500 transition-colors duration-200 ease-in-out transform active:scale-95 w-full"
+                  onClick={() => handleDownloadClick(docsLinks)}
+                  className="ml-2 block px-3 py-3 font-small leading-4 text-center rounded-lg border-neutral-300 border dark:border-neutral-600 dark:bg-transparent dark:text-white dark:hover:bg-neutral-600 hover:bg-neutral-200 transition-colors duration-200 ease-in-out transform active:scale-95 w-full"
                 >
-                  Скачать
+                  Инструкция
                 </button>
-                {docs && docsLinks.length > 0 && (
-                  <button
-                    onClick={() => handleDownloadClick(docsLinks)}
-                    className="ml-2 block px-3 py-3 font-small leading-4 text-center rounded-lg border-neutral-300 border dark:border-neutral-600 dark:bg-transparent dark:text-white dark:hover:bg-neutral-600 hover:bg-neutral-200 transition-colors duration-200 ease-in-out transform active:scale-95 w-full"
-                  >
-                    Инструкция
-                  </button>
-                )}
-              </div>
-              <ul className="mt-6 px-6 space-y-4 flex-1 border-t border-neutral-300 dark:border-neutral-500">
-                <p className="mt-6 font-semibold dark:text-neutral-300">В комплекте:</p>
-                {features.map((feature) => (
-                  <li key={feature} className="leading-6 flex">
-                    <CheckIcon className="mt-2 w-3 h-3 text-red-600 shrink-0" />
-                    <span className="ml-3 dark:text-neutral-400">{feature}</span>
-                  </li>
-                ))}
-              </ul>
+              )}
             </div>
-          ))}
+            <ul className="mt-6 px-6 space-y-4 flex-1 border-t border-neutral-300 dark:border-neutral-500">
+              <p className="mt-6 font-semibold dark:text-neutral-300">В комплекте:</p>
+              {features.map((feature) => (
+                <li key={feature} className="leading-6 flex">
+                  <CheckIcon className="mt-2 w-3 h-3 text-red-600 shrink-0" />
+                  <span className="ml-3 dark:text-neutral-400">{feature}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
 
       {modalLinks && (
