914d8b6070078000c5c79017175fd386168aeaec
mysubcult <mysubcult1@gmail.com>
2024-09-14 17:39:41 +0400

    Добавлен GitHub Actions для автоматического развертывания

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,5 +18,7 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
 	require('@tailwindcss/forms'),
+	require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
   ],
 };
