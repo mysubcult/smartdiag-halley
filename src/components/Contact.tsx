1a73e79fb5c5217a3635e3f00ec8e51a6f08aa34
Artyom Gorin <mysubcult1@gmail.com>
2024-09-14 13:19:17 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -285,7 +285,17 @@ export default function Contact() {
   );
 }
 
-const ContactInfo = ({ href, imageSrc, alt, platform, icon, hoverColor }) => (
+// Определение компонента ContactInfo с типизацией
+interface ContactInfoProps {
+  href: string;
+  imageSrc: string;
+  alt: string;
+  platform: string;
+  icon: JSX.Element;
+  hoverColor: string;
+}
+
+const ContactInfo: React.FC<ContactInfoProps> = ({ href, imageSrc, alt, platform, icon, hoverColor }) => (
   <div className="flex flex-col items-center space-y-2 mx-4">
     <div className="flex items-center space-x-2">
       {icon}
@@ -313,7 +323,15 @@ const ContactInfo = ({ href, imageSrc, alt, platform, icon, hoverColor }) => (
   </div>
 );
 
-const InputField = ({ type, placeholder, error, register }) => (
+// Компонент InputField с типизацией
+interface InputFieldProps {
+  type: string;
+  placeholder: string;
+  error: any;
+  register: any;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ type, placeholder, error, register }) => (
   <div className="mb-4 mt-4">
     <input
       type={type}
@@ -332,7 +350,15 @@ const InputField = ({ type, placeholder, error, register }) => (
   </div>
 );
 
-const SelectField = ({ id, options, error, register }) => (
+// Компонент SelectField с типизацией
+interface SelectFieldProps {
+  id: string;
+  options: { value: string; label: string }[];
+  error: any;
+  register: any;
+}
+
+const SelectField: React.FC<SelectFieldProps> = ({ id, options, error, register }) => (
   <div className="mb-4 mt-4">
     <select
       id={id}
@@ -355,7 +381,14 @@ const SelectField = ({ id, options, error, register }) => (
   </div>
 );
 
-const TextArea = ({ placeholder, error, register }) => (
+// Компонент TextArea с типизацией
+interface TextAreaProps {
+  placeholder: string;
+  error: any;
+  register: any;
+}
+
+const TextArea: React.FC<TextAreaProps> = ({ placeholder, error, register }) => (
   <div className="mb-4 mt-4">
     <textarea
       placeholder={placeholder}
@@ -373,20 +406,54 @@ const TextArea = ({ placeholder, error, register }) => (
   </div>
 );
 
-const SuccessMessage = ({ onReset }) => (
+// Компонент SuccessMessage
+interface MessageProps {
+  onReset: () => void;
+}
+
+const SuccessMessage: React.FC<MessageProps> = ({ onReset }) => (
   <div className="flex flex-col items-center justify-center text-center text-white rounded-md">
-    {/* SVG и сообщение об успешной отправке */}
-    {/* ... */}
+    <svg
+      width="100"
+      height="100"
+      className="text-green-500"
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M26.6666 50L46.6666 66.6667L73.3333 33.3333M50 96.6667C43.8716 96.6667 37.8033 95.4596 32.1414 93.1144C26.4796 90.7692 21.3351 87.3317 17.0017 82.9983C12.6683 78.6649 9.23082 73.5204 6.8856 67.8586C4.54038 62.1967 3.33331 56.1283 3.33331 50C3.33331 43.8716 4.54038 37.8033 6.8856 32.1414C9.23082 26.4796 12.6683 21.3351 17.0017 17.0017C21.3351 12.6683 26.4796 9.23084 32.1414 6.88562C37.8033 4.5404 43.8716 3.33333 50 3.33333C62.3767 3.33333 74.2466 8.24998 82.9983 17.0017C91.75 25.7534 96.6666 37.6232 96.6666 50C96.6666 62.3768 91.75 74.2466 82.9983 82.9983C74.2466 91.75 62.3767 96.6667 50 96.6667Z"
+        stroke="currentColor"
+        strokeWidth="3"
+      />
+    </svg>
+    <h3 className="py-5 text-2xl font-medium text-green-500">Успешно!</h3>
+    <p className="text-neutral-900 dark:text-neutral-300 md:px-4">Ваше сообщение отправлено.</p>
     <button className="mt-6 py-2 px-4 bg-red-500 rounded-full focus:outline-none text-neutral-100" onClick={onReset}>
       Вернуться
     </button>
   </div>
 );
 
-const ErrorMessage = ({ onReset }) => (
+// Компонент ErrorMessage
+const ErrorMessage: React.FC<MessageProps> = ({ onReset }) => (
   <div className="flex flex-col items-center justify-center text-center text-neutral-900 dark:text-neutral-300 rounded-md">
-    {/* SVG и сообщение об ошибке */}
-    {/* ... */}
+    <svg
+      width="97"
+      height="97"
+      viewBox="0 0 97 97"
+      className="text-rose-500"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M27.9995 69C43.6205 53.379 52.3786 44.621 67.9995 29M26.8077 29L67.9995 69M48.2189 95C42.0906 95 36.0222 93.7929 30.3604 91.4477C24.6985 89.1025 19.554 85.6651 15.2206 81.3316C10.8872 76.9982 7.44975 71.8538 5.10454 66.1919C2.75932 60.53 1.55225 54.4617 1.55225 48.3333C1.55225 42.205 2.75932 36.1366 5.10454 30.4748C7.44975 24.8129 10.8872 19.6684 15.2206 15.335C19.554 11.0016 24.6985 7.56418 30.3604 5.21896C36.0222 2.87374 42.0906 1.66667 48.2189 1.66667C60.5957 1.66667 72.4655 6.58333 81.2172 15.335C89.9689 24.0867 94.8856 35.9566 94.8856 48.3333C94.8856 60.71 89.9689 72.5798 81.2172 81.3316C72.4655 90.0833 60.5957 95 48.2189 95Z"
+        stroke="currentColor"
+        strokeWidth="3"
+      />
+    </svg>
+    <h3 className="py-5 text-2xl font-medium text-rose-500">Ошибка</h3>
+    <p className="text-neutral-900 dark:text-neutral-300 md:px-4">Ваше сообщение не было отправлено. Пожалуйста, попробуйте снова позже.</p>
     <button className="mt-6 py-2 px-4 bg-red-500 rounded-full focus:outline-none text-neutral-100" onClick={onReset}>
       Вернуться
     </button>
