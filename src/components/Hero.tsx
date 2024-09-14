e1c6ea62ca3e43ae7b6b93c757638a4eb48d77f8
Artyom Gorin <mysubcult1@gmail.com>
2024-09-14 20:25:51 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export function Hero() {
   return (
-    <section id="hero" className="bg-white dark:bg-neutral-900">
-      <div className="max-w-7xl mx-auto pt-6 sm:pt-5 pb-16 px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2">
-        <div className="pt-28 sm:pt-6 md:pt-32 flex flex-col justify-center text-center sm:justify-start sm:text-start">
-          <h1 className="text-5xl font-bold">
-            Добро пожаловать в <br />
+    <div id="hero" className="bg-white dark:bg-neutral-900">
+      <div className="max-w-7xl mx-auto pt-6 sm:pt-5 pb-16 px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="pt-28 sm:pt-6 md:pt-32 flex flex-col justify-center text-center sm:text-start">
+          <h1 className="text-5xl font-bold leading-tight">
+            Добро пожаловать в <br />{" "}
             <span className="text-red-600 font-extrabold">
               SmartDiag <span className="wave">👋</span>
             </span>
           </h1>
-          <p className="pt-6 text-base w-auto sm:w-10/12 md:w-10/12 dark:text-neutral-400">
+          <p className="pt-6 text-base w-auto sm:w-10/12 dark:text-neutral-400">
             Здесь вы найдёте всё необходимое программное обеспечение для
             диагностики и обслуживания вашего автомобиля. Мы предлагаем
             высококачественное и надёжное оборудование, которое поможет вам
@@ -20,27 +20,25 @@ export function Hero() {
             что техническое обслуживание автомобиля может быть простым и
             доступным для каждого.
           </p>
-          <div className="flex flex-col sm:flex-row pt-10 gap-4 min-w-[350px] justify-center sm:justify-start">
+          <div className="flex flex-col sm:flex-row pt-10 gap-4 min-w-[350px]">
             <Link href="#soft" scroll={false}>
-              <a className="btn-grad-red text-base font-medium flex items-center">
+              <a className="btn-grad-red text-base font-medium flex items-center group">
                 Программы для приборов
                 <span className="icon-container ml-2">
                   <svg
-                    className="icon"
+                    className="icon w-5 h-5 transition-transform duration-1000 group-hover:rotate-180"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
                     fill="currentColor"
                     viewBox="0 0 16 16"
                   >
                     <path
                       d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"
                       fill="white"
-                    />
+                    ></path>
                     <path
                       d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"
                       fill="white"
-                    />
+                    ></path>
                   </svg>
                 </span>
               </a>
@@ -52,14 +50,15 @@ export function Hero() {
             </Link>
           </div>
         </div>
-        <div className="flex items-center mt-12 justify-center md:justify-end">
+
+        <div className="flex items-center mt-12">
           <Image
             src="/images/hero/hero.svg"
             alt="Illustration representing car diagnostics"
-            width={512}
+            width={512.5}
             height={331}
             quality={75}
-            sizes="(max-width: 768px) 100vw, 50vw"
+            sizes="100vw"
             priority
           />
         </div>
@@ -72,36 +71,28 @@ export function Hero() {
           display: flex;
           align-items: center;
           justify-content: center;
-          background: linear-gradient(135deg, #f00, #f06); /* Добавлен градиент */
+          background: linear-gradient(90deg, #ef4444, #f87171);
           border: none;
-          padding: 0.5rem 1rem;
           border-radius: 0.375rem;
-          color: #fff;
+          padding: 0.5rem 1rem;
+          color: white;
           cursor: pointer;
-          transition: background-color 0.3s ease;
         }
 
-        .btn-grad-red:hover {
-          background-color: #c00; /* Тёмный цвет при наведении */
-        }
-
-        .icon-container {
+        .btn-grad-black {
+          position: relative;
+          overflow: hidden;
           display: flex;
           align-items: center;
           justify-content: center;
-        }
-
-        .icon {
-          width: 20px;
-          height: 20px;
-          transition: transform 0.3s ease; /* Уменьшено время анимации */
-          color: currentColor;
-        }
-
-        .btn-grad-red:hover .icon {
-          transform: rotate(180deg);
+          background: linear-gradient(90deg, #111827, #1f2937);
+          border: none;
+          border-radius: 0.375rem;
+          padding: 0.5rem 1rem;
+          color: white;
+          cursor: pointer;
         }
       `}</style>
-    </section>
+    </div>
   );
 }
