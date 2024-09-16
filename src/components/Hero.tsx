975d183d045cd9a8b3677f99a5344b1c5bff782b
Artyom Gorin <mysubcult1@gmail.com>
2024-09-16 12:18:14 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Hero() {
   return (
     <div id="hero" className="bg-white dark:bg-neutral-900">
-      <div className="max-w-7xl mx-auto py-6 sm:py-5 px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="max-w-7xl mx-auto pt-20 sm:pt-32 pb-16 px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="flex flex-col justify-center text-center sm:text-start space-y-6">
           <h1 className="text-5xl font-bold leading-tight">
             Добро пожаловать в <br />
@@ -15,7 +15,7 @@ export function Hero() {
           <p className="text-base sm:w-10/12 dark:text-neutral-400">
             Здесь вы найдёте всё необходимое программное обеспечение для диагностики и обслуживания вашего автомобиля. Мы предлагаем высококачественное и надёжное оборудование, которое поможет вам быстро и эффективно провести анализ и ремонт вашего авто. Мы уверены, что техническое обслуживание автомобиля может быть простым и доступным для каждого.
           </p>
-          <div className="flex flex-col sm:flex-row gap-4 min-w-[350px]">
+          <div className="flex flex-col sm:flex-row sm:items-center gap-4 min-w-[350px]">
             <Link href="#soft" scroll={false}>
               <a className="btn-grad-red text-base font-medium flex items-center group">
                 Программы для приборов
