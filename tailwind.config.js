5149e0a28b91c566b85b826c232e6802c629cf93
Artyom Gorin <mysubcult1@gmail.com>
2024-09-16 13:36:24 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,21 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
     },
   },
   plugins: [
     require("@tailwindcss/typography"),
-	require('@tailwindcss/forms'),
-	require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/container-queries'),
   ],
 };
