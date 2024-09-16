fdbf75d514f5ef79df246b0b116fa65db8c2eea1
Artyom Gorin <mysubcult1@gmail.com>
2024-09-16 11:18:37 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Contact from "../components/Contact";
 import Soft from "../components/Soft";
+import { Hero } from "../components/Hero";
 import Layout from "../components/Layout";
 import Articles from "../components/Articles";
 import { Services } from "../components/Services";
@@ -7,6 +8,7 @@ import { Services } from "../components/Services";
 export default function Home() {
   return (
     <Layout title="SmartDiag - Ваш проводник в мире автодиагностики">
+      <Hero />
       <Soft />
       <Articles />
       <Services />
