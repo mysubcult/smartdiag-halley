654be9a6c3e2130a110d228c97077112140cbc07
Artyom Gorin <mysubcult1@gmail.com>
2024-09-17 08:20:23 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,9 +34,9 @@ export default function Navbar() {
   return (
     <nav className="navbar fixed top-0 left-0 right-0 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-400 border-b border-neutral-200 dark:border-neutral-700 backdrop-blur-sm bg-white/90 dark:bg-neutral-900/80 z-20">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
+        <div className="relative flex h-16 items-center justify-between flex-wrap">
+          <div className="flex flex-1 items-center justify-start">
+            <div className="flex flex-shrink-0 items-center">
               <Link href="/" scroll={false} onClick={handleNavigationClick("#hero")}>
                 <Image
                   className="block h-12 w-auto"
@@ -50,16 +50,14 @@ export default function Navbar() {
                 />
               </Link>
             </div>
-          </div>
 
-          {/* Десктопное меню */}
-          <div className="hidden md:flex md:items-center md:space-x-5 md:flex-nowrap">
-            <div className="flex items-center space-x-5 flex-nowrap">
+            {/* Десктопная версия меню */}
+            <div className="hidden flex-wrap items-center space-x-5 ml-4 lg:flex">
               {navigation.map((item) => (
                 <Link
                   key={item.name}
                   href={item.href}
-                  className="relative text-lg font-bold text-neutral-900 dark:text-neutral-400 hover:text-red-500 before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-red-500 hover:before:w-full before:transition-all before:duration-300 before:ease-in-out whitespace-nowrap"
+                  className="relative text-lg font-bold text-neutral-900 dark:text-neutral-400 hover:text-red-500 before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-red-500 hover:before:w-full before:transition-all before:duration-300 before:ease-in-out"
                   style={{ textDecoration: "none" }}
                   scroll={false}
                   onClick={item.anchor ? handleNavigationClick(item.anchor) : undefined}
@@ -68,11 +66,14 @@ export default function Navbar() {
                 </Link>
               ))}
             </div>
+          </div>
 
-            {/* Кнопки магазинов */}
-            <div className="flex items-center space-x-2 flex-nowrap">
+          {/* Группа кнопок для магазинов */}
+          <div className="flex items-center space-x-4">
+            {/* Скрываем кнопки на мобильной версии */}
+            <div className="hidden lg:flex space-x-2">
               <Link href="https://www.ozon.ru/seller/smartdiag-862410/" target="_blank" rel="noopener noreferrer">
-                <button className="flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 whitespace-nowrap">
+                <button className="flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
                   <Image
                     src="/images/logos/favicon.ico"
                     alt="OZON"
@@ -86,7 +87,7 @@ export default function Navbar() {
               </Link>
 
               <Link href="https://market.yandex.ru/business--smartdiag/50025236" target="_blank" rel="noopener noreferrer">
-                <button className="flex items-center justify-center bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-500 hover:to-yellow-700 text-black px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 whitespace-nowrap">
+                <button className="flex items-center justify-center bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-500 hover:to-yellow-700 text-black px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
                   <Image
                     src="https://yastatic.net/market-export/_/i/favicon/ymnew/favicon.ico"
                     alt="Яндекс Маркет"
@@ -100,7 +101,7 @@ export default function Navbar() {
               </Link>
 
               <Link href="https://www.wildberries.ru/seller/1343369" target="_blank" rel="noopener noreferrer">
-                <button className="flex items-center justify-center bg-gradient-to-r from-purple-500 to-purple-700 hover:from-purple-600 hover:to-purple-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105 whitespace-nowrap">
+                <button className="flex items-center justify-center bg-gradient-to-r from-purple-500 to-purple-700 hover:from-purple-600 hover:to-purple-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
                   <Image
                     src="/images/logos/favicon.ico"
                     alt="Wildberries"
@@ -113,26 +114,26 @@ export default function Navbar() {
                 </button>
               </Link>
             </div>
-          </div>
 
-          {/* Мобильное меню */}
-          <div className="flex items-center md:hidden">
             <ThemeSwitchButton />
-            <button
-              className="ml-2 inline-flex items-center justify-center rounded-md text-neutral-900 dark:text-white menu-icon-container hover:bg-gray-200 dark:hover:bg-neutral-800 transition-colors"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className={`h-6 w-6 ${isMenuOpen ? "hidden" : "block"}`} />
-              <XMarkIcon className={`h-6 w-6 ${isMenuOpen ? "block" : "hidden"}`} />
-            </button>
+
+            {/* Мобильная версия меню */}
+            <div className="lg:hidden">
+              <button
+                className="inline-flex items-center justify-center rounded-md text-neutral-900 dark:text-white menu-icon-container hover:bg-gray-200 dark:hover:bg-neutral-800 transition-colors"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className={`h-6 w-6 ${isMenuOpen ? "hidden" : "block"}`} />
+                <XMarkIcon className={`h-6 w-6 ${isMenuOpen ? "block" : "hidden"}`} />
+              </button>
+            </div>
           </div>
         </div>
       </div>
 
-      {/* Выпадающее мобильное меню */}
       {isMenuOpen && (
-        <div className="md:hidden mobile-menu bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 rounded-xl shadow-lg p-4 mx-4 mt-2 z-30">
+        <div className="lg:hidden mobile-menu bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 rounded-xl shadow-lg p-4 absolute right-4 top-20 w-64 z-30">
           <div className="flex flex-col items-center space-y-4">
             {navigation.map((item) => (
               <Link
