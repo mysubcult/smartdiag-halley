c4d39f5d0d13cf4287862c60d3cd7778e8d6181e
Artyom Gorin <mysubcult1@gmail.com>
2024-09-17 08:04:03 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitchButton from "./ThemeSwitchButton";
@@ -20,20 +20,7 @@ function classNames(...classes: string[]): string {
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
-  const [fontSize, setFontSize] = useState("18px");
-  const [isMobileView, setIsMobileView] = useState(false);
-
   const router = useRouter();
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobileView(window.innerWidth <= 1200);
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   const handleNavigationClick = useCallback(
     (anchor: string) => (event: React.MouseEvent) => {
@@ -47,24 +34,6 @@ export default function Navbar() {
     },
     [router]
   );
-
-  const updateFontSize = useCallback(() => {
-    const baseFontSize = 18;
-    const minFontSize = 14;
-    const screenHeight = window.innerHeight;
-    const itemsCount = isSubMenuOpen ? navigation.length + 3 : navigation.length;
-    const maxMenuHeight = screenHeight - 64;
-    const requiredHeight = itemsCount * 48;
-
-    const scaleFactor = maxMenuHeight / requiredHeight;
-    setFontSize(`${Math.max(minFontSize, baseFontSize * Math.min(scaleFactor, 1))}px`);
-  }, [isSubMenuOpen]);
-
-  useEffect(() => {
-    updateFontSize();
-    window.addEventListener("resize", updateFontSize);
-    return () => window.removeEventListener("resize", updateFontSize);
-  }, [updateFontSize]);
 
   return (
     <nav className="navbar fixed top-0 left-0 right-0 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-400 border-b border-neutral-200 dark:border-neutral-700 backdrop-blur-sm bg-white/90 dark:bg-neutral-900/80 z-20">
@@ -86,7 +55,8 @@ export default function Navbar() {
               </Link>
             </div>
 
-            <div className={`${isMobileView ? "hidden" : "flex"} navbar-nav ml-4`}>
+            {/* Десктопная версия меню */}
+            <div className="hidden lg:flex navbar-nav ml-4">
               <div className="flex space-x-5 items-center">
                 {navigation.map((item) => (
                   <Link
@@ -105,55 +75,54 @@ export default function Navbar() {
           </div>
 
           <div className="flex items-center gap-2">
-            {!isMobileView && (
-              <>
-                <Link href="https://www.ozon.ru/seller/smartdiag-862410/" target="_blank" rel="noopener noreferrer">
-                  <button className="w-full flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
-                    <Image
-                      src="/images/logos/favicon.ico"
-                      alt="OZON"
-                      className="w-5 h-5 mr-2"
-                      width={20}
-                      height={20}
-                      loading="lazy"
-                    />
-                    OZON
-                  </button>
-                </Link>
+            <div className="hidden lg:flex">
+              <Link href="https://www.ozon.ru/seller/smartdiag-862410/" target="_blank" rel="noopener noreferrer">
+                <button className="w-full flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    src="/images/logos/favicon.ico"
+                    alt="OZON"
+                    className="w-5 h-5 mr-2"
+                    width={20}
+                    height={20}
+                    loading="lazy"
+                  />
+                  OZON
+                </button>
+              </Link>
 
-                <Link href="https://market.yandex.ru/business--smartdiag/50025236" target="_blank" rel="noopener noreferrer">
-                  <button className="w-full flex items-center justify-center bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-500 hover:to-yellow-700 text-black px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
-                    <Image
-                      src="https://yastatic.net/market-export/_/i/favicon/ymnew/favicon.ico"
-                      alt="Яндекс Маркет"
-                      className="w-5 h-5 mr-2"
-                      width={20}
-                      height={20}
-                      loading="lazy"
-                    />
-                    Яндекс Маркет
-                  </button>
-                </Link>
+              <Link href="https://market.yandex.ru/business--smartdiag/50025236" target="_blank" rel="noopener noreferrer">
+                <button className="w-full flex items-center justify-center bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-500 hover:to-yellow-700 text-black px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    src="https://yastatic.net/market-export/_/i/favicon/ymnew/favicon.ico"
+                    alt="Яндекс Маркет"
+                    className="w-5 h-5 mr-2"
+                    width={20}
+                    height={20}
+                    loading="lazy"
+                  />
+                  Яндекс Маркет
+                </button>
+              </Link>
 
-                <Link href="https://www.wildberries.ru/seller/1343369" target="_blank" rel="noopener noreferrer">
-                  <button className="w-full flex items-center justify-center bg-gradient-to-r from-purple-500 to-purple-700 hover:from-purple-600 hover:to-purple-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
-                    <Image
-                      src="/images/logos/favicon.ico"
-                      alt="Wildberries"
-                      className="w-5 h-5 mr-2"
-                      width={20}
-                      height={20}
-                      loading="lazy"
-                    />
-                    Wildberries
-                  </button>
-                </Link>
-              </>
-            )}
+              <Link href="https://www.wildberries.ru/seller/1343369" target="_blank" rel="noopener noreferrer">
+                <button className="w-full flex items-center justify-center bg-gradient-to-r from-purple-500 to-purple-700 hover:from-purple-600 hover:to-purple-900 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out transform hover:scale-105">
+                  <Image
+                    src="/images/logos/favicon.ico"
+                    alt="Wildberries"
+                    className="w-5 h-5 mr-2"
+                    width={20}
+                    height={20}
+                    loading="lazy"
+                  />
+                  Wildberries
+                </button>
+              </Link>
+            </div>
 
             <ThemeSwitchButton />
 
-            {isMobileView && (
+            {/* Мобильная версия меню */}
+            <div className="lg:hidden">
               <button
                 className="inline-flex items-center justify-center rounded-md text-neutral-900 dark:text-white menu-icon-container hover:bg-gray-200 dark:hover:bg-neutral-800 transition-colors"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -162,13 +131,13 @@ export default function Navbar() {
                 <Bars3Icon className={`h-6 w-6 ${isMenuOpen ? "hidden" : "block"}`} />
                 <XMarkIcon className={`h-6 w-6 ${isMenuOpen ? "block" : "hidden"}`} />
               </button>
-            )}
+            </div>
           </div>
         </div>
       </div>
 
       {isMenuOpen && (
-        <div className="mobile-menu bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 rounded-xl shadow-lg p-4 absolute right-4 top-20 w-64 z-30">
+        <div className="lg:hidden mobile-menu bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-700 rounded-xl shadow-lg p-4 absolute right-4 top-20 w-64 z-30">
           <div className="flex flex-col items-center space-y-4">
             {navigation.map((item) => (
               <Link
