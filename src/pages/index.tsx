6063b9fc0fb264830ad82e33a7c5147e090e1b16
Artyom Gorin <mysubcult1@gmail.com>
2024-09-20 01:27:29 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,33 @@ import { Hero } from "../components/Hero";
 import Layout from "../components/Layout";
 import Articles from "../components/Articles";
 import { Services } from "../components/Services";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Layout title="SmartDiag - Ваш проводник в мире автодиагностики">
+      <Head>
+        {/* Мета-теги для поисковых систем */}
+        <meta
+          name="description"
+          content="SmartDiag предлагает широкий выбор диагностических приборов и программ для автосервиса. Подключение и диагностика автомобилей через VCDS, Вася, Autocom CDP+, Delphi DS150, ELM 327, Wurth WoW Snooper, Mucar, Thinkdiag, Diagzone, X-DIAG, ProDiag, Galletto 1260, BMW E-NET (E-Sys), K-Dcan INPA, ELS 27, Mini-VCI, Lexia 3/PP2000, Kingbolen, Thinkcar."
+        />
+        <meta
+          name="keywords"
+          content="диагностическое оборудование для авто, автодиагностика, VCDS, Вася диагност, Autocom CDP+, Delphi DS150, ELM 327, Wurth WoW Snooper, Mucar, Thinkdiag, Diagzone, X-DIAG, ProDiag, Galletto 1260, BMW E-NET, E-Sys, K-Dcan INPA, ELS 27, Mini-VCI, Lexia 3, PP2000, Kingbolen, Thinkcar, автооборудование, диагностика автомобилей, программное обеспечение для диагностики, автосервис"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="og:title"
+          content="SmartDiag - Ваш проводник в мире автодиагностики"
+        />
+        <meta
+          property="og:description"
+          content="SmartDiag предлагает качественные диагностические решения для автомобилей. Поддержка популярных приборов: VCDS, Вася, Autocom CDP+, Delphi DS150, ELM 327, Wurth WoW Snooper, Mucar, Thinkdiag, Diagzone, X-DIAG, ProDiag, Galletto 1260, BMW E-NET (E-Sys), K-Dcan INPA, ELS 27, Mini-VCI, Lexia 3/PP2000, Kingbolen, Thinkcar."
+        />
+      </Head>
+
+      {/* Компоненты страницы */}
       <Hero />
       <Soft />
       <Articles />
