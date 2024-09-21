bc7e8fe91e0d5352d2c8c11df710158c173fdbbb
Artyom Gorin <mysubcult1@gmail.com>
2024-09-21 08:31:14 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import PrivacyPolicyModal from './PrivacyPolicyModal';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
 
 type Info = {
   access_key: string;
@@ -73,12 +72,7 @@ export default function Contact() {
 
   return (
     <div className="bg-white dark:bg-neutral-900" id="contact">
-      <motion.div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 text-center"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, ease: 'easeOut' }}
-      >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 text-center">
         <h2 className="text-4xl font-bold">Обратная связь 📩</h2>
         <p className="pt-6 pb-6 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
           Мы очень ценим ваше мнение и постоянно работаем над улучшением нашего сервиса. Если у вас есть предложения,
@@ -86,23 +80,11 @@ export default function Contact() {
           которая находится ниже, или связаться с нами через мессенджеры. Наша команда старается оперативно отвечать на
           все запросы, и мы гарантируем, что вы получите ответ в течение 12 рабочих часов.
         </p>
-      </motion.div>
+      </div>
 
-      <motion.div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 gap-y-8 md:gap-x-8"
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: { opacity: 0, y: 20 },
-          visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.2 } },
-        }}
-      >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 gap-y-8 md:gap-x-8">
         {/* Левая часть с контактной информацией */}
-        <motion.div
-          className="bg-gray-50 dark:bg-gray-800 p-6 rounded-lg shadow-lg"
-          whileHover={{ scale: 1.02 }}
-          transition={{ duration: 0.3 }}
-        >
+        <div className="bg-gray-50 dark:bg-gray-800 p-6 rounded-lg shadow-lg">
           <h2 className="text-lg font-bold text-center mb-6">📞 Наши контакты</h2>
 
           <div className="mb-6">
@@ -128,10 +110,18 @@ export default function Contact() {
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
+                    style={{
+                      fillRule: 'evenodd',
+                      clipRule: 'evenodd',
+                      strokeLinejoin: 'round',
+                      strokeMiterlimit: '1.41421',
+                      fill: 'currentColor',
+                    }}
                   >
-                    <path d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Z" />
+                    <path
+                      id="telegram-1"
+                      d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
+                    />
                   </svg>
                 }
                 hoverColor="hover:text-blue-500"
@@ -146,8 +136,7 @@ export default function Contact() {
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ fill: 'currentColor' }}
                   >
                     <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
                   </svg>
@@ -183,14 +172,10 @@ export default function Contact() {
             <h3 className="text-md font-semibold mb-2 text-red-600">Адрес компании:</h3>
             <p className="text-sm text-gray-700 dark:text-gray-300">📍 г. Тольятти, ул. Ленинградская 68</p>
           </div>
-        </motion.div>
+        </div>
 
         {/* Правая часть с формой обратной связи */}
-        <motion.div
-          className="bg-gray-50 dark:bg-gray-800 p-6 rounded-lg shadow-lg"
-          whileHover={{ scale: 1.02 }}
-          transition={{ duration: 0.3 }}
-        >
+        <div className="bg-gray-50 dark:bg-gray-800 p-6 rounded-lg shadow-lg">
           <h2 className="text-lg font-bold text-center mb-5">✍️ Обратная связь</h2>
           {!isSubmitSuccessful && (
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -279,11 +264,9 @@ export default function Contact() {
                 </span>
               </div>
 
-              <motion.button
+              <button
                 type="submit"
                 className="w-full py-3 font-semibold text-white bg-red-600 rounded-md transition duration-300 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
               >
                 {isSubmitting ? (
                   <svg
@@ -302,13 +285,13 @@ export default function Contact() {
                 ) : (
                   'Отправить сообщение'
                 )}
-              </motion.button>
+              </button>
             </form>
           )}
           {isSubmitSuccessful && modalState.isSuccess && <SuccessMessage onReset={reset} />}
           {isSubmitSuccessful && !modalState.isSuccess && <ErrorMessage onReset={reset} />}
-        </motion.div>
-      </motion.div>
+        </div>
+      </div>
       <PrivacyPolicyModal
         isOpen={modalState.isModalOpen}
         onClose={() => setModalState({ ...modalState, isModalOpen: false })}
@@ -448,12 +431,7 @@ interface MessageProps {
 }
 
 const SuccessMessage: React.FC<MessageProps> = ({ onReset }) => (
-  <motion.div
-    className="flex flex-col items-center justify-center text-center text-white rounded-md"
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-  >
+  <div className="flex flex-col items-center justify-center text-center text-white rounded-md">
     <svg
       width="100"
       height="100"
@@ -473,17 +451,12 @@ const SuccessMessage: React.FC<MessageProps> = ({ onReset }) => (
     <button className="mt-6 py-2 px-4 bg-red-500 rounded-full focus:outline-none text-neutral-100" onClick={onReset}>
       Вернуться
     </button>
-  </motion.div>
+  </div>
 );
 
 // Компонент ErrorMessage
 const ErrorMessage: React.FC<MessageProps> = ({ onReset }) => (
-  <motion.div
-    className="flex flex-col items-center justify-center text-center text-neutral-900 dark:text-neutral-300 rounded-md"
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-  >
+  <div className="flex flex-col items-center justify-center text-center text-neutral-900 dark:text-neutral-300 rounded-md">
     <svg
       width="97"
       height="97"
@@ -505,5 +478,5 @@ const ErrorMessage: React.FC<MessageProps> = ({ onReset }) => (
     <button className="mt-6 py-2 px-4 bg-red-500 rounded-full focus:outline-none text-neutral-100" onClick={onReset}>
       Вернуться
     </button>
-  </motion.div>
+  </div>
 );
