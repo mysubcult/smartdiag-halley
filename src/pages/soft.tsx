e607ad7be5cad18f19777e732e91b67f5e0bde42
Artyom Gorin <mysubcult1@gmail.com>
2024-09-24 11:19:37 +0400

    Create soft.tsx

---
+++ src/pages/soft.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Soft } from '../components/Soft';
+
+const soft: FC = () => {
+  return (
+    <div>
+      <Soft />
+    </div>
+  );
+};
+
+export default soft;
