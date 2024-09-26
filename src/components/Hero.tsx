fef818bb4cfdccbb7982afccef203a94e2071603
Artyom Gorin <mysubcult1@gmail.com>
2024-09-26 13:37:13 +0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ export function Hero() {
   return (
     <section
       id="hero"
-      className="bg-white dark:bg-neutral-900 flex items-center justify-center flex-1"
+      className="bg-white dark:bg-neutral-900 flex items-start lg:items-center justify-center flex-1"
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 sm:py-32 flex flex-col lg:flex-row items-center lg:justify-center gap-8 w-full">
         
