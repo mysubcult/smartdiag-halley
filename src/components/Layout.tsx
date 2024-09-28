5eff99531bab9112d9122176f9fc8392e498f364
Artyom Gorin <mysubcult1@gmail.com>
2024-09-28 17:31:01 +0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,6 @@
 
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import Footer from './Footer';
 import Navbar from './Navbar';
 import { ReactNode } from 'react';
 
@@ -52,7 +51,6 @@ const Layout = ({ children, title, description, keywords, image, type }: LayoutP
       <main className="flex-grow flex items-center justify-center">
         {children}
       </main>
-      <Footer />
     </div>
   );
 };
