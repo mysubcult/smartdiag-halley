b7559011b7f782ec17c9b4d9be950b746fef1658
Artyom Gorin <mysubcult1@gmail.com>
2024-10-02 02:03:46 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,11 +19,9 @@ type Info = {
 };
 
 export default function Contact() {
-  const [modalState, setModalState] = useState({
-    isModalOpen: false,
-    isChecked: true,
-    isSuccess: false,
-  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const {
     register,
@@ -49,7 +47,7 @@ export default function Contact() {
     return placeholders[topic] || 'Введите ваше сообщение';
   };
 
-  const onSubmit = async (data: Info, e: any) => {
+  const onSubmit = async (data: Info) => {
     try {
       const response = await fetch('https://api.web3forms.com/submit', {
         method: 'POST',
@@ -57,18 +55,19 @@ export default function Contact() {
           'Content-Type': 'application/json',
           Accept: 'application/json',
         },
-        body: JSON.stringify(data, null, 2),
+        body: JSON.stringify(data),
       });
+      
       const json = await response.json();
-      if (json.success) {
-        setModalState((prevState) => ({ ...prevState, isSuccess: true }));
-        e.target.reset();
+      
+      if (response.ok && json.success) {
+        setIsSuccess(true);
         reset();
       } else {
-        setModalState((prevState) => ({ ...prevState, isSuccess: false }));
+        setIsSuccess(false);
       }
     } catch (error) {
-      setModalState((prevState) => ({ ...prevState, isSuccess: false }));
+      setIsSuccess(false);
       console.error(error);
     }
   };
@@ -117,7 +116,7 @@ export default function Contact() {
             <h3 className="text-md font-semibold mb-4 text-red-600">Мы доступны в мессенджерах:</h3>
             <div className="flex justify-center space-x-8">
               <ContactInfo
-                href="https://смартдиаг-поддержка.рф/telegram" // Убедитесь, что ссылка правильная
+                href="https://смартдиаг-поддержка.рф/telegram"
                 imageSrc="/images/hero/telegram-qr.svg"
                 alt="QR Code Telegram"
                 platform="Telegram"
@@ -126,7 +125,7 @@ export default function Contact() {
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
-                    className="text-blue-500" // Добавляем цвет через Tailwind
+                    className="text-blue-500"
                     style={{
                       fillRule: 'evenodd',
                       clipRule: 'evenodd',
@@ -136,14 +135,14 @@ export default function Contact() {
                     }}
                   >
                     <path
-                      d="M18.384,15a1.992,1.992,0,0,0,.1-1c0-.3-.05-.59-.14-.87l2.12-1.65a.5.5,0,0,0,.11-.7l-2-3.464a.5.5,0,0,0-.6-.2l-2.49,1a7.014,7.014,0,0,0-1.6-.93l-.38-2.65A.5.5,0,0,0,14,4h-4a.5.5,0,0,0-.5.42l-.38,2.65c-.56.24-1.09.56-1.6.93l-2.49-1a.5.5,0,0,0-.6.22l-2,3.464a.5.5,0,0,0,.12.63l2.12,1.65c-.09.28-.14.58-.14.87 0,.3.05,.59.14,.87l-2.12,1.65a.5.5,0,0,0-.12.63l2,3.464c.12.2.38.3.6.2l2.49-1c.51.37,1.04.69,1.6,.93l.38,2.65c.04,.23,.24,.42,.5,.42h4c.26,0,.46-.19,.5-.42l.38-2.65c.56-.24,1.09-.56,1.6-.93l2.49,1c.22,.1,.48,0,.6-.2l2-3.464a.5.5,0,0,0-.12-.63l-2.12-1.65c.09-.28,.14-.58,.14-.87Zm-7.14,2.564a3,3,0,1,1,3-3A3,3,0,0,1,12,15.5Z"
+                      d="M18.384,15a1.992,1.992,0,0,0,.1-1c0-.3-.05-.59-.14-.87l2.12-1.65a.5.5,0,0,0,.11-.7l-2-3.464a.5.5,0,0,0-.6-.2l-2.49,1a7.014,7.014,0,0,0-1.6-.93l-.38-2.65A.5.5,0,0,0,14,4h-4a.5.5,0,0,0-.5.42l-.38,2.65c-.56.24-1.09.56-1.6.93l-2.49-1a.5.5,0,0,0-.6.22l-2,3.464a.5.5,0,0,0,.12.63l2.12,1.65c-.09.28-.14.58-.14.87 0,.3.05,.59.14,.87l-2.12,1.65a.5.5,0,0,0-.12.63l2,3.464c.12.2.38.3.6.2l2.49-1c.51.37,1.04.69,1.6.93l.38,2.65c.04,.23,.24,.42,.5,.42h4c.26,0,.46-.19,.5-.42l.38-2.65c.56-.24,1.09-.56,1.6-.93l2.49,1c.22,.1,.48,0,.6-.2l2-3.464a.5.5,0,0,0-.12-.63l-2.12-1.65c.09-.28,.14-.58,.14-.87Zm-7.14,2.564a3,3,0,1,1,3-3A3,3,0,0,1,12,15.5Z"
                     />
                   </svg>
                 }
                 hoverColor="hover:text-blue-500"
               />
               <ContactInfo
-                href="https://смартдиаг-поддержка.рф/whatsapp" // Убедитесь, что ссылка правильная
+                href="https://смартдиаг-поддержка.рф/whatsapp"
                 imageSrc="/images/hero/whatsapp-qr.svg"
                 alt="QR Code WhatsApp"
                 platform="WhatsApp"
@@ -152,7 +151,7 @@ export default function Contact() {
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
-                    className="text-green-500" // Добавляем цвет через Tailwind
+                    className="text-green-500"
                     style={{ fill: 'currentColor' }}
                   >
                     <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
@@ -269,19 +268,15 @@ export default function Contact() {
                   type="checkbox"
                   id="agree"
                   className="mr-2 cursor-pointer rounded text-red-600 focus:ring-red-500 bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-700"
-                  checked={modalState.isChecked}
-                  onChange={(e) =>
-                    setModalState({ ...modalState, isChecked: e.target.checked })
-                  }
+                  checked={isChecked}
+                  onChange={(e) => setIsChecked(e.target.checked)}
                   required
                 />
                 <span className="text-sm text-gray-700 dark:text-gray-300">
                   Я прочитал и согласен с{' '}
                   <button
                     type="button"
-                    onClick={() =>
-                      setModalState({ ...modalState, isModalOpen: true })
-                    }
+                    onClick={() => setIsModalOpen(true)}
                     className="text-red-600 hover:underline"
                   >
                     правилами на обработку персональных данных
@@ -321,13 +316,13 @@ export default function Contact() {
               </button>
             </form>
           )}
-          {isSubmitSuccessful && modalState.isSuccess && <SuccessMessage onReset={reset} />}
-          {isSubmitSuccessful && !modalState.isSuccess && <ErrorMessage onReset={reset} />}
+          {isSubmitSuccessful && isSuccess && <SuccessMessage onReset={reset} />}
+          {isSubmitSuccessful && !isSuccess && <ErrorMessage onReset={reset} />}
         </div>
       </div>
       <PrivacyPolicyModal
-        isOpen={modalState.isModalOpen}
-        onClose={() => setModalState({ ...modalState, isModalOpen: false })}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
       />
     </div>
   );
@@ -353,7 +348,7 @@ const ContactInfo: React.FC<ContactInfoProps> = ({
 }) => (
   <div className="flex flex-col items-center space-y-2">
     <div className="flex items-center space-x-2">
-      <span className={`${hoverColor}`}>{icon}</span> {/* Оборачиваем иконку в span для управления цветом */}
+      <span className={`${hoverColor}`}>{icon}</span>
       <a
         href={href}
         target="_blank"
