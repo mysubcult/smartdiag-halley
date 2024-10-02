1c42f206b4c46cc579e6f2c64d69682532ede1d0
Artyom Gorin <mysubcult1@gmail.com>
2024-10-02 21:51:36 +0400

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,14 +4,11 @@ import Layout from "../components/Layout";
 
 export default function NotFound() {
   const pageTitle = "404 - Упс! Что-то пошло не так";
-  const pageDescription = "Страница, которую вы ищете, не найдена. Возможно, она была удалена или вы ввели неправильный адрес.";
+  const pageDescription = "Страница, которую вы ищете, не найдена.";
 
   return (
     <Layout title={pageTitle} description={pageDescription}>
-      <div
-        className="bg-white dark:bg-neutral-900 w-full px-4 pt-32 pb-16"
-        id="faq"
-      >
+      <div className="bg-white dark:bg-neutral-900 w-full px-4 pt-32 pb-16" id="faq">
         <h2 className="text-4xl font-bold text-center">Упс! Что-то пошло не так.</h2>
         <p className="pt-6 pb-16 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
           Страница не найдена.
@@ -19,17 +16,17 @@ export default function NotFound() {
         <Image
           src="/images/404/404.svg"
           alt="Image 404"
-          width={1920}
-          height={1080}
+          layout="responsive" // Example usage of 'layout'
+          width={350} // Consistent width with layout
+          height={196} // Maintain aspect ratio
           quality={75}
-          sizes="100vw"
-          className="w-[350px] md:w-4/12 justify-center text-center mx-auto"
+          className="mx-auto"
         />
         <div className="mt-16 text-center">
-          <Link href="/">
-            <button className="bg-rose-500 text-white text-base rounded-full px-16 p-3 font-medium">
+          <Link href="/" passHref>
+            <a className="bg-rose-500 text-white text-base rounded-full px-16 p-3 font-medium">
               Вернуться на главную
-            </button>
+            </a>
           </Link>
         </div>
       </div>
