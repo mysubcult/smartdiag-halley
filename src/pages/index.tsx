2d276eec0056e312e22606b8d3df4081c62bd3e9
Artyom Gorin <mysubcult1@gmail.com>
2024-10-03 17:30:39 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <>
       <Head>
         {/* Мета-теги для поисковых систем */}
+        <title>SmartDiag - Ваш проводник в мире автодиагностики</title>
         <meta
           name="description"
           content="SmartDiag предлагает широкий выбор диагностических приборов и программ для автосервиса. Подключение и диагностика автомобилей через VCDS, Вася, Autocom CDP+, Delphi DS150, ELM 327, Wurth WoW Snooper, Mucar, Thinkdiag, Diagzone, X-DIAG, ProDiag, Galletto 1260, BMW E-NET (E-Sys), K-Dcan INPA, ELS 27, Mini-VCI, Lexia 3/PP2000, Kingbolen, Thinkcar."
