a9f4a1b10015d4a13484194db41250a5383b00af
Artyom Gorin <mysubcult1@gmail.com>
2024-10-03 10:25:13 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Добавляем эту строку для статической сборки
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 }
 
 module.exports = nextConfig
