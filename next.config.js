7cb3723a243e462a092504012361d879d49451f7
Artyom Gorin <mysubcult1@gmail.com>
2024-10-04 21:53:25 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,10 @@ const path = require('path');
 const nextConfig = {
   output: 'export', // Добавляем эту строку для статической сборки
   reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true, // Включаем сохранение позиции прокрутки
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
   },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
