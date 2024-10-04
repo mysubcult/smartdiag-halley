677f69208eb940e7245188163dc861fd627f1c2d
Artyom Gorin <mysubcult1@gmail.com>
2024-10-04 14:12:54 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,11 +25,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <ThemeProvider attribute="class">
-        <Layout>
-          <AnimatePresence mode="wait" initial={false}>
-            <Component key={router.asPath} {...pageProps} />
-          </AnimatePresence>
-        </Layout>
+        <AnimatePresence mode="wait">
+          <Layout key={router.asPath}>
+            <Component {...pageProps} />
+          </Layout>
+        </AnimatePresence>
         <Script
           id="lhc-widget-script"
           strategy="afterInteractive"
