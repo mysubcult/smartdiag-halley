4e225b47ce43e095c3b60113f04b7385cceed01c
Artyom Gorin <mysubcult1@gmail.com>
2024-10-18 17:58:29 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import Layout from '@/components/Layout';
 import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -44,42 +45,52 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <ThemeProvider attribute="class">
         <Layout>
-          <Component {...pageProps} />
-        </Layout>
+          <AnimatePresence exitBeforeEnter mode="wait" initial={false}>
+            <motion.div
+              key={router.asPath}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+              <Component {...pageProps} />
+            </motion.div>
+          </AnimatePresence>
 
-        <Script
-          id="lhc-widget-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var LHC_API = LHC_API||{};
-              LHC_API.args = {
-                mode: 'widget',
-                lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
-                wheight: 450,
-                wwidth: 350,
-                pheight: 520,
-                pwidth: 500,
-                domain: 'xn--80aajcuv3afm.xn--p1ai',
-                leaveamessage: true,
-                department: ["1"],
-                theme: "9",
-                check_messages: false,
-                lang: 'rus/'
-              };
-              (function() {
-                var po = document.createElement('script');
-                po.type = 'text/javascript';
-                po.setAttribute('crossorigin', 'anonymous');
-                po.async = true;
-                var date = new Date();
-                po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(po, s);
-              })();
-            `,
-          }}
-        />
+          <Script
+            id="lhc-widget-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                var LHC_API = LHC_API||{};
+                LHC_API.args = {
+                  mode: 'widget',
+                  lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
+                  wheight: 450,
+                  wwidth: 350,
+                  pheight: 520,
+                  pwidth: 500,
+                  domain: 'xn--80aajcuv3afm.xn--p1ai',
+                  leaveamessage: true,
+                  department: ["1"],
+                  theme: "9",
+                  check_messages: false,
+                  lang: 'rus/'
+                };
+                (function() {
+                  var po = document.createElement('script');
+                  po.type = 'text/javascript';
+                  po.setAttribute('crossorigin', 'anonymous');
+                  po.async = true;
+                  var date = new Date();
+                  po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
+                  var s = document.getElementsByTagName('script')[0];
+                  s.parentNode.insertBefore(po, s);
+                })();
+              `,
+            }}
+          />
+        </Layout>
       </ThemeProvider>
     </main>
   );
