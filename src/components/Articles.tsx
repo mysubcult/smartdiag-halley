c240a05443ff607fd5e8307e89ab62975da199fa
Artyom Gorin <mysubcult1@gmail.com>
2024-10-18 11:01:56 +0400

    Update Articles.tsx

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -2,6 +2,10 @@ import React, { useState, useMemo } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import {
+  MagnifyingGlassIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/24/solid";
 
 const categories = [
   { name: "Все", value: "Все" },
@@ -29,28 +33,14 @@ const blogPosts = [
     category: "Установка ПО",
     keywords: ["установка ПО", "Autocom 2021", "инструкция"],
   },
-  {
-    title: "тест",
-    image: "/images/blog/post1.jpg",
-    excerpt: "тест.",
-    link: "/articles/software/autocom2021",
-    category: "Установка ПО",
-    keywords: ["установка ПО", "Autocom 2021", "инструкция"],
-  },
-  {
-    title: "Инструкdция по установке Autocom 2021 Инструкdция по установке Autocom 2021 Инструкdция по установке Autocom 2021",
-    image: "/images/blog/post1.jpg",
-    excerpt: "Инструкdция по установке Autocom 2021 Инструкdция по установке Autocom 2021 Инструкdция по установке Autocom 2021",
-    link: "/articles/software/autocom2021",
-    category: "Установка ПО",
-    keywords: ["установка ПО", "Autocom 2021", "инструкция"],
-  },
 ];
 
 export default function Blog() {
   const [selectedCategory, setSelectedCategory] = useState<string>("Все");
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   const postsPerPage = 8;
 
@@ -96,14 +86,112 @@ export default function Blog() {
         </motion.p>
       </div>
 
-      {/* Фильтры */}
-      <div className="max-w-max mx-auto px-4 sm:px-6 lg:px-8 mt-8">
-        <div className="flex flex-wrap items-center bg-gray-100 dark:bg-gray-800 p-4 rounded-lg shadow space-y-4 sm:space-y-0 sm:space-x-4">
+      {/* Mobile Menu */}
+      <div className="lg:hidden flex flex-col items-center pt-4">
+        <motion.div
+          className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg shadow-md w-full max-w-xs"
+          layout
+        >
+          <div className="flex items-center justify-between">
+            <motion.button
+              className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-black dark:text-white rounded-full flex items-center justify-between w-full"
+              onClick={() => {
+                setIsMobileMenuOpen((prev) => !prev);
+                if (isSearchOpen) setIsSearchOpen(false);
+              }}
+              aria-expanded={isMobileMenuOpen}
+              aria-label="Выберите категорию"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <span>{selectedCategory}</span>
+              <motion.div
+                animate={{ rotate: isMobileMenuOpen ? 180 : 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <ChevronDownIcon className="w-5 h-5" />
+              </motion.div>
+            </motion.button>
+            <motion.button
+              className="ml-4 px-4 py-2 bg-gray-200 dark:bg-gray-700 text-black dark:text-white rounded-full shadow"
+              onClick={() => {
+                setIsSearchOpen((prev) => !prev);
+                if (isMobileMenuOpen) setIsMobileMenuOpen(false);
+              }}
+              aria-label="Поиск"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <MagnifyingGlassIcon className="w-6 h-6" />
+            </motion.button>
+          </div>
+
+          {/* Dropdown */}
+          <motion.ul
+            className="bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden text-center m-0 p-0 list-none"
+            initial={false}
+            animate={
+              isMobileMenuOpen
+                ? { opacity: 1, height: "auto", marginTop: "1rem" }
+                : { opacity: 0, height: 0, marginTop: 0 }
+            }
+            transition={{ duration: 0.3 }}
+          >
+            {categories.map((category, index) => (
+              <li
+                key={category.value}
+                className={`text-black dark:text-white px-4 py-2 leading-tight hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-none ${
+                  index === 0 ? "first:rounded-t-none" : ""
+                } ${
+                  index === categories.length - 1 ? "last:rounded-b-none" : ""
+                }`}
+                onClick={() => {
+                  setSelectedCategory(category.value);
+                  setIsMobileMenuOpen(false);
+                }}
+              >
+                {category.name}
+              </li>
+            ))}
+          </motion.ul>
+
+          {/* Search Bar */}
+          <motion.div
+            className="overflow-hidden w-full"
+            initial={false}
+            animate={
+              isSearchOpen
+                ? { opacity: 1, height: "auto", marginTop: "1rem" }
+                : { opacity: 0, height: 0, marginTop: 0 }
+            }
+            transition={{ duration: 0.3 }}
+          >
+            <input
+              type="text"
+              placeholder="Поиск..."
+              id="mobile-search"
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                setCurrentPage(1);
+              }}
+              className="w-full px-4 py-2 border-none focus:outline-none bg-gray-100 dark:bg-gray-700 dark:text-white rounded-lg"
+            />
+          </motion.div>
+        </motion.div>
+      </div>
+
+      {/* Desktop Filters and Search */}
+      <div className="hidden lg:block max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
+        <div className="max-w-max mx-auto flex flex-wrap items-center bg-gray-100 dark:bg-gray-800 p-2 rounded-lg shadow space-y-4 sm:space-y-0 sm:space-x-4">
           <div className="flex flex-wrap gap-2">
             {categories.map((category) => (
               <motion.button
                 key={category.value}
-                onClick={() => setSelectedCategory(category.value)}
+                onClick={() => {
+                  setSelectedCategory(category.value);
+                  setCurrentPage(1);
+                }}
                 className={`px-4 py-2 rounded-full text-sm font-medium transition-colors duration-300 ${
                   selectedCategory === category.value
                     ? "bg-red-500 text-white shadow-lg"
@@ -116,13 +204,16 @@ export default function Blog() {
               </motion.button>
             ))}
           </div>
-          {/* Поиск */}
           <div className="w-full sm:w-auto mt-4 sm:mt-0">
             <input
               type="text"
               placeholder="Поиск..."
+              id="desktop-search"
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                setCurrentPage(1);
+              }}
               className="w-full sm:w-64 px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 dark:bg-gray-700 dark:text-white"
             />
           </div>
