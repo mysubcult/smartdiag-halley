335e8df3b4f0587c56965d29cd1b0820e000de1c
Artyom Gorin <mysubcult1@gmail.com>
2024-11-30 05:19:01 +0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ interface SocialLink {
 const menus: MenuItem[] = [
   { title: 'Главная', url: '/' },
   { title: 'Программы', url: '/soft' },
-  { title: 'Инструкции', url: 'https://docs.смартдиаг.рф/' },
+  { title: 'Инструкции', url: 'https://docs.смартдиаг.рф/docs/intro' },
   { title: 'О нас', url: '/about' },
   { title: 'Обратная связь', url: '/contact' },
 ];
