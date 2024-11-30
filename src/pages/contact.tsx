f62d3cfafe80334d2539c24d5b53a2c1c81b1536
Artyom Gorin <mysubcult1@gmail.com>
2024-11-30 05:44:57 +0400

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,7 +8,7 @@ const ContactPage: React.FC = () => {
     <>
       <Head>
         {/* Мета-теги для SEO */}
-        <title>Контакты - SmartDiag</title>
+        <title>Контакты | SmartDiag</title>
         <meta
           name="description"
           content="Свяжитесь с нами для получения поддержки, заказов или любых вопросов. SmartDiag всегда готов помочь вам с диагностическими решениями для автомобилей."
@@ -18,7 +18,7 @@ const ContactPage: React.FC = () => {
           content="контакты, поддержка, заказ оборудования, установка ПО, SmartDiag"
         />
         <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="Контакты - SmartDiag" />
+        <meta property="og:title" content="Контакты | SmartDiag" />
         <meta
           property="og:description"
           content="Свяжитесь с нами для получения поддержки, заказов или любых вопросов. SmartDiag всегда готов помочь вам с диагностическими решениями для автомобилей."
